fc9c69c657a25c9d24529779c31be1a50367b823
wlongabaugh <wlongabaugh@systemsbiology.org>
2019-12-20 16:06:13 -0800

    TARGET File Metadata Table Test

--- GDC-Metadata-Processing/build_release_metadata_bq_tables.py
+++ GDC-Metadata-Processing/build_release_metadata_bq_tables.py
@@ -22,13 +22,99 @@ Make sure the VM has BigQuery and Storage Read/Write permissions!
 Extract GDC Metadata into Per-Project/Build File BQ Tables
 This is still a work in progress (10/16/19)
 
+NOTE AS CURRENTLY SET UP IT DOES NOT PULL IN GENE EXPRESSION TEXT FILES
+Inventory as of Release 18 for files that are not multi-case ("multi" or ";" delimited)
+program_name	file_type	data_format	count
+BEATAML1.0	simple_somatic_mutation	VCF	103
+BEATAML1.0	aligned_reads	BAM	119
+CGCI	biospecimen_supplement	BCR PPS XML	120
+CGCI	biospecimen_supplement	BCR XML	120
+CGCI	biospecimen_supplement	BCR SSF XML	93
+CGCI	clinical_supplement	BCR XML	117
+CGCI	aligned_reads	BAM	500
+CGCI	gene_expression	TSV	226
+CGCI	gene_expression	TXT	339
+CPTAC	aligned_reads	BAM	3227
+CPTAC	annotated_somatic_mutation	VCF	1296
+CPTAC	gene_expression	TXT	1551
+CPTAC	gene_expression	TSV	1034
+CPTAC	simple_somatic_mutation	VCF	1296
+CTSP	aligned_reads	BAM	212
+CTSP	gene_expression	TSV	82
+CTSP	gene_expression	TXT	123
+FM	simple_somatic_mutation	VCF	18004
+FM	annotated_somatic_mutation	VCF	18004
+HCMI	biospecimen_supplement	BCR XML	7
+HCMI	annotated_somatic_mutation	VCF	56
+HCMI	aligned_reads	BAM	84
+HCMI	gene_expression	TXT	42
+HCMI	gene_expression	TSV	28
+HCMI	simple_somatic_mutation	VCF	56
+MMRF	annotated_somatic_mutation	VCF	4368
+MMRF	aligned_reads	BAM	6577
+MMRF	gene_expression	TXT	2577
+MMRF	gene_expression	TSV	1718
+MMRF	simple_somatic_mutation	VCF	4368
+NCICCR	aligned_reads	BAM	2400
+NCICCR	gene_expression	TXT	1443
+NCICCR	gene_expression	TSV	962
+ORGANOID	aligned_reads	BAM	298
+ORGANOID	annotated_somatic_mutation	VCF	65
+ORGANOID	gene_expression	TXT	165
+ORGANOID	gene_expression	TSV	110
+ORGANOID	simple_somatic_mutation	VCF	65
+TARGET	aligned_reads	BAM	3121
+TARGET	gene_expression	TXT	2130
+TARGET	annotated_somatic_mutation	VCF	1615
+TARGET	mirna_expression	TXT	1170
+TARGET	simple_somatic_mutation	VCF	2050
+TARGET	gene_expression	TSV	472
+TCGA	aligned_reads	BAM	44873
+TCGA	biospecimen_supplement	BCR XML	11314
+TCGA	gene_expression	TXT	33279
+TCGA	methylation_beta_value	TXT	12359
+TCGA	slide_image	SVS	30072
+TCGA	mirna_expression	TXT	22164
+TCGA	copy_number_segment	TXT	45258
+TCGA	biospecimen_supplement	BCR SSF XML	10557
+TCGA	clinical_supplement	BCR XML	11167
+TCGA	clinical_supplement	BCR OMF XML	1051
+TCGA	biospecimen_supplement	BCR Auxiliary XML	2884
+TCGA	simple_somatic_mutation	VCF	44461
+TCGA	annotated_somatic_mutation	VCF	44461
+VAREPOP	annotated_somatic_mutation	VCF	7
+VAREPOP	aligned_reads	BAM	7
+VAREPOP	simple_somatic_mutation	VCF	7
+
+Distinct file types:
+
+extract_alignment_file_data_sql:
+  simple_somatic_mutation
+  annotated_somatic_mutation
+  aligned_reads
+
+extract_file_data_sql_slides:
+  slide_image
+
+extract_file_data_sql_clinbio:
+  biospecimen_supplement
+  clinical_supplement
+
+extract_other_file_data_sql:
+  copy_number_segment
+  gene_expression
+  methylation_beta_value
+  mirna_expression
+
+Notes:
+CGCI (only) has null aliquot IDS for "gene_expression" type (Rel 18)
+FM clinbio entries = 0 because they are all multi-case (Rel 17)
+
 '''
 
 import yaml
 import sys
 import io
-import requests
-from json import loads as json_loads
 
 from common_etl.support import generic_bq_harness, confirm_google_vm, \
                                bq_harness_with_result, delete_table_bq_job
@@ -81,20 +167,20 @@ BAM and VCF extraction: BAMS, simple somatic, and annotated somatic VCFs in the 
 '''
 def extract_aligned_file_data(release_table, program_name, target_dataset, dest_table, do_batch):
 
-    sql = extract_file_data_sql(release_table, program_name)
+    sql = extract_alignment_file_data_sql(release_table, program_name)
     return generic_bq_harness(sql, target_dataset, dest_table, do_batch, True)
 
 '''
 ----------------------------------------------------------------------------------------------
 SQL for above:
 '''
-def extract_file_data_sql(release_table, program_name):
+def extract_alignment_file_data_sql(release_table, program_name):
     return '''
         SELECT 
             a.file_id as file_gdc_id,
             a.case_gdc_id,
             # When there are two aliquots (tumor/normal VCFs, it looks like the target table is using the second
-            # no matter what is is...
+            # no matter what it is...
             CASE WHEN (STRPOS(a.associated_entities__entity_gdc_id, ";") != 0)
                  THEN REGEXP_EXTRACT(a.associated_entities__entity_gdc_id,
                                      r"^[a-zA-Z0-9-]+;([a-zA-Z0-9-]+)$") 
@@ -121,12 +207,66 @@ def extract_file_data_sql(release_table, program_name):
             a.acl
         FROM `{0}` AS a
         WHERE ( a.program_name = '{1}' ) AND
-              ( ( a.file_type = "simple_somatic_mutation" AND a.data_format = "VCF" ) OR
-                ( a.file_type = "annotated_somatic_mutation" AND a.data_format = "VCF" ) OR
-                ( a.file_type = "aligned_reads" AND a.data_format = "BAM" ) ) AND
+              # Do not restrict file type:
+              #( ( a.file_type = "simple_somatic_mutation" AND a.data_format = "VCF" ) OR
+              #  ( a.file_type = "annotated_somatic_mutation" AND a.data_format = "VCF" ) OR
+              #  ( a.file_type = "aligned_reads" AND a.data_format = "BAM" ) ) AND
+              ( a.file_type = "simple_somatic_mutation" OR
+                a.file_type = "annotated_somatic_mutation" OR
+                a.file_type = "aligned_reads" ) AND
               ( a.associated_entities__entity_type ="aliquot" )
         '''.format(release_table, program_name)
 
+'''
+----------------------------------------------------------------------------------------------
+Various other files
+'''
+def extract_other_file_data(release_table, program_name, target_dataset, dest_table, do_batch):
+
+    sql = extract_other_file_data_sql(release_table, program_name)
+    return generic_bq_harness(sql, target_dataset, dest_table, do_batch, True)
+
+'''
+----------------------------------------------------------------------------------------------
+SQL for above:
+'''
+def extract_other_file_data_sql(release_table, program_name):
+    return '''
+        SELECT
+            a.file_id as file_gdc_id,
+            a.case_gdc_id,
+            # When there are two aliquots (tumor/normal VCFs, it looks like the target table is using the second
+            # no matter what it is...
+            CASE WHEN (STRPOS(a.associated_entities__entity_gdc_id, ";") != 0)
+                 THEN REGEXP_EXTRACT(a.associated_entities__entity_gdc_id,
+                                     r"^[a-zA-Z0-9-]+;([a-zA-Z0-9-]+)$")
+              ELSE a.associated_entities__entity_gdc_id
+            END as aliquot_id,
+            a.project_short_name, # TCGA-OV
+            REGEXP_EXTRACT(a.project_short_name, r"^[A-Z]+-([A-Z]+$)") as disease_code, # OV
+            a.program_name, # TCGA
+            a.experimental_strategy as data_type,
+            a.data_category,
+            a.experimental_strategy,
+            a.file_type,
+            a.file_size,
+            a.data_format,
+            a.platform,
+            CAST(null AS STRING) as file_name_key,
+            a.index_file_gdc_id as index_file_id,
+            CAST(null AS STRING) as index_file_name_key,
+            a.index_file_size,
+            a.access,
+            a.acl
+        FROM `{0}` AS a
+        WHERE ( a.program_name = '{1}' ) AND
+              ( a.file_type = "copy_number_segment" OR
+                a.file_type = "gene_expression" OR
+                a.file_type = "methylation_beta_value" OR
+                # CGCI has null aliquot IDS for gene_expression type:
+                a.file_type = "mirna_expression" ) # AND
+              # ( a.associated_entities__entity_type ="aliquot" )
+        '''.format(release_table, program_name)
 
 '''
 ----------------------------------------------------------------------------------------------
@@ -134,14 +274,14 @@ Slide extraction
 '''
 def extract_slide_file_data(release_table, program_name, target_dataset, dest_table, do_batch):
 
-    sql = extract_file_data_sql_archived_slides(release_table, program_name)
+    sql = extract_file_data_sql_slides(release_table, program_name)
     return generic_bq_harness(sql, target_dataset, dest_table, do_batch, True)
 
 '''
 ----------------------------------------------------------------------------------------------
 SQL for above:
 '''
-def extract_file_data_sql_archived_slides(release_table, program_name):
+def extract_file_data_sql_slides(release_table, program_name):
     return '''
         SELECT 
             a.file_id as file_gdc_id,
@@ -168,9 +308,10 @@ def extract_file_data_sql_archived_slides(release_table, program_name):
             a.access,
             a.acl
         FROM `{0}` AS a
-        WHERE a.program_name = '{1}' AND ( a.file_type = "slide_image" AND a.data_format = "SVS" )
+        # Do not restrict type
+        # WHERE a.program_name = '{1}' AND ( a.file_type = "slide_image" AND a.data_format = "SVS" )
+        WHERE a.program_name = '{1}' AND a.file_type = "slide_image"
         '''.format(release_table, program_name)
-
 
 '''
 ----------------------------------------------------------------------------------------------
@@ -214,12 +355,12 @@ def extract_file_data_sql_clinbio(release_table, program_name):
               #  ( a.file_type = "biospecimen_supplement" AND a.data_format = "BCR XML" ) ) AND
               ( a.file_type = "clinical_supplement" OR a.file_type = "biospecimen_supplement" ) AND
               ( a.associated_entities__entity_type = "case" ) AND
+              # This dropping of multi-case entries makes FM table empty:
               # Armor against multiple case entries:
               ( a.case_gdc_id NOT LIKE "%;%" )  AND
               # Armor against multiple case entries:
               ( a.case_gdc_id != "multi" )
         '''.format(release_table, program_name)
-
 
 '''
 ----------------------------------------------------------------------------------------------
@@ -227,14 +368,14 @@ Get case barcodes associated with the clinical files:
 '''
 def extract_case_barcodes(release_table, aliquot_2_case_table, program_name, target_dataset, dest_table, do_batch):
 
-    sql = case_barcodes_sql_archive(release_table, aliquot_2_case_table, program_name)
+    sql = case_barcodes_sql(release_table, aliquot_2_case_table, program_name)
     return generic_bq_harness(sql, target_dataset, dest_table, do_batch, True)
 
 '''
 ----------------------------------------------------------------------------------------------
 SQL for above:
 '''
-def case_barcodes_sql_archive(release_table, aliquot_2_case_table, program_name):
+def case_barcodes_sql(release_table, aliquot_2_case_table, program_name):
     return '''
         WITH
         a1 AS (SELECT DISTINCT case_gdc_id, case_barcode FROM `{1}` GROUP BY case_gdc_id, case_barcode)            
@@ -247,6 +388,7 @@ def case_barcodes_sql_archive(release_table, aliquot_2_case_table, program_name)
             a.project_short_name,
             a.disease_code,
             a.program_name,
+            # FIXME DO WE WANT TO DROP THIS ARCHIVAL FIX???
             # Archival table had null in this slot:
             CAST(null AS STRING) as data_type,
             a.data_category,
@@ -271,14 +413,14 @@ Get sample and case barcodes associated with the sequence files:
 '''
 def extract_aliquot_barcodes(release_table, aliquot_2_case_table, program_name, target_dataset, dest_table, do_batch):
 
-    sql = aliquot_barcodes_sql_archive(release_table, aliquot_2_case_table, program_name)
+    sql = aliquot_barcodes_sql(release_table, aliquot_2_case_table, program_name)
     return generic_bq_harness(sql, target_dataset, dest_table, do_batch, True)
 
 '''
 ----------------------------------------------------------------------------------------------
 SQL for above:
 '''
-def aliquot_barcodes_sql_archive(release_table, aliquot_2_case_table, program_name):
+def aliquot_barcodes_sql(release_table, aliquot_2_case_table, program_name):
 
     return '''
         SELECT
@@ -313,14 +455,14 @@ Get sample and case barcodes associated with the slide files:
 '''
 def extract_slide_barcodes(release_table, slide_2_case_table, program_name, target_dataset, dest_table, do_batch):
 
-    sql = slide_barcodes_sql_archive(release_table, slide_2_case_table, program_name)
+    sql = slide_barcodes_sql(release_table, slide_2_case_table, program_name)
     return generic_bq_harness(sql, target_dataset, dest_table, do_batch, True)
 
 '''
 ----------------------------------------------------------------------------------------------
 SQL for above:
 '''
-def slide_barcodes_sql_archive(release_table, slide_2_case_table, program_name):
+def slide_barcodes_sql(release_table, slide_2_case_table, program_name):
 
     return '''
         # Some slides have two entries in the slide_2_case table if they depict two portions. Remove the dups:
@@ -360,9 +502,9 @@ def slide_barcodes_sql_archive(release_table, slide_2_case_table, program_name):
 ----------------------------------------------------------------------------------------------
 Glue different tables together:
 '''
-def build_union(slide_table, align_table, clin_table, target_dataset, dest_table, do_batch):
+def build_union(slide_table, align_table, clin_table, other_table, target_dataset, dest_table, do_batch):
 
-    sql = union_sql(slide_table, align_table, clin_table)
+    sql = union_sql(slide_table, align_table, clin_table, other_table)
     return generic_bq_harness(sql, target_dataset, dest_table, do_batch, True)
 
 
@@ -370,14 +512,16 @@ def build_union(slide_table, align_table, clin_table, target_dataset, dest_table
 ----------------------------------------------------------------------------------------------
 SQL for above:
 '''
-def union_sql(slide_table, align_table, clin_table):
+def union_sql(slide_table, align_table, clin_table, other_table):
     return '''
         SELECT * FROM `{0}`
         UNION ALL
         SELECT * FROM `{1}`
         UNION ALL
         SELECT * FROM `{2}`
-        '''.format(slide_table, align_table, clin_table)
+        UNION ALL
+        SELECT * FROM `{3}`
+        '''.format(slide_table, align_table, clin_table, other_table)
 
 '''
 ----------------------------------------------------------------------------------------------
@@ -488,7 +632,16 @@ def do_dataset_and_build(steps, build, build_tag, dataset, params):
                                             step_one_table, params['BQ_AS_BATCH']) 
         if not success:
             print("{} {} pull_clinbio job failed".format(dataset, build))
-            return False      
+            return False
+
+    if 'pull_other' in steps:
+        step_one_table = "{}_{}_{}".format(dataset, build, params['OTHER_STEP_1_TABLE'])
+        table_collection.append('{}.{}.{}'.format(params['WORKING_PROJECT'], params['TARGET_DATASET'], step_one_table))
+        success = extract_other_file_data(file_table, dataset, params['TARGET_DATASET'],
+                                            step_one_table, params['BQ_AS_BATCH'])
+        if not success:
+            print("{} {} pull_other job failed".format(dataset, build))
+            return False
         
     if 'slide_barcodes' in steps:
         in_table = '{}.{}.{}'.format(params['WORKING_PROJECT'], 
@@ -516,6 +669,20 @@ def do_dataset_and_build(steps, build, build_tag, dataset, params):
         if not success:
             print("{} {} align_barcodes job failed".format(dataset, build))
             return False
+
+    if 'other_barcodes' in steps:
+        in_table = '{}.{}.{}'.format(params['WORKING_PROJECT'],
+                                     params['TARGET_DATASET'],
+                                     "{}_{}_{}".format(dataset, build, params['OTHER_STEP_1_TABLE']))
+        step_two_table = "{}_{}_{}".format(dataset, build, params['OTHER_STEP_2_TABLE'])
+        table_collection.append('{}.{}.{}'.format(params['WORKING_PROJECT'], params['TARGET_DATASET'], step_two_table))
+        success = extract_aliquot_barcodes(in_table, params['ALIQUOT_TABLE'], dataset, params['TARGET_DATASET'],
+                                           step_two_table, params['BQ_AS_BATCH'])
+
+        if not success:
+            print("{} {} align_barcodes job failed".format(dataset, build))
+            return False
+
         
     if 'clinbio_barcodes' in steps:
         in_table = '{}.{}.{}'.format(params['WORKING_PROJECT'], 
@@ -540,11 +707,15 @@ def do_dataset_and_build(steps, build, build_tag, dataset, params):
                                         "{}_{}_{}".format(dataset, build, params['ALIGN_STEP_2_TABLE']))
         clinbio_table = '{}.{}.{}'.format(params['WORKING_PROJECT'], 
                                           params['TARGET_DATASET'], 
-                                          "{}_{}_{}".format(dataset, build, params['CLINBIO_STEP_2_TABLE']))        
+                                          "{}_{}_{}".format(dataset, build, params['CLINBIO_STEP_2_TABLE']))
+        other_table = '{}.{}.{}'.format(params['WORKING_PROJECT'],
+                                        params['TARGET_DATASET'],
+                                        "{}_{}_{}".format(dataset, build, params['OTHER_STEP_2_TABLE']))
+
 
         union_table = "{}_{}_{}".format(dataset, build, params['UNION_TABLE'])
         table_collection.append('{}.{}.{}'.format(params['WORKING_PROJECT'], params['TARGET_DATASET'], union_table))
-        success = build_union(slide_table, align_table, clinbio_table,
+        success = build_union(slide_table, align_table, clinbio_table, other_table,
                               params['TARGET_DATASET'], union_table, params['BQ_AS_BATCH'])
         if not success:
             print("{} {} union_tables job failed".format(dataset, build))
@@ -565,14 +736,14 @@ def do_dataset_and_build(steps, build, build_tag, dataset, params):
             print("{} {} create_final_table job failed".format(dataset, build))
             return False
 
-        #
-        # Clear out working temp tables:
-        #
+    #
+    # Clear out working temp tables:
+    #
 
     if 'dump_working_tables' in steps:
         dump_table_tags = ['SLIDE_STEP_1_TABLE', 'SLIDE_STEP_2_TABLE', 'ALIGN_STEP_1_TABLE',
                            'ALIGN_STEP_2_TABLE', 'CLINBIO_STEP_1_TABLE', 'CLINBIO_STEP_2_TABLE',
-                           'UNION_TABLE']
+                           'OTHER_STEP_1_TABLE', 'OTHER_STEP_2_TABLE']
         dump_tables = ["{}_{}_{}".format(dataset, build, params[x]) for x in dump_table_tags]
         for table in dump_tables:
             delete_table_bq_job(params['TARGET_DATASET'], table)
@@ -620,7 +791,7 @@ def main(args):
         file_table = "{}_{}".format(params['FILE_TABLE'], build_tag)
         #datasets = extract_program_names(file_table, params['BQ_AS_BATCH'])
         datasets = []
-        datasets.append("FM")
+        datasets.append("TARGET")
         for dataset in datasets:
             print ("Processing build {} ({}) for program {}".format(build, build_tag, dataset))  
             ok = do_dataset_and_build(steps, build, build_tag, dataset, params)
