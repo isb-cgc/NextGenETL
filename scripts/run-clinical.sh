13a10a5ae6b576189ae1c12bc20c1bd7f26dbad3
laurenwolfe-isb <lwolfe@systemsbiology.org>
2020-03-20 21:09:25 -0700

    Add clinical data bash script

---
+++ scripts/run-clinical.sh
@@ -0,0 +1,16 @@
+source ~/setEnvVars.sh
+
+export MY_VENV=~/virtualEnvETL
+export PYTHONPATH=.:${MY_VENV}/lib:~/extlib
+
+mkdir -p ~/config
+pushd ~/config > /dev/null
+gsutil cp gs://${CONFIG_BUCKET}/${CURRENT_CONFIG_PATH}/ClinicalBQBuild.yaml .
+popd > /dev/null
+
+pushd ${MY_VENV} > /dev/null
+source bin/activate
+popd > /dev/null
+cd ..
+python3 ./BQ_Table_Building/build_clinical_data_bq_table.py ~/config/ClinicalBQBuild.yaml
+deactivate
