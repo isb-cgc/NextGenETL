d6ddf707ad0c8ffec69f1121f60e170e4b84632f
laurenwolfe-isb <lwolfe@systemsbiology.org>
2020-03-20 19:42:58 -0700

    Did a bit more refactoring--abstracted a couple of functions for later reuse, set up to pull configuration from YAML, moved script to BQ_Table_Building, fleshed out comments.

--- BQ_Table_Building/build_clinical_data_bq_table.py
+++ BQ_Table_Building/build_clinical_data_bq_table.py
@@ -17,26 +17,254 @@ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 """
 
-# todo: based on setting in yaml config, either:
-#  - call api and get _mapping for field groups specified in field group name list string,
-#    then use that response to dynamically generate a json schema file.
-#  - pass pre-build schema file to use in creating the bq table
+import requests
+import json
+import time
+import os
+from common_etl.utils import infer_data_types, load_config, generate_bq_schema, collect_field_values, \
+    create_mapping_dict
+from google.cloud import bigquery
 
-"""
-NOTE: schema file syntax:
+YAML_HEADERS = ('params', 'steps')
+REQUIRED_YAML_PARAMS = ['ENDPOINT', 'EXPAND_FIELD_GROUPS', 'BATCH_SIZE', 'START_INDEX', 'MAX_PAGES', 'IO_MODE',
+                        'OUTPUT_FILEPATH', 'BQ_AS_BATCH', 'BQ_SCHEMA_FILEPATH']
 
-[
-    {"name": "field_name", "description": "Documentation info", type="integer"}, 
-    {"name": "field_name_2", "description": "Documentation info", type="string"} 
-]
-"""
 
-"""
-PLAN:
+def retrieve_and_output_cases(params):
+    """
+    Retrieves case records from API and outputs them to a JSON file,
+    which is later used to populate the clinical data BQ table.
+    :param params: API and file output params, from YAML configuration file
+    """
+    start_time = time.time()  # for benchmarking
+    total_cases_count = 0
+    is_last_page = False
 
-retrieve schema file, if exists
-else, generate json file from mappings:
- - get field_groups from yaml config
- - get associated fields from _mapping endpoint, u
+    with open(params['OUTPUT_FILEPATH'], params['IO_MODE']) as json_output_file:
+        inserted_count = 0
 
-"""
+        '''
+        Create a single parent array for case objects (if not stripped, it would append an array for each batch). 
+        Since it's not constructed in-memory, there's no size limit.
+        '''
+        json_output_file.write('{"cases": [')
+
+        while not is_last_page:
+            try:
+                request_params = {
+                    'from': params['START_INDEX'],
+                    'size': params['BATCH_SIZE'],
+                    'expand': params['EXPAND_FIELD_GROUPS']
+                }
+
+                # retrieve and parse a "page" (batch) of case objects
+                res = requests.post(url=params['ENDPOINT'], data=request_params)
+
+                if res.status_code != 200:
+                    restart_idx = inserted_count + params['START_INDEX']
+
+                    print('\n[ERROR] API request returned status code {}, exiting script.'.format(str(res.status_code)))
+                    print('This round, script inserted {} records. If IO_MODE is set to append in yaml conig, '
+                          'set START_INDEX to {} to resume writing to output file.'.format(inserted_count, restart_idx))
+                    exit(1)
+            except requests.exceptions.MissingSchema as err:
+                print('\n[ERROR] ', end='')
+                print(err)
+                print('(Hint: check the ENDPOINT value supplied in yaml config.)')
+                exit(1)
+
+            res_json = res.json()['data']
+
+            # Currently, if response doesn't contain this metadata, it indicates an invalid response or request.
+            if 'pagination' in res_json:
+                response_metadata = res_json['pagination']
+                total_cases_count = response_metadata['total']
+                curr_page = response_metadata['page']
+                last_page = response_metadata['pages']
+                cases_json = res_json['hits']
+            else:
+                raise TypeError("[ERROR] 'pagination' key not found in response json, exiting.")
+
+            # iterate over each case object and append to json file
+            for i in range(len(cases_json)):
+                json.dump(obj=cases_json[i], fp=json_output_file)
+
+                if i < len(cases_json) - 1:
+                    json_output_file.write(', ')
+
+            if curr_page != last_page or (params['MAX_PAGES'] and curr_page < params['MAX_PAGES']):
+                json_output_file.write(',')
+
+                # increment starting index for next batch of cases
+                request_params['from'] += request_params['size']
+            else:
+                is_last_page = True
+
+                # If this is the last page, append metadata and finish constructing the json object
+                json_metadata = {
+                    "inserted_count": inserted_count,
+                    "total_available_count": response_metadata['total']
+                }
+
+                json_output_file.write('], "metadata": ')
+                json.dump(obj=json_metadata, fp=json_output_file)
+                json_output_file.write('}')
+
+            print("Inserted page {} of {}!".format(curr_page, last_page))
+            inserted_count += response_metadata['count']
+
+    # calculate processing time and file size
+    total_time = time.time()-start_time
+    file_size = os.stat(params['OUTPUT_FILEPATH']).st_size / 1048576.0
+
+    print(
+        "\nClinical data retrieval complete. \n\n"
+        "RESULTS REPORT \n"
+        "------------- \n"
+        "{} cases inserted \n"
+        "{} cases available \n"
+        "{:.3f} mb file size \n"
+        "{:.3f} sec to execute script \n".format(inserted_count, total_cases_count, file_size, total_time)
+    )
+
+
+def create_field_records_dict(field_mapping_dict, field_data_type_dict):
+    """
+    Generate flat dict containing schema metadata object with fields 'name', 'type', 'description'
+    :param field_mapping_dict:
+    :param field_data_type_dict:
+    :return: schema fields object dict
+    """
+    schema_dict = {}
+
+    for key in field_data_type_dict:
+        try:
+            column_name = field_mapping_dict[key]['name'].split('.')[-1]
+            description = field_mapping_dict[key]['description']
+        except KeyError:
+            # cases.id not returned by mapping endpoint. In such cases, substitute an empty description string.
+            column_name = key.split(".")[-1]
+            description = ""
+
+        if field_data_type_dict[key]:
+            # if script was able to infer a data type using field's values, default to using that type
+            field_type = field_data_type_dict[key]
+        elif key in field_mapping_dict:
+            # otherwise, include type from _mapping endpoint
+            field_type = field_mapping_dict[key]['type']
+        else:
+            # this could happen in the case where a field was added to the cases endpoint with only null values,
+            # and no entry for the field exists in mapping
+            print("[ERROR] Not adding field {} because no type found".format(key))
+            continue
+
+        # this is the format for bq schema json object entries
+        schema_dict[key] = {
+            "name": column_name,
+            "type": field_type,
+            "description": description
+        }
+
+    return schema_dict
+
+
+def create_bq_schema_file(params):
+    """
+    Creates a BQ schema JSON file.
+    :param params: YAML config params
+    """
+    # generate dict containing field mapping results
+    field_mapping_dict = create_mapping_dict(params['ENDPOINT'])
+
+    with open(params['OUTPUT_FILEPATH'], 'r') as data_file:
+        json_obj = json.load(data_file)
+
+        if not json_obj:
+            print('[ERROR] Empty result retrieved from dataset file, nothing to generate.')
+            exit(1)
+
+    # for each field, generate a set of values used to infer datatype
+    field_value_sets = collect_field_values(dict(), 'cases', json_obj, '')
+    field_data_type_dict = infer_data_types(field_value_sets)
+
+    # create a flattened dict of schema fields
+    schema_dict = create_field_records_dict(field_mapping_dict, field_data_type_dict)
+
+    schema_fp = os.getcwd() + params['BQ_SCHEMA_FILEPATH']
+    record_type_name = params['ENDPOINT'].split('/')[-1]
+
+    generate_bq_schema(schema_dict, record_type_name, params['EXPAND_FIELD_GROUPS'], schema_fp)
+
+
+def validate_params(params):
+    def is_valid_idx_param(yaml_param):
+        try:
+            if not isinstance(params[yaml_param], int) or int(params[yaml_param]) < 0:
+                print('[ERROR] Invalid value for {} in yaml config (supplied value: {}). '
+                      'Value should be a non-negative integer'.format(yaml_param, params[yaml_param]))
+        except ValueError as e:
+            print("\n[ERROR] Non-integer value for {} in yaml config:".format(yaml_param))
+            print("[ERROR] ", end='')
+            print(e)
+
+        return True
+
+    # verify all required params exist in yaml config
+    for param in REQUIRED_YAML_PARAMS:
+        if param not in params:
+            print('[ERROR] Required param {} not found in yaml config.'.format(param))
+            exit(1)
+
+    # verify that api index-related params are set to non-negative integers
+    if not (is_valid_idx_param('BATCH_SIZE') and is_valid_idx_param('START_INDEX') and is_valid_idx_param('MAX_PAGES')):
+        exit(1)
+
+    # BATCH_SIZE must also be positive
+    if params['BATCH_SIZE'] == 0:
+        print('[ERROR] BATCH_SIZE in yaml_config should be greater than 0.')
+        exit(1)
+
+    # confirm that API request url is valid
+    try:
+        request_params = {
+            'from': params['START_INDEX'],
+            'size': params['BATCH_SIZE'],
+            'expand': params['EXPAND_FIELD_GROUPS']
+        }
+        res = requests.post(url=params['ENDPOINT'], data=request_params)
+
+        if res.status_code != 200:
+            print('API request return status {}, exiting script.'.format(str(res.status_code)))
+            exit(1)
+    except requests.exceptions.MissingSchema as err:
+        print('\n[ERROR] ', end='')
+        print(err)
+        print('(Hint: check the ENDPOINT value supplied in yaml config.)')
+        exit(1)
+
+
+def main(args):
+    if len(args) != 2:
+        print(" ")
+        print(" Usage : {} <configuration_yaml>".format(args[0]))
+        return
+
+    # Load the YAML config file
+    with open(args[1], mode='r') as yaml_file:
+        params, steps = load_config(yaml_file, YAML_HEADERS)
+
+    # Validate YAML config params
+    validate_params(params)
+
+    if 'retrieve_and_output_cases' in steps:
+        # Hits the GDC api endpoint, builds a json output data file
+        retrieve_and_output_cases(params)
+
+    if 'create_bq_schema_file' in steps:
+        # Creates a BQ schema json file
+        create_bq_schema_file(params)
+
+
+if __name__ == '__main__':
+    # main(sys.argv)
+    main(('build_clinical_data_bq_table.py', '../ConfigFiles/ClinicalBQBuild.yaml'))
--- common_etl/utils.py
+++ common_etl/utils.py
@@ -1,7 +1,8 @@
 import io
 import yaml
-import sys
 import pprint
+import json
+import requests
 
 
 def load_config(yaml_file, yaml_dict_keys):
@@ -14,24 +15,30 @@ def load_config(yaml_file, yaml_dict_keys):
     """
     yaml_dict = None
 
-    with open(yaml_file, mode='r') as yaml_file:
-        config_stream = io.StringIO(yaml_file.read())
+    config_stream = io.StringIO(yaml_file.read())
 
-        try:
-            yaml_dict = yaml.load(config_stream, Loader=yaml.FullLoader)
-        except yaml.YAMLError as ex:
-            print(ex)
+    try:
+        yaml_dict = yaml.load(config_stream, Loader=yaml.FullLoader)
+    except yaml.YAMLError as ex:
+        print(ex)
+        exit(1)
 
-        if yaml_dict is None:
-            sys.exit("Bad YAML load, exiting.")
+    if yaml_dict is None:
+        print("Bad YAML load, exiting.")
+        exit(1)
 
-        # Dynamically generate a list of dictionaries for the return statement, since tuples are immutable
-        return_dicts = [yaml_dict[key] for key in yaml_dict_keys]
+    # Dynamically generate a list of dictionaries for the return statement, since tuples are immutable
+    return_dicts = [yaml_dict[key] for key in yaml_dict_keys]
 
-        return tuple(return_dicts)
+    return tuple(return_dicts)
 
 
 def check_value_type(value):
+    """
+    Checks value for type (possibilities are string, float and integers)
+    :param value: value to type check
+    :return: type in BQ's format
+    """
     # if has leading zero, then should be considered a string, even if only composed of digits
     val_is_none = value == '' or value == 'NA' or value == 'null' or value is None or value == 'None'
     val_is_decimal = value.startswith('0.')
@@ -50,40 +57,190 @@ def check_value_type(value):
     if val_is_none:
         return None
     elif val_is_id:
-        return 'string'
+        return 'STRING'
     elif val_is_decimal or val_is_float:
-        return 'float'
+        return 'FLOAT64'
     elif val_is_num:
-        return 'integer'
+        return 'INT64'
     else:
-        return 'string'
+        return 'STRING'
 
     print("ERROR NOT FINDING TYPE, value: {}".format(value))
 
 
 def infer_data_types(flattened_json):
+    """
+    Infer data type of fields based on values contained in dataset.
+    :param flattened_json: file containing dict of field names (key) and sets of field values (value)
+    :return: dict of field names and inferred type (None if no data in value set).
+    """
     data_types = dict()
     for column in flattened_json:
         data_types[column] = None
 
         for value in flattened_json[column]:
-            if data_types[column] == 'string':
+            if data_types[column] == 'STRING':
                 break
 
             val_type = check_value_type(str(value))
 
             if not val_type:
                 continue
-            elif val_type == 'float' or val_type == 'string':
+            elif val_type == 'FLOAT64' or val_type == 'STRING':
                 data_types[column] = val_type
-            elif val_type == 'integer':
+            elif val_type == 'INT64':
                 if not data_types[column]:
-                    data_types[column] = 'integer'
+                    data_types[column] = 'INT64'
             else:
                 print("[ERROR] NO TYPE SET FOR val {}, type {}".format(value, val_type))
 
     return data_types
 
-def pprint_json(data):
+
+def create_nested_schema_obj(name, field_list):
+    """
+    Create a repeated record for BQ schema.
+    :param name: field's short name (without parent hierarchy)
+    :param field_list: parent list containing field referenced by name
+    :return: BQ schema repeated record
+    """
+    return {
+        "name": name,
+        "type": "RECORD",
+        "mode": "REPEATED",
+        "fields": field_list
+    }
+
+
+def collect_field_values(field_dict, key, parent_dict, prefix):
+    """
+    Recursively inserts sets of values for a given field into return dict (used to infer field data type)
+    :param field_dict: A dict of key:value pairs -- field_name : set(field_values)
+    :param key: field name
+    :param parent_dict: dict containing field and it's values
+    :param prefix: string representation of current location in field hierarchy
+    :return: field_dict
+    """
+    # If the value of parent_dict[key] is a list at this level, and a dict at the next (or a dict at this level,
+    # as seen in second conditional statement), iterate over each list element's dictionary entries.
+    # (Sometimes lists are composed of strings rather than dicts, and those are later converted to strings.)
+    if isinstance(parent_dict[key], list) and isinstance(parent_dict[key][0], dict):
+        for dict_item in parent_dict[key]:
+            for dict_key in dict_item:
+                field_dict = collect_field_values(field_dict, dict_key, dict_item, prefix + key + ".")
+    elif isinstance(parent_dict[key], dict):
+        for dict_key in parent_dict[key]:
+            field_dict = collect_field_values(field_dict, dict_key, parent_dict[key], prefix + key + ".")
+    else:
+        field_name = prefix + key
+
+        if field_name not in field_dict:
+            field_dict[field_name] = set()
+
+        # This type of list can be converted to a comma-separated value string
+        if isinstance(parent_dict[key], list):
+            value = ", ".join(parent_dict[key])
+        else:
+            value = parent_dict[key]
+
+        field_dict[field_name].add(value)
+
+    return field_dict
+
+
+def generate_bq_schema(schema_dict, record_type, expand_fields_list, output_fp):
+
+    # create a dict of lists using expand field group names
+    field_groups = expand_fields_list.split(',')
+
+    # add field group names to a list, in order to generate a dict
+    field_group_names = [record_type]
+
+    for field_group in field_groups:
+        field_group_names.append(record_type + '.' + field_group)
+
+    record_lists_dict = {fg:[] for fg in field_group_names}
+
+    # add field to correct field grouping list based on full field name
+    for field in schema_dict:
+        split_field_name = field.split('.')
+
+        # record_lists_dict key is equal to the parent field components of full field name
+        list_key = '.'.join(split_field_name[:-1])
+        record_lists_dict[list_key].append(schema_dict[field])
+
+    # calculate max field depth in order to nest expand field groups in bq schema
+    max_field_depth = 0
+
+    for list_key in record_lists_dict:
+        curr_depth = len(list_key.split('.'))
+        if max_field_depth < curr_depth:
+            max_field_depth = curr_depth
+
+    curr_level = max_field_depth
+
+    # insert nested field groupings into appropriate parent list
+    while curr_level > 1:
+        field_group_names = list(record_lists_dict.keys())
+
+        for field_group_name in field_group_names:
+            split_group_name = field_group_name.split('.')
+
+            # building from max depth inward, to avoid iterating through entire schema object in order to append
+            # child field groupings. Therefore, skip any field groupings at a shallower depth.
+            if len(split_group_name) == curr_level:
+                parent_name = '.'.join(split_group_name[:-1])
+                field_name = split_group_name[-1]
+
+                # pop in order to avoid adding the fields twice
+                field_group_list = record_lists_dict.pop(field_group_name)
+                record_lists_dict[parent_name].append(create_nested_schema_obj(field_name, field_group_list))
+
+        curr_level -= 1
+
+    schema_base_list = record_lists_dict[record_type]
+
+    with open(output_fp, 'w') as schema_file:
+        json.dump(schema_base_list, schema_file)
+
+
+def create_mapping_dict(endpoint):
+    """
+    Creates a dict containing field mappings for given endpoint.
+    Note: only differentiates the GDC API's 'long' type (called 'integer' in GDC data dictionary) and
+    'float' type (called 'number' in GDC data dictionary). All others typed as string.
+    :param endpoint: API endpoint for which to retrieve mapping.
+    :return: dict of field mappings. Each entry object contains field name, type, and description
+    """
+    field_mapping_dict = {}
+
+    # retrieve mappings json object
+    res = requests.get(endpoint + '/_mapping')
+    field_mappings = res.json()['_mapping']
+
+    for field in field_mappings:
+        # convert data types from GDC format to formats used in BQ
+        if field_mappings[field]['type'] == 'long':
+            field_type = 'INT64'
+        elif field_mappings[field]['type'] == 'float':
+            field_type = 'FLOAT64'
+        else:
+            field_type = 'STRING'
+
+        # create json object of field mapping data
+        field_mapping_dict[field] = {
+            'name': field.split('.')[-1],
+            'type': field_type,
+            'description': field_mappings[field]['description']
+        }
+
+    return field_mapping_dict
+
+
+def pprint_json(json_obj):
+    """
+    Pretty prints json objects.
+    :param json_obj: json object to pprint
+    """
     pp = pprint.PrettyPrinter(indent=4)
-    pp.pprint(data)
+    pp.pprint(json_obj)
