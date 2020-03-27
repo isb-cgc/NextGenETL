eb12dd95c00a69f6869caa439f7d3192a4d17a02
lhagen-isb <50964829+lhagen-isb@users.noreply.github.com>
2020-03-27 09:46:46 -0700

    added publish table step

--- BQ_Table_Building/build_rna_seq_gexp_bq_table.py
+++ BQ_Table_Building/build_rna_seq_gexp_bq_table.py
@@ -731,7 +731,24 @@ def main(args):
 
     if 'dump_working_tables' in steps:
         table_cleaner(params, file_sets, False)
-            
+
+    #
+    # publish table:
+    #
+
+    if 'publish' in steps:
+
+        source_table = '{}.{}.{}'.format(params['WORKING_PROJECT'], params['TARGET_DATASET'],
+                                         params['FINAL_TARGET_TABLE'])
+        publication_dest = '{}.{}.{}'.format(params['PUBLICATION_PROJECT'], params['PUBLICATION_DATASET'],
+                                             params['PUBLICATION_TABLE'])
+
+        success = publish_table(source_table, publication_dest)
+
+        if not success:
+            print("publish table failed")
+            return
+
     print('job completed')
 
 if __name__ == "__main__":
