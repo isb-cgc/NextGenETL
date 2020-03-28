d05d1bdfc9c5b8559b85109bb7d5d631ec7f34dd
laurenwolfe-isb <lwolfe@systemsbiology.org>
2020-03-27 19:00:46 -0700

    Trying to derive cde_id from schema endpoint on GDC, in process

--- common_etl/utils.py
+++ common_etl/utils.py
@@ -401,5 +401,5 @@ def pprint_json(json_obj):
     Pretty prints json objects.
     :param json_obj: json object to pprint
     """
-    pp = pprint.PrettyPrinter(indent=4)
+    pp = pprint.PrettyPrinter(indent=2)
     pp.pprint(json_obj)
