b8afe3ebfa13b0e93557d14f97d6b2d1d87c3c7e
lhagen-isb <50964829+lhagen-isb@users.noreply.github.com>
2020-04-03 08:45:46 -0700

    Update build_rna_seq_gexp_bq_table.py

--- BQ_Table_Building/build_rna_seq_gexp_bq_table.py
+++ BQ_Table_Building/build_rna_seq_gexp_bq_table.py
@@ -746,7 +746,7 @@ def main(args):
         print('archive files from VM')
         archive_file_prefix = "{}_{}".format(date.today(), params['PUBLICATION_DATASET'])
         yaml_file = re.search(r"\/(\w*.yaml)$", args[1])
-        archive_yaml = "{}_{}".format(archive_file_prefix, yaml_file.group())
+        archive_yaml = "{}_{}".format(archive_file_prefix, yaml_file.group(1))
         print(archive_yaml)
 #        for file_set in file_sets:
 #            count_name, count_dict = next(iter(file_set.items()))
