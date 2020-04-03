fdd59c9ba9b59fb293eaab546f5bb5e73b01b827
lhagen-isb <50964829+lhagen-isb@users.noreply.github.com>
2020-04-03 09:03:15 -0700

    Update build_rna_seq_gexp_bq_table.py

--- BQ_Table_Building/build_rna_seq_gexp_bq_table.py
+++ BQ_Table_Building/build_rna_seq_gexp_bq_table.py
@@ -746,8 +746,8 @@ def main(args):
         print('archive files from VM')
         archive_file_prefix = "{}_{}".format(date.today(), params['PUBLICATION_DATASET'])
         yaml_file = re.search(r"\/(\w*.yaml)$", args[1])
-        archive_yaml = "{}/{}/{}_{}".format(params['ARCHIVE_BUCKET_DIR'],
-                                            params['ARCHIVE_CONFIG'],
+        archive_yaml = "{}/{}_{}".format(params['ARCHIVE_BUCKET_DIR'],
+#                                            params['ARCHIVE_CONFIG'],
                                             archive_file_prefix,
                                             yaml_file.group(1))
         upload_to_bucket(params['ARCHIVE_BUCKET'],
