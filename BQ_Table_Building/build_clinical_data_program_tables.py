28ef528e4d22a45a96004a3e56812bd91d4849e9
laurenwolfe-isb <lwolfe@systemsbiology.org>
2020-04-07 10:20:11 -0700

    Exploring nesting for different programs

--- BQ_Table_Building/build_clinical_data_program_tables.py
+++ BQ_Table_Building/build_clinical_data_program_tables.py
@@ -162,7 +162,7 @@ def main():
     todo: why did MMRF have follow_ups__molecular_tests in the nested list?
     """
 
-    program_name = "VAREPOP"
+    program_name = "MMRF"
     nested_name = "family_histories"
 
     cases, nested_key_set = flatten_case_json(program_name)
