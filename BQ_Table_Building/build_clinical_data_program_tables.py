96942f582967725e2380cd8a822bf8f3e9673985
laurenwolfe-isb <lwolfe@systemsbiology.org>
2020-04-25 00:14:24 -0700

    generate bq schema

--- BQ_Table_Building/build_clinical_data_program_tables.py
+++ BQ_Table_Building/build_clinical_data_program_tables.py
@@ -230,8 +230,11 @@ def create_bq_table_and_insert_rows(program_name, cases, schema_field_list, orde
 
 def create_bq_tables(program_name, bq_params, tables_dict):
     base_table_name = [bq_params["GDC_RELEASE"], 'clin', program_name]
+
     for table in tables_dict.keys():
         split_path = table.split(".")
+
+        table_name = base_table_name
 
         if len(split_path) > 1:
             table_name = base_table_name + split_path[1:]
