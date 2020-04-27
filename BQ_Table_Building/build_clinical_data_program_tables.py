b6fc35f3a189656cab2e1491eda4783b3badd0d4
laurenwolfe-isb <lwolfe@systemsbiology.org>
2020-04-27 13:17:52 -0700

    infer data types

--- BQ_Table_Building/build_clinical_data_program_tables.py
+++ BQ_Table_Building/build_clinical_data_program_tables.py
@@ -153,7 +153,7 @@ def lookup_column_types():
     results = get_query_results(query)
 
     for result in results:
-        print(result.data_type)
+        print(result.data_type[13:-2])
 
 
 def create_bq_tables(program_name, bq_params, table_hierarchy, cases):
