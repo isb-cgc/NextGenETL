28a587cc432025175e290c2fe704b8ed3d5b6d44
laurenwolfe-isb <lwolfe@systemsbiology.org>
2020-04-27 15:11:16 -0700

    infer data types

--- BQ_Table_Building/build_clinical_data_program_tables.py
+++ BQ_Table_Building/build_clinical_data_program_tables.py
@@ -238,18 +238,14 @@ def lookup_column_types():
         split_diagnoses = diagnoses.split('annotations ')
         if len(split_diagnoses) > 1:
             diagnoses = split_diagnoses[0]
-            annotations = split_diagnoses[1]
+            annotations = split_diagnoses[1][:-1]
+            treatments = treatments[:-2]
         else:
             split_treatments = treatments.split('annotations ')
-            treatments = split_treatments[0]
-            annotations = split_treatments[1]
+            treatments = split_treatments[0][:-1]
+            annotations = split_treatments[1][:-2]
 
-    diagnoses = diagnoses.strip(',')
-    diagnoses = diagnoses.strip('>')
-    treatments = treatments.strip(',')
-    treatments = treatments.strip('>')
-    annotations = annotations.strip(',')
-    annotations = annotations.strip('>')
+        diagnoses = diagnoses[:-1] + '>>'
 
     print(diagnoses)
     print(treatments)
