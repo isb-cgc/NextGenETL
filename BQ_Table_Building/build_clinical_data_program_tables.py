330d0024b18d73f5b9e9ae83e6312d7eeeb7ae8a
laurenwolfe-isb <lwolfe@systemsbiology.org>
2020-05-04 21:12:56 -0700

    bug fix for expand_field_group include

--- BQ_Table_Building/build_clinical_data_program_tables.py
+++ BQ_Table_Building/build_clinical_data_program_tables.py
@@ -560,8 +560,16 @@ def create_child_table_id_list(flattened_case_dict, parent_fg, child_fg):
 
     child_table = parent_table + '.' + child_fg
 
-    # remove pluralization from field group name to make id keys
-    parent_id_key = parent_fg[:-1] + 'id'
+    if parent_fg == 'diagnoses':
+        parent_id_key = 'diagnosis_id'
+    elif parent_fg[-2:] == 'es':
+        has_fatal_error("Added a new field group with an irregular pluralization, edit create_child_table_id_list().")
+    elif parent_fg[-1] == 's':
+        # remove pluralization from field group name to make id keys
+        parent_id_key = parent_fg[:-1] + '_id'
+    else:
+        parent_id_key = parent_fg + '_id'
+
     child_id_key = child_fg[:-1] + '_id'
     child_id_list_key = child_id_key + 's'
 
