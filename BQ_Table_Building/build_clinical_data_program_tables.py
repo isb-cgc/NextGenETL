bbe2bbd11e5989dec6bd4de501641bd2788785de
laurenwolfe-isb <lwolfe@systemsbiology.org>
2020-05-05 10:14:30 -0700

    testing

--- BQ_Table_Building/build_clinical_data_program_tables.py
+++ BQ_Table_Building/build_clinical_data_program_tables.py
@@ -719,12 +719,13 @@ def insert_case_data(cases, record_counts):
             print("2 len(flattened_case_dict['cases.follow_ups']) = {}".format(
                 len(flattened_case_dict['cases.follow_ups'])))
 
+        continue
         flattened_case_dict = merge_single_entry_field_groups(flattened_case_dict, table_keys)
 
         if 'cases.follow_ups' in flattened_case_dict:
             print("3 len(flattened_case_dict['cases.follow_ups']) = {}".format(
                 len(flattened_case_dict['cases.follow_ups'])))
-        continue
+
 
         if isinstance(flattened_case_dict['cases'], dict):
             flattened_case_dict['cases'] = [flattened_case_dict['cases']]
