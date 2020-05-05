e4fe82a695c24e563b29440aaa8249c2e629392e
laurenwolfe-isb <lwolfe@systemsbiology.org>
2020-05-04 17:38:36 -0700

    HCMI messed up now

--- BQ_Table_Building/build_clinical_data_program_tables.py
+++ BQ_Table_Building/build_clinical_data_program_tables.py
@@ -503,6 +503,7 @@ def insert_case_data(program_name, cases, table_names_dict):
     for case in cases:
         flattened_case_dict = flatten_case(case)
         if count == 0:
+            print(flattened_case_dict)
             print(flattened_case_dict.keys())
 
         for field_group_key in flattened_case_dict.copy():
