31ec0daca7810596cbf682d1980835ff2c6237a2
laurenwolfe-isb <lwolfe@systemsbiology.org>
2020-05-05 12:53:07 -0700

    Trying to insert the data into BQ

--- BQ_Table_Building/build_clinical_data_program_tables.py
+++ BQ_Table_Building/build_clinical_data_program_tables.py
@@ -725,10 +725,6 @@ def insert_case_data(cases, record_counts, tables_dict):
                 flattened_case_dict = create_child_table_id_list(flattened_case_dict, parent_fg, child_fg)
 
             table_id = tables_dict[table]
-
-            print("table_id: {}".format(table_id))
-            print("tables_d: {}".format(tables_dict))
-
             client = bigquery.Client()
             bq_table = client.get_table(table_id)
             client.insert_rows(bq_table, flattened_case_dict[table])
