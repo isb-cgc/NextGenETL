6ebcdcb0babe159dd0d99f842f6348cf79e61b2d
laurenwolfe-isb <lwolfe@systemsbiology.org>
2020-05-04 23:26:30 -0700

    ordered printing

--- BQ_Table_Building/build_clinical_data_program_tables.py
+++ BQ_Table_Building/build_clinical_data_program_tables.py
@@ -672,10 +672,12 @@ def ordered_print(flattened_case_dict, column_order_list):
             prefix += '__'
 
         for entry in flattened_case_dict[table]:
+            print("table: {}, entry: {}".format(table, entry))
             field_order_dict = dict()
 
             for key in entry:
                 col_order_lookup_key = prefix + key
+                print("key: {}, col_l_key: {}".format(key, col_order_lookup_key))
                 field_order_dict[key] = column_order_dict[col_order_lookup_key]
 
             for field_key, order in sorted(field_order_dict.items(), key=lambda item: item[1]):
