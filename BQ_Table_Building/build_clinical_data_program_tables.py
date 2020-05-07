b99eb9e4b896fd1a252c6e717986c8f3dece9b29
laurenwolfe-isb <lwolfe@systemsbiology.org>
2020-05-07 10:29:53 -0700

    frequency counts for one-to-many

--- BQ_Table_Building/build_clinical_data_program_tables.py
+++ BQ_Table_Building/build_clinical_data_program_tables.py
@@ -928,7 +928,7 @@ def main(args):
     }
 
     # program_names = get_programs_list(params)
-    program_names = ['VAREPOP', 'CGCI']
+    program_names = ['VAREPOP', 'CGCI']
 
     global COLUMN_ORDER_DICT
     COLUMN_ORDER_DICT = import_column_order(args[2])
