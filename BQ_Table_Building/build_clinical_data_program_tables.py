4479b9fd86fdd3d84a21dc3ee0b0538eb0be13d4
laurenwolfe-isb <lwolfe@systemsbiology.org>
2020-05-06 18:07:16 -0700

    missing field group from remove fields

--- BQ_Table_Building/build_clinical_data_program_tables.py
+++ BQ_Table_Building/build_clinical_data_program_tables.py
@@ -86,16 +86,14 @@ def retrieve_program_case_structure(program_name, cases, params):
         for field_key in case_:
             # Hits for cases
             if isinstance(case_[field_key], dict):
-                tables_[parent_path].add(field_key)
+                for key in case_[field_key]:
+                    tables_[parent_path].add(key)
             else:
-                # at this point, the field_key references a dict or list
-                nested_path = parent_path + '.' + field_key
-
-                record_counts_[nested_path] = max(record_counts_[nested_path], len(case_[field_key]))
+                record_counts_[parent_path] = max(record_counts_[parent_path], len(case_[field_key]))
 
                 for field_group_entry in case_[field_key]:
-                    tables_, record_counts_ = build_case_structure(
-                        tables_, field_group_entry, record_counts_, nested_path)
+                    nested_path = parent_path + '.' + field_key
+                    tables_, record_counts_ = build_case_structure(tables_, field_group_entry, record_counts_, nested_path)
 
         return tables_, record_counts_
 
