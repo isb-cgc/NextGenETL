02067d737f96115e082f192f9defba37a363a061
laurenwolfe-isb <lwolfe@systemsbiology.org>
2020-05-07 06:06:40 -0700

    Adding record counts for one-to-many tables

--- BQ_Table_Building/build_clinical_data_program_tables.py
+++ BQ_Table_Building/build_clinical_data_program_tables.py
@@ -739,8 +739,11 @@ def insert_case_data(cases, record_counts, tables_dict, params):
                 start_idx = end_idx
                 end_idx += batch_size
 
+
             # insert the last set of records
             sub_list = insert_lists[table][start_idx:]
+            if table == 'cases':
+                print(sub_list)
             client.insert_rows(bq_table, sub_list)
             print("{} records inserted.".format(len(insert_lists[table]), table))
         except exceptions.BadRequest as err:
