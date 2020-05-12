41c50e46750de315efdb51f313dfddfb95b44609
laurenwolfe-isb <lwolfe@systemsbiology.org>
2020-05-12 10:01:21 -0700

    testing

--- BQ_Table_Building/build_clinical_data_program_tables.py
+++ BQ_Table_Building/build_clinical_data_program_tables.py
@@ -1102,13 +1102,15 @@ def test_table_output(params):
 
 
 def get_main_table_count(table, max_count_entry, params):
-    parent_table = get_parent_field_group(table)
-    entry_id_key = get_table_id_key(parent_table, params)
+    table_fg = convert_bq_table_id_to_fg(table_fg)
+    parent_table = get_parent_field_group(table_fg)
+    entry_id_key = get_table_id_key(table_fg, params)
+    parent_id_key = get_table_id_key(parent_table, params)
     entry_id = max_count_entry[entry_id_key]
 
 
 
-    print("id type key: {} max count id: {}".format(entry_id_key, entry_id))
+    print("id type key: {} max count id: {}".format(parent_id_key, entry_id))
 
     return None
 
