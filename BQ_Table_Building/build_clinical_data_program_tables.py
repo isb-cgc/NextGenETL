25bc1a3732f8a12b8ae62f76cab604cdf4880c6c
laurenwolfe-isb <lwolfe@systemsbiology.org>
2020-05-12 12:01:38 -0700

    testing

--- BQ_Table_Building/build_clinical_data_program_tables.py
+++ BQ_Table_Building/build_clinical_data_program_tables.py
@@ -1077,6 +1077,9 @@ def test_table_output(params):
             table_id_key = get_table_id_key(table_fg, params)
             table_field = get_field_name(table_fg)
 
+            parent_fg = get_parent_field_group(table_fg)
+            parent_fg_id_key = get_table_id_key(parent_fg, params)
+
             parent_table_fg = get_parent_table(table_fg_list, table_fg)
             parent_id_key = get_table_id_key(parent_table_fg, params)
             parent_field_name = get_field_name(parent_table_fg)
@@ -1091,7 +1094,7 @@ def test_table_output(params):
             max_count, max_count_id = get_max_count(record_count_list)
 
             mt_case_id, mt_child_id, mt_max_count = get_main_table_count(params, program_name, table_id_key,
-                                                   table_field, parent_id_key, parent_field_name)
+                                                   table_field, parent_fg_id_key, parent_fg)
 
             print("case_id: {}, child_id: {}: max_count: {}".format(mt_case_id, mt_child_id, mt_max_count))
 
