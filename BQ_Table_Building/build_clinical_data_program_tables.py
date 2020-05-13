44ef2ae501fc61a5f90cf4f3c688ab60cc3f2f6c
laurenwolfe-isb <lwolfe@systemsbiology.org>
2020-05-13 05:41:39 -0700

    okay! think that'll do it for this reference column over insertion

--- BQ_Table_Building/build_clinical_data_program_tables.py
+++ BQ_Table_Building/build_clinical_data_program_tables.py
@@ -798,10 +798,12 @@ def merge_single_entry_field_groups(flattened_case_dict, table_keys, bq_program_
             idx = 0
             for entry in flattened_case_dict[parent_table].copy():
                 try:
+                    print("Is list: {}".format(isinstance(entry, list)))
+                    print("Is dict: {}".format(isinstance(entry, dict)))
                     entry_id = entry[parent_id_column]
                 except KeyError as err:
-                    print('FLATTENED at EXCEPTION {}\n\n{}'.format(flattened_case_dict, err))
-                if record_count_dict not in record_count_dict:
+                    has_fatal_error('FLATTENED at EXCEPTION {}\n\n{}'.format(flattened_case_dict, err))
+                if entry_id not in record_count_dict:
                     record_count_dict[entry_id] = {'entry_idx': idx, 'record_count': 0}
                     idx += 1
 
