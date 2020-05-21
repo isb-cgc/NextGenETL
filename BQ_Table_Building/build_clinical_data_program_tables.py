b334a884fc1a3be5b3f8863295bb0faf40ebe8a1
laurenwolfe-isb <lwolfe@systemsbiology.org>
2020-05-21 01:54:06 -0700

    why aren't fields being removed?

--- BQ_Table_Building/build_clinical_data_program_tables.py
+++ BQ_Table_Building/build_clinical_data_program_tables.py
@@ -747,10 +747,9 @@ def create_and_load_tables(program_name, cases, schemas, tables):
     print("\nInserting case records...")
     for case in cases:
         flattened_case_dict = flatten_case(case)
+        flattened_case_dict = merge_single_entry_field_groups(flattened_case_dict, tables)
         print(flattened_case_dict)
         exit()
-
-        flattened_case_dict = merge_single_entry_field_groups(flattened_case_dict, tables)
 
         for table in flattened_case_dict.keys():
             if table not in tables:
