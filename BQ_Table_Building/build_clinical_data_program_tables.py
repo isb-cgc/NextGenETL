5a111ab36cec4dfa4fc6c2ad135877aa2632b581
laurenwolfe-isb <lwolfe@systemsbiology.org>
2020-05-21 06:38:29 -0700

    mol_test count not populating when case has no follow_ups entries with mol_tests

--- BQ_Table_Building/build_clinical_data_program_tables.py
+++ BQ_Table_Building/build_clinical_data_program_tables.py
@@ -628,8 +628,8 @@ def flatten_case_entry(record, field_group, flat_case, case_id, pid, pid_field, 
                     tables=tables)
 
                 record_count = len(field_val)
-                parent_table = field_group + '.' + field
-                count_col = get_bq_name(API_PARAMS, field, parent_table)
+                parent_table = get_parent_table(tables, field_group + '.' + field)
+                count_col = get_bq_name(API_PARAMS, field, 'count')
 
                 # todo delete print
                 print("record_count: {}".format(record_count))
