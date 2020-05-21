08d8de9888c22a02a97b598ac02438a39ff61d68
laurenwolfe-isb <lwolfe@systemsbiology.org>
2020-05-21 10:49:45 -0700

    mol_test count not populating when case has no follow_ups entries with mol_tests

--- BQ_Table_Building/build_clinical_data_program_tables.py
+++ BQ_Table_Building/build_clinical_data_program_tables.py
@@ -752,12 +752,34 @@ def merge_single_entry_field_groups(flattened_case, bq_program_tables):
     return flattened_case
 
 
+def collect_ids(entry, curr_idx, fg, ids):
+    split_fg = fg.split('.')
+
+    if curr_idx < 0 or curr_idx >= len(split_fg):
+        has_fatal_error("invalid index returned in collect_ids: {}".format(curr_idx))
+    elif curr_idx != len(split_fg) - 1:
+        for child_entry in entry[split_fg[curr_idx]]:
+            ids = collect_ids(child_entry, curr_idx + 1, fg, ids)
+    else:
+        for child_entry in entry[split_fg[curr_idx]]:
+            id_field = get_table_id_key(fg)
+            ids.add(child_entry[id_field])
+    return ids
+
+
+def get_field_group_ids(flattened_case, field_group):
+    return collect_ids(entry=flattened_case, curr_idx=0, fg=field_group, ids=set())
+
+
 def assign_record_counts(flattened_case, tables, case_id_counts):
     fg_depths = {fg: get_field_depth(fg) for fg in flattened_case.keys()}
+    tables_depths = {fg: get_field_depth(fg) for fg in tables}
 
     # todo, start from children or parent?
     fg_entry_counts = dict()
     for fg, depth in sorted(fg_depths.items(), key=lambda i: i[1]):
+        print("get_fg_ids: {}".format(get_field_group_ids(flattened_case, fg)))
+
         if depth == 1:
             case_id = flattened_case[fg][0]['case_id']
             continue
@@ -771,12 +793,7 @@ def assign_record_counts(flattened_case, tables, case_id_counts):
         # used to insert the count into parent table
         parent_table = get_parent_table(tables, fg)
 
-        # todo delete print
-        print("parent_table: {}".format(parent_table))
         parent_fg_id_column = get_bq_name(API_PARAMS, parent_fg_id_field, parent_table)
-
-        # todo delete print
-        print("parent_fg_id_column: {}".format(parent_fg_id_column))
 
         fg_ids = dict()
 
@@ -788,6 +805,29 @@ def assign_record_counts(flattened_case, tables, case_id_counts):
                 fg_ids[parent_fg_id] += 1
 
         fg_entry_counts[fg] = fg_ids
+
+    for table, depth in sorted(tables_depths.items(), key=lambda i: i[1]):
+        if table == 'cases':
+            continue
+        # if none of the records have entries for a child field group, add zero counts
+        if table not in fg_entry_counts:
+            parent_field_group = get_parent_field_group(table)
+            parent_fg_table_id_key = get_table_id_key(parent_field_group)
+
+            # used to create the ancestor's id field
+            parent_fg_id_field = get_bq_name(API_PARAMS, parent_fg_table_id_key)
+            # used to insert the count into parent table
+            parent_table = get_parent_table(tables, table)
+
+            fg_ids = dict()
+
+            for id in fg_entry_counts[parent_table]:
+                fg_ids[id] = 0
+
+            fg_entry_counts[table] = fg_ids
+
+        else:
+            table_ids = {id for id in fg_entry_counts[table]}
 
     case_id_counts[case_id] = fg_entry_counts
 
