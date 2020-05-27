123fec1c7b7f07cdeaa6ca0d0b662f19b3505460
laurenwolfe-isb <lwolfe@systemsbiology.org>
2020-05-27 09:47:08 -0700

    generate docs

--- gdc_clinical_resources/generate_docs.py
+++ gdc_clinical_resources/generate_docs.py
@@ -85,17 +85,24 @@ def get_table_list_for_curr_release(api_params, bq_params):
 
 def style_table_entry(table_name, table_json_attr):
     unit, size = convert_bytes_to_largest_unit(table_json_attr['numBytes'])
-    print("\t{} - {} {}".format(table_name, size, unit))
+    # print("\t{} - {} {}".format(table_name, size, unit))
 
 
 def generate_docs(api_params, bq_params):
     program_tables_json = get_table_list_for_curr_release(api_params, bq_params)
 
+    print('single tables')
     for program, tables in program_tables_json.items():
-        if len(tables) > 1:
+        if len(tables) == 1:
             print(program)
             for table, table_attrs in tables.items():
                 style_table_entry(table, table_attrs)
+
+    print('multiple tables')
+    for program, tables in program_tables_json.items():
+        if len(tables) > 1:
+            print(program)
+
 
 """
 
