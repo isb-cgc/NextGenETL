e15f8e934e111ca2ea010b39c82276a35abb5ff9
lhagen-isb <50964829+lhagen-isb@users.noreply.github.com>
2020-05-27 09:27:30 -0700

    created files to move tables in BigQuery

---
+++ BQ_Table_Building/build_generic_table.py
@@ -0,0 +1,99 @@
+"""
+
+Copyright 2019, Institute for Systems Biology
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+"""
+
+'''
+Make sure the VM has BigQuery and Storage Read/Write permissions!
+'''
+
+
+import sys
+import os
+from os.path import expanduser
+import yaml
+import io
+from json import loads as json_loads
+
+from common_etl.support import confirm_google_vm, publish_table
+
+
+'''
+----------------------------------------------------------------------------------------------
+The configuration reader. Parses the YAML configuration into dictionaries
+'''
+def load_config(yaml_config):
+    yaml_dict = None
+    config_stream = io.StringIO(yaml_config)
+    try:
+        yaml_dict = yaml.load(config_stream, Loader=yaml.FullLoader)
+    except yaml.YAMLError as ex:
+        print(ex)
+
+    if yaml_dict is None:
+        return None, None, None
+
+    return yaml_dict['files_and_buckets_and_tables'], yaml_dict['bq_filters'], yaml_dict['steps']
+
+
+'''
+----------------------------------------------------------------------------------------------
+
+Main Control Flow
+Note that the actual steps run are configured in the YAML input! This allows you
+to e.g. skip previously run steps.
+'''
+def main(args):
+
+    if not confirm_google_vm():
+        print('This job needs to run on a Google Cloud Compute Engine to avoid storage egress charges [EXITING]')
+        return
+
+
+    if len(args) != 2:
+        print(" ")
+        print(" Usage : {} <configuration_yaml>".format(args[0]))
+        return
+
+    print('job started')
+
+    #
+    # Get the YAML config loaded:
+    #
+
+    with open(args[1], mode='r') as yaml_file:
+        params, bq_filters, steps = load_config(yaml_file.read())
+
+    #
+    # publish table:
+    #
+
+    if 'publish' in steps:
+
+        source_table = '{}.{}.{}'.format(params['WORKING_PROJECT'], params['TARGET_DATASET'],
+                                         params['FINAL_TARGET_TABLE'])
+        publication_dest = '{}.{}.{}'.format(params['PUBLICATION_PROJECT'], params['PUBLICATION_DATASET'],
+                                             params['PUBLICATION_TABLE'])
+
+        success = publish_table(source_table, publication_dest)
+
+        if not success:
+            print("publish table failed")
+            return
+    print('job completed')
+
+if __name__ == "__main__":
+    main(sys.argv)
