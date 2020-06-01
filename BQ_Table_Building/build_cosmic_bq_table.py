9c630246837c85be739567125812a210aa79bba0
lhagen-isb <50964829+lhagen-isb@users.noreply.github.com>
2020-06-01 15:29:16 -0700

    Create build_cosmic_bq_table.py

---
+++ BQ_Table_Building/build_cosmic_bq_table.py
@@ -0,0 +1,123 @@
+"""
+
+Copyright 2019, Institute for Systems Biology
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+"""
+
+'''
+Make sure the VM has BigQuery and Storage Read/Write permissions!
+'''
+
+
+import sys
+import os
+from os.path import expanduser
+import yaml
+import io
+from git import Repo
+from json import loads as json_loads
+from createSchemaP3 import build_schema
+
+from common_etl.support import create_clean_target, bucket_to_local
+
+'''
+----------------------------------------------------------------------------------------------
+The configuration reader. Parses the YAML configuration into dictionaries
+'''
+def load_config(yaml_config):
+    yaml_dict = None
+    config_stream = io.StringIO(yaml_config)
+    try:
+        yaml_dict = yaml.load(config_stream, Loader=yaml.FullLoader)
+    except yaml.YAMLError as ex:
+        print(ex)
+
+    if yaml_dict is None:
+        return None, None, None
+
+    return yaml_dict['files_and_buckets_and_tables'], yaml_dict['steps']
+
+
+'''
+----------------------------------------------------------------------------------------------
+Build Pull List from TXT
+'''
+
+def build_pull_list_from_txt(bucket_location, file):
+
+
+'''
+----------------------------------------------------------------------------------------------
+Download files
+'''
+
+'''
+Fix column names
+'''
+
+'''
+----------------------------------------------------------------------------------------------
+Main Control Flow
+Note that the actual steps run are configured in the YAML input! This allows you
+to e.g. skip previously run steps.
+'''
+
+def main(args):
+
+    if not confirm_google_vm():
+        print('This job needs to run on a Google Cloud Compute Engine to avoid storage egress charges [EXITING]')
+        return
+
+
+    if len(args) != 2:
+        print(" ")
+        print(" Usage : {} <configuration_yaml>".format(args[0]))
+        return
+
+    print('job started')
+
+    #
+    # Get the YAML config loaded:
+    #
+
+    with open(args[1], mode='r') as yaml_file:
+        params, bq_filters, steps = load_config(yaml_file.read())
+
+
+    #
+    # BQ does not like to be given paths that have "~". So make all local paths absolute:
+    #
+
+    home = expanduser("~")
+    local_files_dir = "{}/{}".format(home, params['LOCAL_FILES_DIR'])
+    manifest_file = "{}/{}".format(home, params['MANIFEST_FILE'])
+    local_pull_list = "{}/{}".format(home, params['LOCAL_PULL_LIST'])
+    file_traversal_list = "{}/{}".format(home, params['FILE_TRAVERSAL_LIST'])
+    hold_schema_dict = "{}/{}".format(home, params['HOLD_SCHEMA_DICT'])
+    hold_schema_list = "{}/{}".format(home, params['HOLD_SCHEMA_LIST'])
+
+    if 'clear_target_directory' in steps:
+        print('clear_target_directory')
+        create_clean_target(local_files_dir)
+
+    if 'build_pull_list' in steps:
+        print('build_pull_list')
+        bucket_location = '{}.{}.{}'
+
+        success =
+
+        if not success:
+            print("Build pull list failed")
+            return
