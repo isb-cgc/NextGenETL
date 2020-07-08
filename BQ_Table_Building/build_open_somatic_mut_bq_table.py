19d9b60841281125c0028f418885793da6add103
lhagen-isb <50964829+lhagen-isb@users.noreply.github.com>
2020-07-08 12:04:11 -0700

    Update build_open_somatic_mut_bq_table.py

--- BQ_Table_Building/build_open_somatic_mut_bq_table.py
+++ BQ_Table_Building/build_open_somatic_mut_bq_table.py
@@ -384,7 +384,7 @@ def main(args):
     
     if 'build_traversal_list' in steps:
         all_files = build_file_list(local_files_dir)
-        program_list = build_program_list(all_files, params['PROGRAM'])
+        #program_list = build_program_list(all_files, params['PROGRAM'])
         if not check_caller_list(all_files, callers, params['PROGRAM']):
             print("Unexpected caller mismatch! Expecting {}".format(callers))
             return
