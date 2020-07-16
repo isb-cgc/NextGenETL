980077f07c4dff208c1ee125e265c5c1795854a0
lhagen-isb <50964829+lhagen-isb@users.noreply.github.com>
2020-07-16 15:27:28 -0700

    Update build_cnvr_bq_table.py

--- BQ_Table_Building/build_cnvr_bq_table.py
+++ BQ_Table_Building/build_cnvr_bq_table.py
@@ -240,7 +240,7 @@ def main(args):
         print('concat_all_files')
         with open(file_traversal_list, mode='r') as traversal_list_file:
             all_files = traversal_list_file.read().splitlines()
-        concat_all_files(all_files, one_big_tsv)
+        concat_all_files(all_files, one_big_tsv, params['PROGRAM'])
 
     #
     # Schemas and table descriptions are maintained in the github repo:
