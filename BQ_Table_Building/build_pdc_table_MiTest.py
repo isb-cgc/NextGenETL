41ab0b2acec0b75e48b6716e0275edd4e62b97c8
mitianisb <mtian@systemsbiology.org>
2020-08-09 16:15:09 -0700

    Convert quant matrix to table

--- BQ_Table_Building/build_pdc_table_MiTest.py
+++ BQ_Table_Building/build_pdc_table_MiTest.py
@@ -1,41 +1,16 @@
 import requests
 import json
+import numpy
 
 # use dict [study_id:aliquot_submitter_id -> aliquot_id]
-# use dict [aliquot_id -> gdc_case_id]
+# use dict [aliquot_id -> case_id]
 GLOBAL_STUDY_ID_ALIQUOT_SUBMITTER_ID_TO_ALIQUOT_ID_DICT = {}
-GLOBAL_ALIQUOT_ID_TO_GDC_CASE_ID_DICT = {}
+GLOBAL_ALIQUOT_ID_TO_CASE_ID_DICT = {}
 PDC_END_POINT = 'https://pdc.cancer.gov/graphql'
 
 # Useful functions in Ron's code
 # query_to_get_biospecimenData_on_one_study
 # GLOBAL_DICT_ALIQUOT_ID_TO_CASE_ID
-
-def build_mapping_dicts_one_study(study_id):
-    query = '{ biospecimenPerStudy(study_id: "' + \
-            study_id + '"' + \
-            ') { aliquot_id sample_id case_id aliquot_submitter_id sample_submitter_id case_submitter_id aliquot_status' \
-            ' case_status sample_status project_name sample_type disease_type primary_site pool taxon} }'
-    
-    response = requests.post(PDC_END_POINT, json={'query': query})
-
-    if response.ok:
-        json_res = response.json()
-
-        if 'errors' in json_res:
-            print('Cannot build mapping dicts for study = ' + study_id)
-        else:
-            biospecimen_per_study_list = json_res[u'data'][u'biospecimenPerStudy']
-
-            for i in range(len(biospecimen_per_study_list)):
-                aliquot_id = biospecimen_per_study_list[i][u'aliquot_id']
-                gdc_case_id = biospecimen_per_study_list[i][u'case_id']
-                study_id_aliquot_submitter_id = study_id + ":" + biospecimen_per_study_list[i][u'aliquot_submitter_id']
-                GLOBAL_STUDY_ID_ALIQUOT_SUBMITTER_ID_TO_ALIQUOT_ID_DICT[study_id_aliquot_submitter_id] = aliquot_id
-                GLOBAL_ALIQUOT_ID_TO_GDC_CASE_ID_DICT[aliquot_id] = gdc_case_id
-
-    return
-
 
 def pull_quant_matrix_one_study(study_submitter_id):
     quant_log2_ratio_query = ('{ quantDataMatrix(study_submitter_id: \"'
@@ -54,42 +29,95 @@ def pull_quant_matrix_one_study(study_submitter_id):
             return json_res[u'data'][u'quantDataMatrix']
 
 
-def map_quant_matrix_one_study(study_id, quant_matrix):
+def replace_first_row_to_study_id_and_aliquot_submitter_id(study_id, quant_matrix):
     first_row_data = quant_matrix[0]
-    aliquot_id_row = []
-    aliquot_id_row.append(u'Aliquot ID')
-    gdc_case_id_row = []
-    gdc_case_id_row.append(u'GDC Case ID')
     for i in range(1, len(first_row_data)):
-        aliquot_submitter_id = first_row_data[i].split(":")[1]
-        study_id_and_aliquot_submitter_id = study_id + ':' + aliquot_submitter_id
-        aliquot_id = GLOBAL_STUDY_ID_ALIQUOT_SUBMITTER_ID_TO_ALIQUOT_ID_DICT.get(study_id_and_aliquot_submitter_id,
-                                                                                 None)
-        if aliquot_id:
-            gdc_case_id = GLOBAL_ALIQUOT_ID_TO_GDC_CASE_ID_DICT.get(aliquot_id, None)
+        if ":" in first_row_data[i]:
+            aliquot_submitter_id = first_row_data[i].split(":")[1]
         else:
-            gdc_case_id = None
-        aliquot_id_row.append(aliquot_id)
-        gdc_case_id_row.append(gdc_case_id)
-    quant_matrix.insert(0, aliquot_id_row)
-    quant_matrix.insert(0, gdc_case_id_row)
-    print ("Test")
+            print('no : in here ' + first_row_data[i])
+            aliquot_submitter_id = first_row_data[i]
+        study_id_and_aliquot_submitter_id = study_id + ':' + aliquot_submitter_id
+        quant_matrix[0][i] = study_id_and_aliquot_submitter_id
+    return quant_matrix
 
 
-def generate_quant_matrix_table_one_study(quant_matrix, study_id):
-    # TODO: get all study metadata for the study
+def convert_quant_matrix_to_table(quant_matrix):
+    num_rows = len(quant_matrix)
+    num_cols = len(quant_matrix[0])
+    table = []
+    table.append(['study_id:aliquot_submitter_id', 'gene', 'log2_ratio'])
+    for i in range(1, num_rows):
+        for j in range(1, num_cols):
+            log2_value = quant_matrix[i][j]
+            gene = quant_matrix[i][0]
+            study_id_and_aliquot_submitter_id = quant_matrix[0][j]
+            table.append([study_id_and_aliquot_submitter_id, gene, log2_value])
+    return table
+
+
+def build_mapping_dicts_one_study(study_id):
+    query = '{ biospecimenPerStudy(study_id: "' + \
+            study_id + '"' + \
+            ') { aliquot_id sample_id case_id aliquot_submitter_id sample_submitter_id case_submitter_id aliquot_status' \
+            ' case_status sample_status project_name sample_type disease_type primary_site pool taxon} }'
+
+    response = requests.post(PDC_END_POINT, json={'query': query})
+
+    if response.ok:
+        json_res = response.json()
+
+        if 'errors' in json_res:
+            print('Cannot build mapping dicts for study = ' + study_id)
+        else:
+            biospecimen_per_study_list = json_res[u'data'][u'biospecimenPerStudy']
+
+            for i in range(len(biospecimen_per_study_list)):
+                aliquot_id = biospecimen_per_study_list[i][u'aliquot_id']
+                # not sure if this case_id is GDC case id or PDC case id
+                case_id = biospecimen_per_study_list[i][u'case_id']
+                study_id_aliquot_submitter_id = study_id + ":" + biospecimen_per_study_list[i][u'aliquot_submitter_id']
+                GLOBAL_STUDY_ID_ALIQUOT_SUBMITTER_ID_TO_ALIQUOT_ID_DICT[study_id_aliquot_submitter_id] = aliquot_id
+                GLOBAL_ALIQUOT_ID_TO_CASE_ID_DICT[aliquot_id] = case_id
     return
 
 
-def pull_and_map_quant_matrix_one_study(study_id, study_submitter_id):
+# get aliquot id and case id from two global dicts
+def map_quant_table_one_study(quant_table):
+    quant_table[0].append('aliquot_id')
+    quant_table[0].append('case_id')
+    for i in range(1, len(quant_table)):
+        study_id_and_aliquot_submitter_id = quant_table[i][0]
+        aliquot_id = GLOBAL_STUDY_ID_ALIQUOT_SUBMITTER_ID_TO_ALIQUOT_ID_DICT.get(study_id_and_aliquot_submitter_id,
+                                                                                 None)
+        if aliquot_id:
+            case_id = GLOBAL_ALIQUOT_ID_TO_CASE_ID_DICT.get(aliquot_id, None)
+        else:
+            case_id = None
+
+        quant_table[i].append(aliquot_id)
+        quant_table[i].append(case_id)
+
+    return quant_table
+
+
+def generate_quant_matrix_table_one_study(study_id, study_submitter_id):
+    print("Test generate_quant_matrix_table_one_study")
     my_quant_matrix = pull_quant_matrix_one_study(study_submitter_id)
     if my_quant_matrix:
-        build_mapping_dicts_one_study(study_id)
-        my_quant_matrix = map_quant_matrix_one_study(study_id, my_quant_matrix)
-        my_quant_matrix_table = generate_quant_matrix_table_one_study(my_quant_matrix, study_id)
-        print("Test my_quant_matrix_table")
+        my_quant_matrix = replace_first_row_to_study_id_and_aliquot_submitter_id(study_id, my_quant_matrix)
+        my_quant_table = convert_quant_matrix_to_table(my_quant_matrix)
 
-def pull_and_map_quant_matrix_all_studies():
+        # Optional: map study_id_aliquot_submitter_id to two new rows of case_id and aliquot_id
+        build_mapping_dicts_one_study(study_id)
+        my_quant_table = map_quant_table_one_study(my_quant_table)
+
+        return my_quant_table
+    else:
+        return None
+
+
+def generate_quant_matrix_table_all_studies():
     all_progs_query = """{allPrograms{
         program_id
         program_submitter_id
@@ -139,10 +167,14 @@ def pull_and_map_quant_matrix_all_studies():
         quant_matrix = pull_quant_matrix_one_study(submitter_id)
 
         if quant_matrix:
-            build_mapping_dicts_one_study(study_id)
-            quant_matrix = map_quant_matrix_one_study(quant_matrix)
-            quant_matrix_table = generate_quant_matrix_table_one_study(quant_matrix, study_id)
-            study['quant_res'] = quant_matrix
+            quant_matrix = replace_first_row_to_study_id_and_aliquot_submitter_id(study_id, quant_matrix)
+            quant_table = convert_quant_matrix_to_table(quant_matrix)
+
+            # Optional: map study_id_aliquot_submitter_id to two new rows of case_id and aliquot_id
+            # build_mapping_dicts_one_study(study_id)
+            # quant_table = map_quant_table_one_study(study_id, quant_table)
+
+            # study['quant_res'] = quant_table
             study_sub_no_quant.append(submitter_id)
         else:
             study_sub_has_quant.append(submitter_id)
@@ -157,7 +189,9 @@ def main():
     # single hard-coded study test...
     my_study_id = u'96296fd1-89a4-11ea-b1fd-0aad30af8a83'
     my_study_submitter_id = u'CPTAC HNSCC Discovery Study - Proteome'
-    pull_and_map_quant_matrix_one_study(my_study_id, my_study_submitter_id)
+    generate_quant_matrix_table_one_study(my_study_id, my_study_submitter_id)
+
+
 
 
 if __name__ == '__main__':
