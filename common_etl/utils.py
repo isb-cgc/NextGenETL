65309eba5199f04efff64656c88f2d270ada9422
laurenwolfe-isb <lwolfe@systemsbiology.org>
2020-08-11 15:50:42 -0700

    Current branch state

--- common_etl/utils.py
+++ common_etl/utils.py
@@ -439,6 +439,8 @@ def create_and_load_table(bq_params, jsonl_rows_file, schema, table_name):
     table_id = (bq_params['WORKING_PROJECT'] + '.' + bq_params['TARGET_DATASET'] + '.' +
                 table_name)
 
+    print(schema)
+
     try:
         load_job = client.load_table_from_uri(gs_uri, table_id, job_config=job_config)
 
