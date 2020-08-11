89ccead7eb7d17352668680d223ed011339500c2
laurenwolfe-isb <lwolfe@systemsbiology.org>
2020-08-11 14:51:08 -0700

    Current branch state

--- BQ_Table_Building/build_pdc_tables.py
+++ BQ_Table_Building/build_pdc_tables.py
@@ -1,3 +1,25 @@
+"""
+Copyright 2020, Institute for Systems Biology
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+
 import requests
 import json
 
@@ -94,6 +116,9 @@ def main():
 
     study_sub_has_quant = []
     study_sub_no_quant = []
+
+    print(study_dict)
+    return
 
     for study in studies:
         submitter_id = study['study_submitter_id']
