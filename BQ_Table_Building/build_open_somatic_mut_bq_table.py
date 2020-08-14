12b0e4390777a6fa0729bd720a6d6bc4deb16f70
lhagen-isb <50964829+lhagen-isb@users.noreply.github.com>
2020-08-14 15:10:46 -0700

    Creating schema from generic json file

--- BQ_Table_Building/build_open_somatic_mut_bq_table.py
+++ BQ_Table_Building/build_open_somatic_mut_bq_table.py
@@ -66,7 +66,7 @@ def load_config(yaml_config):
         print(ex)
 
     if yaml_dict is None:
-        return None, None, None, None, None, None, None
+        return None, None, None, None, None, None, None, None
 
     return (yaml_dict['files_and_buckets_and_tables'], yaml_dict['filters'], yaml_dict['bq_filters'], 
             yaml_dict['steps'], yaml_dict['extra_fields'], yaml_dict['key_fields'], yaml_dict['callers'],
