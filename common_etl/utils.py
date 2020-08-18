26102f5f39f2c9397dcd3640a1c42b1afb03fb88
laurenwolfe-isb <lwolfe@systemsbiology.org>
2020-08-18 00:27:34 -0700

    add custom field descriptions

--- common_etl/utils.py
+++ common_etl/utils.py
@@ -571,7 +571,8 @@ def update_bq_table(table_name, metadata):
 def get_table_schema(table_name):
     table = get_bq_table(table_name)
 
-    return table.schema.to_api_repr()
+    for schema_field in table.schema:
+        print(schema_field.to_api_repr())
 
 
 def update_table_schema(bq_params, table_name):
