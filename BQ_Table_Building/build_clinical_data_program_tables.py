46204559675d5423a43e80ef0c0acfe17e8b10da
laurenwolfe-isb <lwolfe@systemsbiology.org>
2020-08-18 16:37:21 -0700

    Create biospecimen stub table

--- BQ_Table_Building/build_clinical_data_program_tables.py
+++ BQ_Table_Building/build_clinical_data_program_tables.py
@@ -24,7 +24,6 @@ import sys
 import json
 import os
 import time
-import pandas
 # from gdc_clinical_resources.test_data_integrity import *
 from common_etl.utils import *
 from gdc_clinical_resources.generate_docs import (generate_docs)
