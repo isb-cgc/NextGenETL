1b13178d081df6d8d472fb9c534c08be3a193c78
laurenwolfe-isb <lwolfe@systemsbiology.org>
2020-08-24 14:01:46 -0700

    exclude unneeded fields from schema

--- common_etl/utils.py
+++ common_etl/utils.py
@@ -681,12 +681,12 @@ def modify_schema_for_webapp(schema, api_params):
         for fg in api_params['WEBAPP_EXCLUDED_FG']:
             exclude_fgs.add(fg)
 
-    print(exclude_fgs)
-
     fields = schema.copy().keys()
 
     for field in fields:
         parent_fg = ".".join(field.split('.')[:-1])
+
+        print(parent_fg)
 
         if parent_fg in exclude_fgs or field in exclude_fields:
             schema.pop(field)
