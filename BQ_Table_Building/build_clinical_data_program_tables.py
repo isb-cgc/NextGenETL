b92ac482699727da7686dffc4f6272b98db87ddc
laurenwolfe-isb <lwolfe@systemsbiology.org>
2020-08-25 13:43:23 -0700

    merged_orders is coming out good, now for the case data

--- BQ_Table_Building/build_clinical_data_program_tables.py
+++ BQ_Table_Building/build_clinical_data_program_tables.py
@@ -545,18 +545,8 @@ def remove_null_fields(table_columns, merged_orders):
         merged_orders_set = merged_orders[table].keys()
         null_fields_set = merged_orders_set - table_cols_set
 
-        """
-        print(merged_orders_set)
-        print()
-        print()
-        print(table_cols_set)
-        print()
-        print()
-        """
-
         for field in null_fields_set:
             merged_orders[table].pop(field)
-            print(field)
 
 
 def create_app_schema_lists(schema, record_counts, merged_orders):
