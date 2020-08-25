31e694d40d95b06b02ea8027d9b7603aa57f621c
laurenwolfe-isb <lwolfe@systemsbiology.org>
2020-08-24 18:30:53 -0700

    create webapp table schemas

--- BQ_Table_Building/build_clinical_data_program_tables.py
+++ BQ_Table_Building/build_clinical_data_program_tables.py
@@ -839,14 +839,15 @@ def create_and_load_tables(program_name, cases, schemas, record_counts, is_webap
             os.remove(jsonl_file_path)
 
     for case in cases:
-        print(case)
         rename_case_fields(case, API_PARAMS)
+
+        print(case)
 
         flattened_case = flatten_case(case, is_webapp)
 
         merge_or_count_records(flattened_case, record_counts, is_webapp)
 
-        print(flattened_case)
+        # print(flattened_case)
 
         for table in flattened_case.keys():
             if table not in tables:
