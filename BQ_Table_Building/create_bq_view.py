90ae1258a0d70a7fd13e98e6203fdaa2c12a6633
lhagen-isb <50964829+lhagen-isb@users.noreply.github.com>
2020-08-25 16:27:49 -0700

    updated print message

--- BQ_Table_Building/create_bq_view.py
+++ BQ_Table_Building/create_bq_view.py
@@ -130,7 +130,10 @@ def main(args):
         print('Compare {} to {}'.format(table_old, table_new))
 
         success = compare_two_tables(table_old, table_new, params['BQ_AS_BATCH'])
-        print(success.total_rows)
+        if success.total_rows == 0:
+            print('the tables are the same')
+        else:
+            print('the tables are NOT the same')
 
         if not success:
             print('compare_tables failed')
