11e600f0363ed139ccbb2dcdb65dde962ba96ca7
laurenwolfe-isb <lwolfe@systemsbiology.org>
2020-08-25 03:57:28 -0700

    filtering out case records for webapp

--- BQ_Table_Building/build_clinical_data_program_tables.py
+++ BQ_Table_Building/build_clinical_data_program_tables.py
@@ -514,7 +514,7 @@ def merge_column_orders(schema, columns, record_counts, column_orders, is_webapp
 
     for table, depth in get_sorted_fg_depths(record_counts, reverse=True):
 
-        schema_key = get_fg_id_name(API_PARAMS, table, is_webapp)
+        schema_key = get_fg_id_name(API_PARAMS, table, is_webapp=False)
 
         if table in columns:
             merged_order_key = table
