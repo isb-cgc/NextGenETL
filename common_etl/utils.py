fa835ce5a8eb760608a0f48918b6d657f71d12fd
laurenwolfe-isb <lwolfe@systemsbiology.org>
2020-08-24 19:32:34 -0700

    rename fields

--- common_etl/utils.py
+++ common_etl/utils.py
@@ -709,24 +709,24 @@ def modify_fields_for_webapp(schema, webapp_column_orders, api_params):
             schema.pop(field)
 
         elif field_name in renamed_fields:
+            old_schema_name = '.'.join([parent_fg, field_name])
             new_field_name = renamed_fields[field_name]
             new_schema_name = '.'.join([parent_fg, new_field_name])
 
             schema[field]['name'] = new_field_name
             schema[new_schema_name] = schema[field]
+
+            if new_schema_name in webapp_column_orders[parent_fg]:
+                webapp_column_orders[parent_fg][new_schema_name] = \
+                    webapp_column_orders[parent_fg][old_schema_name]
+                webapp_column_orders[parent_fg].pop(old_schema_name)
+
             schema.pop(field)
 
         if (parent_fg in webapp_column_orders
                 and field in webapp_column_orders[parent_fg]
                 and field in exclude_fields):
             webapp_column_orders[parent_fg].pop(field)
-        else:
-            for renamed_field in renamed_fields:
-                if renamed_field in webapp_column_orders[parent_fg]:
-                    new_field_name = renamed_fields[renamed_field]
-                    webapp_column_orders[parent_fg][new_field_name] = \
-                        webapp_column_orders[parent_fg][renamed_field]
-
 
     print(webapp_column_orders)
 
