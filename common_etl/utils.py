3a884db3acb61939cb42564d4cc6a7fc11b31338
laurenwolfe-isb <lwolfe@systemsbiology.org>
2020-09-10 18:06:55 -0700

    changed proj to project_name in biospecimen table

--- common_etl/utils.py
+++ common_etl/utils.py
@@ -1350,7 +1350,7 @@ def load_config(yaml_file, yaml_dict_keys):
         yaml_dict = yaml.load(config_stream, Loader=yaml.FullLoader)
         print("yaml_dict".format(yaml_dict))
     except yaml.YAMLError as ex:
-        has_fatal_error(ex, yaml.YAMLError)
+        has_fatal_error(ex, str(yaml.YAMLError))
     if yaml_dict is None:
         has_fatal_error("Bad YAML load, exiting.", ValueError)
 
