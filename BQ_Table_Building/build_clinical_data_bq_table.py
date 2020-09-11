299cac5b9c732eeeb73c613f9b496922453fc19e
laurenwolfe-isb <lwolfe@systemsbiology.org>
2020-09-11 03:05:02 -0700

    changed proj to project_name in biospecimen table

--- BQ_Table_Building/build_clinical_data_bq_table.py
+++ BQ_Table_Building/build_clinical_data_bq_table.py
@@ -19,11 +19,6 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 """
-import sys
-import time
-import os
-import json
-import requests
 from common_etl.utils import *
 
 API_PARAMS = dict()
