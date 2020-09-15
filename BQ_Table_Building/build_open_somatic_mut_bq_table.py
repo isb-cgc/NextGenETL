96d9311d54b5cd4eb71178ae8ae44888a1ae627f
lhagen <lhagen@systemsbiology.org>
2020-09-15 10:21:44 -0700

    update compare_remove_old_current

--- BQ_Table_Building/build_open_somatic_mut_bq_table.py
+++ BQ_Table_Building/build_open_somatic_mut_bq_table.py
@@ -752,7 +752,7 @@ def main(args):
             elif table_moved:
                 print('Deleting old table: {}'.format(old_current_table))
                 delete_table = delete_table_bq_job(params['PUBLICATION_PROJECT'],
-                                                   ".".join(params['PUBLICATION_PROJECT'], [params['PUBLICATION_DATASET'],
+                                                   ".".join([params['PUBLICATION_PROJECT'], params['PUBLICATION_DATASET'],
                                                              publication_table.format('current')]))
                 if not delete_table:
                     print('delete table failed')
