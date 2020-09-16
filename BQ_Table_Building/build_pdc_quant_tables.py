eab5a85fbca8623da0a4450ed96241a058cd31f2
laurenwolfe-isb <lwolfe@systemsbiology.org>
2020-09-15 21:20:02 -0700

    add pdc quant tables

--- BQ_Table_Building/build_pdc_quant_tables.py
+++ BQ_Table_Building/build_pdc_quant_tables.py
@@ -44,7 +44,10 @@ def get_and_write_quant_data(study_id_dict, data_type, jsonl_fp):
                                         query=query_quant_data_matrix(study_submitter_id,
                                                                       data_type))
 
-    print(res_json)
+    if not res_json['quantDataMatrix']:
+        lines_written = 0
+        ("{} lines written for {}.".format(lines_written, study_submitter_id))
+        return lines_written
 
     log2_ratio_list = list()
 
