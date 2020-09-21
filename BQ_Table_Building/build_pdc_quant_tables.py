11836c65b0e561e74f35af4c7bf8656be592e7a3
laurenwolfe-isb <lwolfe@systemsbiology.org>
2020-09-20 18:45:47 -0700

    Get cases samples aliquots

--- BQ_Table_Building/build_pdc_quant_tables.py
+++ BQ_Table_Building/build_pdc_quant_tables.py
@@ -278,7 +278,8 @@ def main(args):
                 API_PARAMS,
                 query=make_cases_samples_aliquots_query(offset, API_PARAMS['CSA_LIMIT']))
 
-            for cases_samples_aliquots in json_res['casesSamplesAliqouts']:
+            for cases_samples_aliquots in \
+                    json_res['paginatedCasesSamplesAliquots']['casesSamplesAliqouts']:
                 case_submitter_id = cases_samples_aliquots['case_submitter_id']
                 case_id = cases_samples_aliquots['case_id']
                 external_case_id = cases_samples_aliquots['external_case_id']
