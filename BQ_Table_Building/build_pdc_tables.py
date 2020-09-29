41ee48ef7d7892cb3ffc585e17dcf1bb04a32482
laurenwolfe-isb <lwolfe@systemsbiology.org>
2020-09-29 12:20:39 -0700

    biospecimen dict

--- BQ_Table_Building/build_pdc_tables.py
+++ BQ_Table_Building/build_pdc_tables.py
@@ -597,16 +597,16 @@ def build_biospec_query(table_id):
 
 def build_biospec_count_query(biospec_table_id, csa_table_id):
     return """
-        WITH aliquot_run_count AS (
+        WITH csa AS (
             SELECT count(distinct aliquot_run_metadata_id) as aliquot_run_metadata_id_count
             FROM `{}`
         )
-        SELECT count(distinct case_id) as case_id_count,
-        count(distinct study_id) as study_id_count,
-        count(distinct sample_id) as sample_id_count,
-        count(distinct aliquot_id) as aliquot_id_count,
-        aliquot_run_count
-        FROM `{}`
+        SELECT count(distinct bio.case_id) as case_id_count,
+        count(distinct bio.study_id) as study_id_count,
+        count(distinct bio.sample_id) as sample_id_count,
+        count(distinct bio.aliquot_id) as aliquot_id_count,
+        csa.aliquot_run_metadata_id_count
+        FROM `{}` as bio
     """.format(csa_table_id, biospec_table_id)
 
 
