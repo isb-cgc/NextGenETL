5725a30d8ea09c3456fcb5dbb0f13d3fdee70c60
laurenwolfe-isb <lwolfe@systemsbiology.org>
2020-09-29 15:03:11 -0700

    nested biospec dict create table

--- BQ_Table_Building/build_pdc_tables.py
+++ BQ_Table_Building/build_pdc_tables.py
@@ -939,7 +939,9 @@ def main(args):
                 'studies': study_list
             })
 
+        print("{ 'cases': '", end='')
         print(case_list[0:100])
+        print("}")
         exit()
 
         case_study_sample_aliquot_obj = {
