b303b178c5e8136f6cc4e61bfa456197881652ea
laurenwolfe-isb <lwolfe@systemsbiology.org>
2020-10-29 14:52:22 -0700

    altering uniprot id handling

--- BQ_Table_Building/build_pdc_tables.py
+++ BQ_Table_Building/build_pdc_tables.py
@@ -1393,15 +1393,15 @@ def main(args):
                                      BQ_PARAMS['DEV_META_DATASET'],
                                      get_table_name(BQ_PARAMS['GENE_TABLE']))
 
-        schema_dict = {}
+        schema_list = []
 
         gene_table = client.get_table(gene_table_id)
 
         for schema_field in gene_table.schema:
             schema_obj = schema_field.to_api_repr()
-            schema_dict.append(schema_obj)
+            schema_list.append(schema_obj)
 
-        print("schema: {}".format(schema_dict))
+        print("schema: {}".format(schema_list))
 
     if 'analyze_gene_table' in steps:
         table_name = get_table_name(BQ_PARAMS['GENE_TABLE'])
