11ebd71bb4001fe24c8cbcd9f2696e7116faca23
lhagen <lhagen@systemsbiology.org>
2020-11-06 11:02:55 -0800

    removed unused variables

--- BQ_Table_Building/build_cosmic_bq_table.py
+++ BQ_Table_Building/build_cosmic_bq_table.py
@@ -325,6 +325,7 @@ def main(args):
        # for line in all_files:
 
             file = line.split('/')[-1]
+            target_file = '/'.join('~',line.split('/')[-3], line.split('/')[-2], line.split('/')[-1])
         #    file_name, ext = os.path.splitext(file)
         #    file_components = file_name.split("_")
         #    data_type = "_".join(file_components[0:(len(file_components) - 2)])
@@ -332,7 +333,7 @@ def main(args):
 
             if 'upload_to_bucket' in steps:
                 print('upload_to_bucket')
-                upload_to_bucket(params['WORKING_BUCKET'], bucket_target_blob, line)
+                upload_to_bucket(params['WORKING_BUCKET'], bucket_target_blob, target_file)
 
             if 'create_bq_from_tsv' in steps:
                 print('create_bq_from_tsv')
