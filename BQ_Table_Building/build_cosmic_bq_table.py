e3e543df2be07384f0b1cbc22a0e75ac18d166f5
lhagen <lhagen@systemsbiology.org>
2020-11-06 11:03:45 -0800

    removed unused variables

--- BQ_Table_Building/build_cosmic_bq_table.py
+++ BQ_Table_Building/build_cosmic_bq_table.py
@@ -325,7 +325,7 @@ def main(args):
        # for line in all_files:
 
             file = line.split('/')[-1]
-            target_file = '/'.join('~',line.split('/')[-3], line.split('/')[-2], line.split('/')[-1])
+            target_file = '/'.join(['~',line.split('/')[-3], line.split('/')[-2], line.split('/')[-1]])
         #    file_name, ext = os.path.splitext(file)
         #    file_components = file_name.split("_")
         #    data_type = "_".join(file_components[0:(len(file_components) - 2)])
