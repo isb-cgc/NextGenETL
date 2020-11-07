6de5a4cfbe580163a25ec9b5471e49155d35a590
laurenwolfe-isb <lwolfe@systemsbiology.org>
2020-11-06 17:09:31 -0800

    Adding age_at_diagnosis_days, program_name, disease_code fields for webapp

--- BQ_Table_Building/build_clinical_data_program_tables.py
+++ BQ_Table_Building/build_clinical_data_program_tables.py
@@ -928,6 +928,8 @@ def create_and_load_tables(program, cases, schemas, record_counts, is_webapp=Fal
         else:
             table_id = get_working_table_id(BQ_PARAMS, table_name)
 
+        print(schemas[record_table])
+
         create_and_load_table(BQ_PARAMS, jsonl_name, schemas[record_table], table_id)
 
 
