679f9c3d6596cf71b9d216a6ce3bbb2fd78d485d
lhagen <lhagen@systemsbiology.org>
2020-11-20 17:00:21 -0800

    update replace_schema_tags

--- BQ_Table_Building/build_cosmic_bq_table.py
+++ BQ_Table_Building/build_cosmic_bq_table.py
@@ -308,7 +308,7 @@ def main(args):
                 print('process_git_schema: {}'.format(line))
                 # Where do we dump the schema git repository?
                 print("schema_file_name: " + schema_file_name)
-                schema_file = "{}/{}/{}".format(params['SCHEMA_REPO_LOCAL'], params['RAW_SCHEMA_DIR'], schema_file_tag)
+                schema_file = "{}/{}/{}".format(params['SCHEMA_REPO_LOCAL'], params['RAW_SCHEMA_DIR'], schema_file_name)
                 #full_file_prefix = "{}/{}".format(params['PROX_DESC_PREFIX'], schema_file_name)
                 print(schema_file + "\t" + data_type)
 
@@ -331,7 +331,7 @@ def main(args):
                     tag_map_list.append(use_pair)
                 #full_file_prefix = "{}/{}".format(params['PROX_DESC_PREFIX'], schema_file_name)
                 # Write out the details
-                success = customize_labels_and_desc(data_type, tag_map_list)
+                success = customize_labels_and_desc(full_file_prefix, tag_map_list)
                 if not success:
                     print("replace_schema_tags failed")
                     return
