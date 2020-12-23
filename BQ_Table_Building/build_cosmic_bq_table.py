372078897a93a88877387578cc7627d63ea702a3
lhagen <lhagen@systemsbiology.org>
2020-12-22 17:13:58 -0800

    update to final schema steps

--- BQ_Table_Building/build_cosmic_bq_table.py
+++ BQ_Table_Building/build_cosmic_bq_table.py
@@ -377,7 +377,7 @@ def main(args):
                 if 'update_final_schema' in steps:
                     success = update_schema(params['SCRATCH_DATASET'],
                                             "_".join([data_type, file_components[-2], schema_release]),
-                                            hold_schema_dict)
+                                            hold_schema_dict.format(file_name))
                     if not success:
                         print("Schema update failed")
                         return
