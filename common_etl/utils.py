5d9920ae4fb9d0bb04aa1051d313254e1f599bbf
laurenwolfe-isb <lwolfe@systemsbiology.org>
2021-01-22 15:57:41 -0800

    clinical diagnoses build jsonl

--- common_etl/utils.py
+++ common_etl/utils.py
@@ -854,7 +854,6 @@ def get_graphql_api_response(api_params, query, fail_on_error=True):
     while not api_res.ok and tries < max_retries:
         if api_res.status_code == 400:
             # don't try again!
-            api_res.raise_for_status()
             has_fatal_error("API response status code {}\nRequest body:\n{}".format(api_res.status_code, req_body))
 
         console_out("API response status code {}: {};\nRetry {} of {}...",
