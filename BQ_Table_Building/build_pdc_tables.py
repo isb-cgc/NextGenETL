7e607d238fc833557cb3403b13ca70ca6ebd39e1
laurenwolfe-isb <lwolfe@systemsbiology.org>
2021-01-26 23:15:41 -0800

    Create tables for per-project clinical data

--- BQ_Table_Building/build_pdc_tables.py
+++ BQ_Table_Building/build_pdc_tables.py
@@ -1579,8 +1579,6 @@ def main(args):
             # - else create a PROJECT_clinical_pdc_current table and a PROJECT_clinical_diagnoses_pdc_current table
             cases = project_dict['cases']
             for case in cases:
-                print(case)
-                exit()
                 if 'case_id' not in case:
                     print(case)
                     exit()
