a5f55637214551e5fd33d44902d9472227c4a96b
laurenwolfe-isb <lwolfe@systemsbiology.org>
2021-02-02 17:15:05 -0800

    build sorted column clin table

--- BQ_Table_Building/build_pdc_tables.py
+++ BQ_Table_Building/build_pdc_tables.py
@@ -1699,7 +1699,9 @@ def main(args):
                 FROM {} clinical
                 """.format(parent_select_str, diagnoses_subquery, temp_diagnoses_table_id)
 
-                load_table_from_query(BQ_PARAMS, diagnoses_table_id, query)
+                print(query)
+
+                # load_table_from_query(BQ_PARAMS, diagnoses_table_id, query)
 
     if 'build_quant_tsvs' in steps:
         for study_id_dict in studies_list:
