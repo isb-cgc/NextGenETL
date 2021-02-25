9a5d84d03ed0add6f626c7f31f74e4d8ac3d8293
laurenwolfe-isb <lwolfe@systemsbiology.org>
2021-02-25 15:32:26 -0800

    Add release checking

--- BQ_Table_Building/build_clinical_data_program_tables.py
+++ BQ_Table_Building/build_clinical_data_program_tables.py
@@ -1435,7 +1435,7 @@ def main(args):
             console_out("{0} processed in {1}!\n", (program, format_seconds(prog_end)))
 
     if 'list_tables_for_publication':
-        print(get_publish_table_ids())
+        print(build_publish_table_list())
 
     if 'update_table_metadata' in steps:
         update_metadata()
