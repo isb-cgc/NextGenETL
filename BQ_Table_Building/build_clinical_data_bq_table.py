c32267dc589eea1892ba4e76fa22b56a08dc89a0
laurenwolfe-isb <lwolfe@systemsbiology.org>
2021-03-05 22:48:16 -0800

    testing gdc case clinical extract script

--- BQ_Table_Building/build_clinical_data_bq_table.py
+++ BQ_Table_Building/build_clinical_data_bq_table.py
@@ -38,13 +38,6 @@ BQ_PARAMS = dict()
 YAML_HEADERS = ('api_params', 'bq_params', 'steps')
 
 
-##################################################################################
-#
-#   API calls and data normalization (for BQ table insert)
-#
-##################################################################################
-
-
 def request_data_from_gdc_api(curr_index):
     """ Make a POST API request and return response (if valid).
 
@@ -82,11 +75,11 @@ def request_data_from_gdc_api(curr_index):
     return None
 
 
-def retrieve_and_save_case_records(scratch_fp):
+def retrieve_and_save_case_records(local_path):
     """Retrieves case records from API and outputs them to a JSONL file, which is later
         used to populate the clinical data BQ table.
 
-    :param scratch_fp: absolute path to data output file
+    :param local_path: absolute path to data output file
     """
     def add_missing_field_groups_to_case_json():
         for field_group in API_PARAMS["FIELD_GROUPS"]:
@@ -110,8 +103,6 @@ def retrieve_and_save_case_records(scratch_fp):
                         index += 1
 
     start_time = time.time()
-
-    print("Outputting json objects to {} in '{}' mode".format(scratch_fp, BQ_PARAMS['IO_MODE']))
 
     jsonl_list = list()
     total_cases = None
@@ -156,242 +147,15 @@ def retrieve_and_save_case_records(scratch_fp):
         err_str = "jsonl count ({}) not equal to total cases ({})".format(len(jsonl_list), total_cases)
         assert total_cases == len(jsonl_list), err_str
 
-    write_list_to_jsonl(scratch_fp, jsonl_list)
+    write_list_to_jsonl(local_path, jsonl_list)
 
-    # calculate processing time and file size
+    print("Output jsonl to {} in '{}' mode".format(local_path, BQ_PARAMS['IO_MODE']))
     extract_time = format_seconds(time.time() - start_time)
-    file_size = os.stat(scratch_fp).st_size / 1048576.0
-
-    print("\nClinical data retrieval complete!")
+    print()
+    print("Clinical data retrieval complete!")
+    file_size = os.stat(local_path).st_size / 1048576.0
     print("\t{:.2f} mb jsonl file size".format(file_size))
-    print("\t{:.1f} to query API and write to local jsonl file\n".format(extract_time))
-
-
-##################################################################################
-#
-#   BQ table creation and data insertion
-#
-##################################################################################
-
-
-def create_field_records_dict(field_mappings, field_data_types):
-    """ Generate schema dict composed of schema field dicts.
-
-    :param field_mappings: dict of {fields: schema entry dicts}
-    :param field_data_types: dict of {fields: data types}
-    :return: SchemaField object dict
-    """
-    # this could use BQ Python API built-in method
-    schema_dict = {}
-
-    for field in field_data_types:
-        try:
-            column_name = field_mappings[field]['name'].split('.')[-1]
-            description = field_mappings[field]['description']
-        except KeyError:
-            # cases.id not returned by mapping endpoint. In such cases,
-            # substitute an empty description string.
-            column_name = field.split(".")[-1]
-            description = ""
-
-        if field_data_types[field]:
-            # if script was able to infer a data type using field's values,
-            # default to using that type
-            field_type = field_data_types[field]
-        elif field in field_mappings:
-            # otherwise, include type from _mapping endpoint
-            field_type = field_mappings[field]['type']
-        else:
-            # this could happen in the case where a field was added to the
-            # cases endpoint with only null values,
-            # and no entry for the field exists in mapping
-            print("[INFO] Not adding field {0} because no type found".format(field))
-            continue
-
-        # this is the format for bq schema json object entries
-        schema_dict[field] = {
-            "name": column_name,
-            "type": field_type,
-            "description": description
-        }
-
-    return schema_dict
-
-
-'''
-def generate_bq_schema(schema_dict, record_type, expand_fields_list):
-    # add fields to a list in order to generate a dict representing nested fields
-    field_group_names = [record_type]
-    nested_depth = 0
-
-    for field_group in expand_fields_list.split(','):
-        nested_field_name = record_type + '.' + field_group
-        nested_depth = max(nested_depth, len(nested_field_name.split('.')))
-        field_group_names.append(nested_field_name)
-
-    record_lists_dict = {field_grp_name: [] for field_grp_name in field_group_names}
-    # add field to correct field grouping list based on full field name
-    for field in schema_dict:
-        # record_lists_dict key is equal to the parent field components of
-        # full field name
-        # remove field from period-delimited field group string
-        field_group = ".".join(field.split('.')[:-1])
-
-        record_lists_dict[field_group].append(schema_dict[field])
-
-    temp_schema_field_dict = {}
-
-    while nested_depth >= 1:
-        for field_group_name in record_lists_dict:
-            split_group_name = field_group_name.split('.')
-
-            # builds from max depth inward to avoid iterating through entire schema obj
-            # in order to append child field groups. Skip any shallower field groups.
-            if len(split_group_name) != nested_depth:
-                continue
-
-            schema_field_sublist = []
-
-            for record in record_lists_dict[field_group_name]:
-                schema_field = bigquery.SchemaField(name=record['name'],
-                                                    field_type=record['type'],
-                                                    mode='NULLABLE',
-                                                    description=record['description'],
-                                                    fields=())
-
-                schema_field_sublist.append(schema_field)
-
-            # remove field from period-delimited field group string
-            parent_name = ".".join(field_group_name.split('.')[:-1])
-
-            if field_group_name in temp_schema_field_dict:
-                schema_field_sublist += temp_schema_field_dict[field_group_name]
-
-            if parent_name:
-                if parent_name not in temp_schema_field_dict:
-                    temp_schema_field_dict[parent_name] = list()
-
-                """
-                NOTE: removed get_field_name() wrapper from name argument--that shouldn't be needed and is 
-                being relocated from utils.py to build_clinical_data_program_tables.py 
-                """
-                schema_field = bigquery.SchemaField(name=field_group_name,
-                                                    field_type='RECORD',
-                                                    mode='REPEATED',
-                                                    description='',
-                                                    fields=tuple(schema_field_sublist))
-
-                temp_schema_field_dict[parent_name].append(schema_field)
-            else:
-                if nested_depth > 1:
-                    has_fatal_error("Empty parent_name at level {}".format(nested_depth), ValueError)
-                return schema_field_sublist
-
-        nested_depth -= 1
-
-    return None
-'''
-
-'''
-def create_bq_schema(data_fp):
-    """Generates two dicts (one using data type inference, one using _mapping API
-    endpoint.) Compares values and builds a SchemaField object, used to
-    initialize the bq table.
-
-    :param data_fp: path to API data output file (jsonl format)
-    """
-    def create_mapping_dict():
-        """Creates a dict containing field mappings for given endpoint.
-        Note: only differentiates the GDC API's 'long' type (called 'integer' in GDC data
-        dictionary) and 'float' type (called 'number' in GDC data dictionary). All others
-        typed as string.
-
-        :return: dict of field maps. Each entry contains field name, type, and description
-        """
-        field_map_dict = {}
-
-        # retrieve mappings json object
-        res = requests.get(API_PARAMS['ENDPOINT'] + '/_mapping')
-        field_mappings = res.json()['_mapping']
-
-        for field in field_mappings:
-            # convert data types from GDC format to formats used in BQ
-            if field_mappings[field]['type'] == 'long':
-                field_type = 'INTEGER'
-            elif field_mappings[field]['type'] == 'float':
-                field_type = 'FLOAT'
-            else:
-                field_type = 'STRING'
-
-            # create json object of field mapping data
-            field_map_dict[field] = {
-                'name': field.split('.')[-1],
-                'type': field_type,
-                'description': field_mappings[field]['description']
-            }
-
-        return field_map_dict
-
-    def collect_values(fields, field, parent, field_grp_prefix):
-        """Recursively inserts sets of values for a given field into return dict (
-        used to infer field data type).
-
-        :param fields: A dict of key:value pairs -- {field_name: set(field_values)}
-        :param field: field name
-        :param parent: dict containing field and it's values
-        :param field_grp_prefix: string representation of current location in field hierarchy
-        :return: field_dict containing field names and a set of its values
-        """
-        # If the value of parent_dict[key] is a list at this level, and a dict at the next
-        # (or a dict at this level, as seen in second conditional statement),
-        # iterate over each list element's dictionary entries. (Sometimes lists are composed
-        # of strings rather than dicts, and those are later converted to strings.)
-        field_name = field_grp_prefix + field
-        new_prefix = field_name + '.'
-
-        if isinstance(parent[field], list) and len(parent[field]) > 0 and isinstance(parent[field][0], dict):
-            for dict_item in parent[field]:
-                for dict_key in dict_item:
-                    fields = collect_values(fields, dict_key, dict_item, new_prefix)
-        elif isinstance(parent[field], dict):
-            for dict_key in parent[field]:
-                fields = collect_values(fields, dict_key, parent[field], new_prefix)
-        else:
-            if field_name not in fields:
-                fields[field_name] = set()
-
-            # This type of list can be converted to a comma-separated value string
-            if isinstance(parent[field], list):
-                value = ", ".join(parent[field])
-            else:
-                value = parent[field]
-
-            fields[field_name].add(value)
-
-        return fields
-
-    # generate dict containing field mapping results
-    field_mapping_dict = create_mapping_dict()
-
-    with open(data_fp, 'r') as data_file:
-        field_dict = dict()
-
-        for line in data_file:
-            json_case = json.loads(line)
-            for key in json_case:
-                field_dict = collect_values(field_dict, key, json_case, 'cases.')
-
-    field_data_type_dict = infer_data_types(field_dict)
-
-    # create a flattened dict of schema fields
-    schema_dict = create_field_records_dict(field_mapping_dict, field_data_type_dict)
-
-    endpoint_name = API_PARAMS['ENDPOINT'].split('/')[-1]
-
-    return generate_bq_schema(schema_dict,
-                              record_type=endpoint_name,
-                              expand_fields_list=",".join(API_PARAMS['FIELD_GROUPS']))  # note: removed list() wrapper
-'''
+    print("\t{} to query API and write to local jsonl file\n".format(extract_time))
 
 
 def main(args):
