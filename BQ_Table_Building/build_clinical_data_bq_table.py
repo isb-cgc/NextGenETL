711717eee714d61d7c89d753dbb8bf14890cded0
laurenwolfe-isb <lwolfe@systemsbiology.org>
2021-03-07 16:06:40 -0800

    testing gdc case clinical extract script

--- BQ_Table_Building/build_clinical_data_bq_table.py
+++ BQ_Table_Building/build_clinical_data_bq_table.py
@@ -163,7 +163,7 @@ def add_missing_fields_to_case_json(grouped_fields_dict, case):
 
 
 def merge_dummy_case_with_case(dummy_case, case):
-    if isinstance(dummy_case, list) and not isinstance(dummy_case, dict):
+    if isinstance(dummy_case, list) or isinstance(dummy_case, dict):
         for field in dummy_case.keys():
             if isinstance(dummy_case[field], list):
                 if field not in case:
