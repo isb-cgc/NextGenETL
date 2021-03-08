eecad31767c7a49f6d086d08bfa776d2077ce6be
laurenwolfe-isb <lwolfe@systemsbiology.org>
2021-03-08 14:35:17 -0800

    Refactored GDC main and added new data type inference

--- BQ_Table_Building/build_clinical_data_program_tables.py
+++ BQ_Table_Building/build_clinical_data_program_tables.py
@@ -309,14 +309,15 @@ def flatten_tables(field_groups, record_counts, is_webapp=False):
     table_columns = dict()
 
     field_grp_depths = {field_grp: len(field_grp.split('.')) for field_grp in field_groups}
+    excluded_fields = get_excluded_fields_all_fgs(field_groups, is_webapp)
+
+    print(excluded_fields)
 
     print(sorted(field_grp_depths.items(), key=lambda i: i[1]))
 
     for field_grp, depth in sorted(field_grp_depths.items(), key=lambda i: i[1]):
         if depth > 3:
             print("\n[INFO] Caution, not confirmed to work with nested depth > 3\n")
-
-        excluded_fields = get_excluded_fields_all_fgs(field_groups, is_webapp)
 
         if is_webapp and field_grp in excluded_fields:
             continue
