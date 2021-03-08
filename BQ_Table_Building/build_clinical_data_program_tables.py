5c9ce5050a988892fe1397195be70abaf949bf14
laurenwolfe-isb <lwolfe@systemsbiology.org>
2021-03-08 14:13:50 -0800

    Refactored GDC main and added new data type inference

--- BQ_Table_Building/build_clinical_data_program_tables.py
+++ BQ_Table_Building/build_clinical_data_program_tables.py
@@ -218,12 +218,8 @@ def parse_bq_schema_obj(schema, fg, schema_list=None, is_webapp=False):
                 schema[field_name]['mode'] = 'REQUIRED'
         else:
             # not a nested field entry--do we need to prefix the schema field name?
-
-            print(field_key)
-
             # schema_field['name'] = get_bq_name(field_key, is_webapp)
             schema_field['name'] = field_key.split('.')[-1]
-            print(schema_field)
             schema[field_key] = schema_field
 
 
