1fbca5e40369402affb92c0757d2430ff1b157be
laurenwolfe-isb <lwolfe@systemsbiology.org>
2021-03-08 00:40:23 -0800

    testing gdc case clinical extract script

--- BQ_Table_Building/build_clinical_data_bq_table.py
+++ BQ_Table_Building/build_clinical_data_bq_table.py
@@ -291,29 +291,33 @@ def generate_jsonl_from_modified_api_json(local_jsonl_path):
             assert fg in temp_case and temp_case[fg], "{} field group null for index {}\n".format(fg, index)
 
         diag_cnt = len(temp_case['diagnoses'][0]) - 2
-        treat_cnt = len(temp_case['diagnoses'][0]['treatments'][0])
-        annot_cnt = len(temp_case['diagnoses'][0]['annotations'][0])
-
         assert_output_count("cases.diagnoses", diag_cnt)
+
+        treat_cnt = len(temp_case['diagnoses'][0]['treatments'][0])
         assert_output_count("cases.diagnoses.treatments", treat_cnt)
+
+        annot_cnt = len(temp_case['diagnoses'][0]['annotations'][0])
         assert_output_count("cases.diagnoses.annotations", annot_cnt)
 
         follow_cnt = len(temp_case['follow_ups'][0]) - 1
-        mol_cnt = len(temp_case['follow_ups'][0]['molecular_tests'][0])
-
         assert_output_count("cases.follow_ups", follow_cnt)
+
+        mol_cnt = len(temp_case['follow_ups'][0]['molecular_tests'][0])
         assert_output_count("cases.follow_ups.molecular_tests", mol_cnt)
 
         exp_cnt = len(temp_case['exposures'][0])
-        demo_cnt = len(temp_case['demographic'])
-        fam_hist_cnt = len(temp_case['family_histories'][0])
-        proj_cnt = len(temp_case['project'])
-        case_cnt = len(temp_case) - 5
-
         assert_output_count("cases.exposures", exp_cnt)
+
+        demo_cnt = len(temp_case['demographic'])
         assert_output_count("cases.demographic", demo_cnt)
+
+        fam_hist_cnt = len(temp_case['family_histories'][0])
         assert_output_count("cases.family_histories", fam_hist_cnt)
+
+        proj_cnt = len(temp_case['project'])
         assert_output_count("cases.project", proj_cnt)
+
+        case_cnt = len(temp_case) - 5
         assert_output_count("cases", case_cnt)
 
         cases_list[index] = temp_case
