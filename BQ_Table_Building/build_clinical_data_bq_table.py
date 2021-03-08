bda637b87d00ad597cbfa4788517530ab7d58eb8
laurenwolfe-isb <lwolfe@systemsbiology.org>
2021-03-07 23:00:15 -0800

    testing gdc case clinical extract script

--- BQ_Table_Building/build_clinical_data_bq_table.py
+++ BQ_Table_Building/build_clinical_data_bq_table.py
@@ -249,11 +249,7 @@ def generate_jsonl_from_modified_api_json(local_jsonl_path):
     with open(local_json_path, 'r') as json_file:
         cases_json = json.load(json_file)
 
-    cases_list = cases_json['cases']
-    print(len(cases_list[0]))
-
-
-    # print(cases_list)
+    cases_list = cases_json['cases'][0]
 
     print("length of cases_list: {}".format(len(cases_list)))
 
