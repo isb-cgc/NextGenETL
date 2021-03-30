73faa6d6f8d2a2107744ef601f51e3a2f1530e55
laurenwolfe-isb <lwolfe@systemsbiology.org>
2021-03-29 18:14:11 -0700

    Bool and string of type. Wonder if the strings could be converted to bool, will check

--- common_etl/utils.py
+++ common_etl/utils.py
@@ -894,7 +894,8 @@ def resolve_type_conflict(field, types_set):
     # Beyond this point, the types_set has at least two type values
     if "ARRAY" in types_set or "RECORD" in types_set:
         # these types cannot be implicitly converted to any other, exit
-        has_fatal_error("Invalid datatype combination for {}: {}".format(field, types_set), TypeError)
+        print("Invalid datatype combination for {}: {}".format(field, types_set))
+        has_fatal_error("", TypeError)
 
     if "STRING" in types_set:
         # if it's classified as a string, we've already disqualified it from the other types,
@@ -902,7 +903,8 @@ def resolve_type_conflict(field, types_set):
         return "STRING"
 
     if "BOOL" in types_set:
-        has_fatal_error("Invalid datatype combination for {}: {}".format(field, types_set), TypeError)
+        print("Invalid datatype combination for {}: {}".format(field, types_set))
+        has_fatal_error("", TypeError)
 
     has_datetime_type = False
 
