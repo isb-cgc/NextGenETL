726c81b98caf8d99d488275ead6a6b80c197319b
laurenwolfe-isb <lwolfe@systemsbiology.org>
2021-04-23 18:03:14 -0700

    Adding views

--- common_etl/utils.py
+++ common_etl/utils.py
@@ -595,7 +595,6 @@ def load_table_from_query(bq_params, table_id, query):
         has_fatal_error(err)
 
 
-'''
 def create_view_from_query(view_id, view_query):
     """
 
@@ -621,7 +620,6 @@ def create_view_from_query(view_id, view_query):
         has_fatal_error(f"View {view_id} not created, exiting.")
     else:
         print(f"Created {view.table_type}: {str(view.reference)}")
-'''
 
 
 def load_bq_schema_from_json(bq_params, filename):
