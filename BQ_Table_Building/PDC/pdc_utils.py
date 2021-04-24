3b58eec9a9cfc5a3d38e33084ecf55c83f488ada
laurenwolfe-isb <lwolfe@systemsbiology.org>
2021-04-23 22:00:40 -0700

    Building non-proteome tables

--- BQ_Table_Building/PDC/pdc_utils.py
+++ BQ_Table_Building/PDC/pdc_utils.py
@@ -123,8 +123,7 @@ def build_obj_from_pdc_api(api_params, endpoint, request_function, request_param
 
     print("Sending {} API request: ".format(endpoint))
 
-    print(f"""
-        api_params={api_params}, endpoint={endpoint}, request_function={request_function}, 
+    print(f"""endpoint={endpoint}, request_function={request_function}, 
         request_params={request_params}, alter_json_function={alter_json_function},
         ids={ids}, insert_id={insert_id}, pause={pause}
     """)
