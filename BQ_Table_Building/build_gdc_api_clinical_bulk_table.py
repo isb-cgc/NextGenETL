22f7f53eaa4413e097b17f9d77faddd1f8637ac1
laurenwolfe-isb <lwolfe@systemsbiology.org>
2021-04-27 17:52:10 -0700

    re-add project

--- BQ_Table_Building/build_gdc_api_clinical_bulk_table.py
+++ BQ_Table_Building/build_gdc_api_clinical_bulk_table.py
@@ -176,6 +176,8 @@ def main(args):
                                                       table_name=bulk_table_name,
                                                       include_release=False)
 
+        print(bulk_table_schema)
+
         # Load jsonl data into BigQuery table
         create_and_load_table_from_jsonl(BQ_PARAMS,
                                          jsonl_file=jsonl_output_file,
