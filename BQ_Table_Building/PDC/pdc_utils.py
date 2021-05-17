1d6ec404634f15ae526a82b5a9494effda75e019
laurenwolfe-isb <lwolfe@systemsbiology.org>
2021-05-17 16:45:34 -0700

    Add friendly name mapping

--- BQ_Table_Building/PDC/pdc_utils.py
+++ BQ_Table_Building/PDC/pdc_utils.py
@@ -315,7 +315,7 @@ def make_retrieve_all_studies_query(api_params, bq_params, output_name):
 
     return f"""
     SELECT pdc_study_id, submitter_id_name AS study_name, embargo_date, 
-        project_submitter_id, analytical_fraction, program_short_name
+        project_submitter_id, analytical_fraction, program_short_name, project_friendly_name
     FROM  `{table_id}`
     """
 
