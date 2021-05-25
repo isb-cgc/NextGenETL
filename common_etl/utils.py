aa182e792e72a1c735e9328d07d0b79dd95a43c6
laurenwolfe-isb <lwolfe@systemsbiology.org>
2021-05-24 19:46:42 -0700

    Testing version diff testing in publish step

--- common_etl/utils.py
+++ common_etl/utils.py
@@ -452,14 +452,13 @@ def publish_table(api_params, bq_params, public_dataset, source_table_id, overwr
                 table_id_no_release = _versioned_table_id.replace(api_params['RELEASE'], '')
                 prev_release_table_id = f"{table_id_no_release}_V{last_major_rel_num}_{last_minor_rel_num}"
 
-                print(prev_release_table_id)
-
                 if exists_bq_table(prev_release_table_id):
                     # found last release table, stop iterating
                     return prev_release_table_id
 
                 # stop at oldest mass-published version, 1_17, if no previous table found
-                if last_major_rel_num == 1 and last_minor_rel_num == 17:
+                # todo switch back to 17
+                if last_major_rel_num == 1 and last_minor_rel_num == 9:
                     return None
         else:
             print(f"Set up release diff check in publish_tables for data source {api_params['DATA_SOURCE']}")
