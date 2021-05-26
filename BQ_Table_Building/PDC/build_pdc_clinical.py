ab0a12df6e04bfa22fef2a12d50f86685a77775b
laurenwolfe-isb <lwolfe@systemsbiology.org>
2021-05-26 16:43:51 -0700

    clinical check, ok

--- BQ_Table_Building/PDC/build_pdc_clinical.py
+++ BQ_Table_Building/PDC/build_pdc_clinical.py
@@ -815,8 +815,7 @@ def main(args):
                           source_table_id=clinical_table_id,
                           get_publish_table_ids=get_publish_table_ids_clinical,
                           find_most_recent_published_table_id=find_most_recent_published_table_id,
-                          overwrite=True,
-                          test_mode=True)
+                          overwrite=True)
 
     end = time.time() - start_time
     print(f"Finished program execution in {format_seconds(end)}!\n")
