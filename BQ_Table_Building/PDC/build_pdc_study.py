5f09cdcef3c578aabe7070bc49d337332520ff37
laurenwolfe-isb <lwolfe@systemsbiology.org>
2021-05-26 11:49:13 -0700

    Removed embargoed clinical case rows -- their contents were null before, but it's a bit sloppy

--- BQ_Table_Building/PDC/build_pdc_study.py
+++ BQ_Table_Building/PDC/build_pdc_study.py
@@ -99,6 +99,7 @@ def alter_all_programs_json(all_programs_json_obj):
                 project['project_short_name'] = None
                 project['program_short_name'] = None
                 project['project_friendly_name'] = None
+                project['program_labels'] = None
                 print(f"""\n**Unmapped project submitter id: {project['project_submitter_id']}. 
                       Add to bq_params['PROJECT_MAP'] and rerun study workflow.\n""")
             else:
@@ -106,6 +107,7 @@ def alter_all_programs_json(all_programs_json_obj):
                 project['project_short_name'] = project_shortname_mapping['PROJECT_SHORT_NAME']
                 project['program_short_name'] = project_shortname_mapping['PROGRAM_SHORT_NAME']
                 project['project_friendly_name'] = project_shortname_mapping['PROJECT_FRIENDLY_NAME']
+                project['program_labels'] = project_shortname_mapping['PROGRAM_LABELS']
 
             studies = project.pop("studies", None)
             for study in studies:
