fae746c87e225d87312c3b02ca34aca1e16cbf65
laurenwolfe-isb <lwolfe@systemsbiology.org>
2021-06-02 14:10:32 -0700

    re-working per-sample file view

--- BQ_Table_Building/PDC/build_pdc_per_sample_file.py
+++ BQ_Table_Building/PDC/build_pdc_per_sample_file.py
@@ -12,61 +12,32 @@ YAML_HEADERS = ('api_params', 'bq_params', 'steps')
 
 
 def make_webapp_per_sample_view_query():
-    meta_dataset = f"{BQ_PARAMS['DEV_PROJECT']}.{BQ_PARAMS['META_DATASET']}"
+    dev_meta_dataset = f"{BQ_PARAMS['DEV_PROJECT']}.{BQ_PARAMS['META_DATASET']}"
+    prod_meta_dataset = f"{BQ_PARAMS['PROD_PROJECT']}.{BQ_PARAMS['PUBLIC_META_DATASET']}"
 
     file_metadata_table_name = construct_table_name(API_PARAMS, prefix=BQ_PARAMS['FILE_METADATA_TABLE'])
-    file_metadata_table_id = f"{meta_dataset}.{file_metadata_table_name}"
+    file_metadata_table_id = f"{prod_meta_dataset}.{file_metadata_table_name}"
 
     file_assoc_table_name = construct_table_name(API_PARAMS, prefix=BQ_PARAMS['FILE_ASSOC_MAPPING_TABLE'])
-    file_assoc_table_id = f"{meta_dataset}.{file_assoc_table_name}"
+    file_assoc_table_id = f"{prod_meta_dataset}.{file_assoc_table_name}"
 
     aliquot_table_name = construct_table_name(API_PARAMS, prefix=BQ_PARAMS['ALIQUOT_TO_CASE_TABLE'])
-    aliquot_table_id = f"{meta_dataset}.{aliquot_table_name}"
+    aliquot_table_id = f"{prod_meta_dataset}.{aliquot_table_name}"
 
     study_table_name = construct_table_name(API_PARAMS, prefix=get_prefix(API_PARAMS, 'allPrograms'))
-    study_table_id = f"{meta_dataset}.{study_table_name}"
-
-    """
-    SELECT fm.file_id AS file_pdc_id, # changed
-    fa.case_id AS case_pdc_id, # changed
-    ac.sample_id AS sample_pdc_id, # changed
-    ac.sample_submitter_id AS sample_barcode, # changed 
-    ac.project_name AS project_short_name, # changed
-    fm.file_type AS file_type, # added
-    fm.file_size, # added
-    """
+    study_table_id = f"{dev_meta_dataset}.{study_table_name}"
 
     return f"""
-        SELECT fm.file_id AS file_pdc_id,  
-            fa.case_id AS case_pdc_id, 
-            ac.case_submitter_id AS case_barcode, 
-            ac.sample_id AS sample_pdc_id, 
-            ac.sample_submitter_id AS sample_barcode, 
-            ac.sample_type, 
-            ac.project_name AS project_short_name, 
-            CAST(NULL AS STRING) AS project_name_suffix, 
-            s.program_short_name AS program_name,
-            fm.file_type AS data_type,
-            fm.data_category, 
-            fm.experiment_type AS experimental_strategy,
-            fm.file_type AS file_type,
-            fm.file_size,
-            fm.file_format AS data_format, 
-            fm.instrument AS platform, 
-            fm.file_name as file_name_key, 
-            CAST(NULL AS STRING) AS index_file_id, 
-            CAST(NULL AS STRING) AS index_file_name_key, 
-            CAST(NULL AS STRING) AS index_file_size,
-            CAST(NULL AS STRING) AS cloud_path, 
-            fm.`access`,
-            CAST(NULL AS STRING) AS acl
+        SELECT fm.file_id, fa.case_id, ac.case_submitter_id, ac.sample_id, ac.sample_submitter_id, ac.sample_type,
+            s.project_short_name, s.program_short_name, fm.file_type, fm.data_category, fm.experiment_type, 
+            fm.file_type, fm.file_size, fm.file_format, fm.instrument, fm.file_name, fm.`access`
         FROM `{file_metadata_table_id}` fm
         JOIN `{file_assoc_table_id}` fa
-            ON fm.file_id = fa.file_id
+        ON fm.file_id = fa.file_id
         JOIN `{aliquot_table_id}` ac
-            ON fa.case_id = ac.case_id
+        ON fa.case_id = ac.case_id
         JOIN `{study_table_id}` s
-            ON s.project_name = ac.project_name
+        ON s.project_name = ac.project_name
         """
 
 
