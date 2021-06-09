61bb650370a529a80d3bff894fc8d4b247e374ad
laurenwolfe-isb <lwolfe@systemsbiology.org>
2021-06-09 16:45:07 -0700

    Publish test per sample file

--- BQ_Table_Building/PDC/build_pdc_study.py
+++ BQ_Table_Building/PDC/build_pdc_study.py
@@ -169,7 +169,7 @@ def modify_study_table():
     res = get_query_results(query)
 
     for row in res:
-        print(row)
+        print(dict(row))
         print(row['primary_site'])
         print(row['pdc_study_id'])
         print(row['disease_type'])
