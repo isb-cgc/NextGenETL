87a95f63261fca6a775419ed65805d31bd6da1b2
eleeisb <elaine.lee@systemsbiology.org>
2021-07-01 11:03:30 -0700

    file name list updated

--- TP53/TP53_Scripts/env_setup.sh
+++ TP53/TP53_Scripts/env_setup.sh
@@ -3,3 +3,4 @@ cd ~/NextGenETL/TP53
 #gsutil cp gs://tp53-data-files/etl/*.csv P53_Database/P53_data_csv/.
 export GOOGLE_APPLICATION_CREDENTIALS='/home/elainelee/secret_keys/isb-cgc-tp53-test.key.json'
 export GCLOUD_PROJECT='isb-cgc-tp53-test'
+
