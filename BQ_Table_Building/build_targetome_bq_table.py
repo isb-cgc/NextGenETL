100e1e93ddc09da4eec811b6950f119e6fcc137d
John Phan <john.phan@gdit.com>
2021-07-21 20:46:41 +0000

    update description steps of targetome pipeline

--- BQ_Table_Building/build_targetome_bq_table.py
+++ BQ_Table_Building/build_targetome_bq_table.py
@@ -58,28 +58,10 @@ def load_config(yaml_config):
     return yaml_dict['files_and_buckets_and_tables'], yaml_dict['bq_filters'], yaml_dict['no_data_values'], yaml_dict['steps']
 
 
-
 '''
 ----------------------------------------------------------------------------------------------
-Convert excel files to CSV files
+Fix null values
 '''
-def convert_excel_to_csv(all_files, local_files_dir):
-
-    for filename in all_files:
-        print(filename)
-        page_dict = pd.read_excel(filename, None)
-        print(page_dict.keys())
-        _, just_name = os.path.split(filename)
-        for k, v in page_dict.items():
-            print("{}/{}-{}.tsv".format(local_files_dir, k, just_name))
-            print(v)
-            #v.to_csv("{}/{}-{}.tsv".format(local_files_dir, k, just_name), sep = "\t", index = None, header=True)
-
-'''
-----------------------------------------------------------------------------------------------
-# ### Fix null values
-'''
-
 def fix_null_values(orig_file, fixed_file, na_values):
     print("processing {}".format(fixed_file))
     with open(fixed_file, 'w') as outfile:
@@ -93,100 +75,6 @@ def fix_null_values(orig_file, fixed_file, na_values):
                 outfile.write('\n')
     return
 
-'''
-----------------------------------------------------------------------------------------------
-Build a header for the bioclin files
-
-'''
-
-def build_a_header(all_files):
-    all_fields = set()
-    per_file = {}
-    for filename in all_files:
-        per_file[filename] = []
-        with open(filename, 'r', encoding="ISO-8859-1") as readfile: # Having a problem with UTF-8
-            header_lines = []
-            for line in readfile:
-                # if we run into one field that is a pure number, it is no longer a header line
-                split_line = line.rstrip('\n').split("\t")
-                header_lines.append(split_line)
-                if split_line[0].startswith("CDE_ID"):
-                    for i in range(len(split_line)):
-                        if split_line[i] == 'CDE_ID:':
-                            per_file[filename].append(header_lines[0][i])
-                        else:
-                            per_file[filename].append(split_line[i])
-                    all_fields.update(per_file[filename])
-                    break
-
-    return all_fields, per_file
-
-'''
-----------------------------------------------------------------------------------------------
-There are a mixture of files, each with a different schema. Group the files into the different
-sets
-'''
-
-def group_by_suffixes(all_files):
-
-    full_and_name = []
-    names_only = []
-    for filename in all_files:
-        path, just_name = os.path.split(filename)
-        full_and_name.append((filename, just_name))
-        names_only.append(just_name)
-
-    prefix = longest_common_prefix(names_only)
-
-    path_suff = []
-    for tup in full_and_name:
-        path_suff.append((tup[0], tup[1][len(prefix):]))
-
-    path_group = []
-    groups = set()
-    p = re.compile('(^.*)_[a-z]+\.txt')
-    for tup in path_suff:
-        m = p.match(tup[1])
-        group = m.group(1)
-        path_group.append((tup[0], group))
-        groups.add(group)
-
-    files_by_group = {}
-
-    for file_tup in path_group:
-        if file_tup[1] not in files_by_group:
-            files_by_group[file_tup[1]] = []
-        files_by_group[file_tup[1]].append(file_tup[0])
-
-    return files_by_group
-
-'''
-----------------------------------------------------------------------------------------------
-Merge Skeleton With Aliquot Data
-Creates the final BQ table by joining the skeleton with the aliquot ID info
-'''
-
-def join_with_aliquot_table(cnv_table, aliquot_table, target_dataset, dest_table, do_batch):
-    return
-
-
-'''
-----------------------------------------------------------------------------------------------
-Hat tip to:
-https://www.w3resource.com/python-exercises/basic/python-basic-1-exercise-70.php
-'''
-def longest_common_prefix(str1):
-    if not str1:
-        return ""
-
-    short_str = min(str1, key=len)
-
-    for i, char in enumerate(short_str):
-        for other in str1:
-            if other[i] != char:
-                return short_str[:i]
-
-    return short_str
 
 '''
 ----------------------------------------------------------------------------------------------
@@ -309,15 +197,8 @@ def main(args):
     if 'analyze_the_schema' in steps:
         print('analyze_the_schema')
         for f in all_files:
-        #for k in group_dict:
             base_name = os.path.splitext(os.path.basename(f))[0]
-            schema_file = "{}/{}/{}.json".format(
-                params['SCHEMA_REPO_LOCAL'], params['RAW_SCHEMA_DIR'], base_name
-            )
             typing_tups = build_schema(fixed_tsv.format(base_name), params['SCHEMA_SAMPLE_SKIPS'])
-            pprint.pprint(typing_tups)
-            #full_file_prefix = "{}/{}".format(params['PROX_DESC_PREFIX'], params['FINAL_TARGET_TABLE'])
-            #schema_dict_loc = "{}_schema.json".format(full_file_prefix)
             hold_schema_dict_for_file = hold_schema_dict.format(base_name)
             hold_schema_list_for_file = hold_schema_list.format(base_name)
             build_combined_schema(None, None,
@@ -330,15 +211,12 @@ def main(args):
         for f in all_files:
             base_name = os.path.splitext(os.path.basename(f))[0]
             upload_to_bucket(params['WORKING_BUCKET'], bucket_target_blob.format(base_name), fixed_tsv.format(base_name))
-#
+
     if 'create_bq_from_tsv' in steps:
         print('create_bq_from_tsv')
         for f in all_files:
             base_name = os.path.splitext(os.path.basename(f))[0]
-#        for k in group_dict:
             bucket_src_url = 'gs://{}/{}'.format(params['WORKING_BUCKET'], bucket_target_blob.format(base_name))
-#            with open(hold_schema_list.format(k), mode='r') as schema_hold_dict:
-#                typed_schema = json_loads(schema_hold_dict.read())
             schema_file = "{}/{}/{}.json".format(
                 params['SCHEMA_REPO_LOCAL'], params['RAW_SCHEMA_DIR'], base_name
             )
@@ -349,48 +227,42 @@ def main(args):
             print('table: ', base_name)
             csv_to_bq(schema, bucket_src_url, params['TARGET_DATASET'],
                       base_name, params['BQ_AS_BATCH'])
-#
-#    if 'add_aliquot_fields' in steps:
-#        print('add_aliquot_fields')
-#        full_target_table = '{}.{}.{}'.format(params['WORKING_PROJECT'],
-#                                              params['TARGET_DATASET'],
-#                                              params['TARGET_TABLE'])
-#        success = join_with_aliquot_table(full_target_table, params['ALIQUOT_TABLE'],
-#                                          params['TARGET_DATASET'], params['FINAL_TARGET_TABLE'], params['BQ_AS_BATCH'])
-#        if not success:
-#            print("Join job failed")
-#
+
     #
     # Update the per-field descriptions:
     #
 
-#    if 'update_field_descriptions' in steps:
-#        print('update_field_descriptions')
-#        full_file_prefix = "{}/{}".format(params['PROX_DESC_PREFIX'], params['FINAL_TARGET_TABLE'])
-#        schema_dict_loc = "{}_schema.json".format(full_file_prefix)
-#        schema_dict = {}
-#        with open(schema_dict_loc, mode='r') as schema_hold_dict:
-#            full_schema_list = json_loads(schema_hold_dict.read())
-#        for entry in full_schema_list:
-#            schema_dict[entry['name']] = {'description': entry['description']}
-#
-#        success = update_schema_with_dict(params['TARGET_DATASET'], params['FINAL_TARGET_TABLE'], schema_dict)
-#        if not success:
-#            print("update_field_descriptions failed")
-#            return
-#
+    if 'update_field_descriptions' in steps:
+        print('update_field_descriptions')
+        for f in all_files:
+            base_name = os.path.splitext(os.path.basename(f))[0]
+            full_file_prefix = "{}/{}".format(params['PROX_DESC_PREFIX'], base_name)
+            schema_dict_loc = "{}_schema.json".format(full_file_prefix)
+            schema_dict = {}
+            with open(schema_dict_loc, mode='r') as schema_hold_dict:
+                full_schema_list = json_loads(schema_hold_dict.read())
+            for entry in full_schema_list:
+                schema_dict[entry['name']] = {'description': entry['description']}
+
+            success = update_schema_with_dict(params['TARGET_DATASET'], base_name, schema_dict)
+            if not success:
+                print("update_field_descriptions failed")
+                return
+
     #
     # Add description and labels to the target table:
     #
 
-#    if 'update_table_description' in steps:
-#        print('update_table_description')
-#        full_file_prefix = "{}/{}".format(params['PROX_DESC_PREFIX'], params['FINAL_TARGET_TABLE'])
-#        success = install_labels_and_desc(params['TARGET_DATASET'], params['FINAL_TARGET_TABLE'], full_file_prefix)
-#        if not success:
-#            print("update_table_description failed")
-#            return
-#
+    if 'update_table_description' in steps:
+        print('update_table_description')
+        for f in all_files:
+            base_name = os.path.splitext(os.path.basename(f))[0]
+            full_file_prefix = "{}/{}".format(params['PROX_DESC_PREFIX'], base_name)
+            success = install_labels_and_desc(params['TARGET_DATASET'], base_name, full_file_prefix)
+            if not success:
+                print("update_table_description failed")
+                return
+
     #
     # publish table:
     #
@@ -417,9 +289,9 @@ def main(args):
 #        dump_tables = [params[x] for x in dump_table_tags]
 #        for table in dump_tables:
 #            delete_table_bq_job(params['TARGET_DATASET'], table)
-#
-#    print('job completed')
-#
+
+    print('job completed')
+
 if __name__ == "__main__":
     main(sys.argv)
 
