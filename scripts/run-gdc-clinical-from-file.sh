09274d554dc511175e2b68cace04129feea6c982
Lauren Wolfe <60630673+laurenwolfe-isb@users.noreply.github.com>
2021-08-11 13:57:52 -0700

    GDC file clinical add shell script

---
+++ scripts/run-gdc-clinical-from-file.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+source ~/setEnvVars.sh
+
+export MY_VENV=~/virtualEnvETL3_9
+export PYTHONPATH=.:${MY_VENV}/lib:~/extlib
+
+mkdir -p ~/config
+pushd ~/config > /dev/null
+gsutil cp gs://${CONFIG_BUCKET}/${CURRENT_CONFIG_PATH}/GDCFileClinicalTablesBuild.yaml .
+popd > /dev/null
+
+pushd ${MY_VENV} > /dev/null
+source bin/activate
+popd > /dev/null
+
+mkdir -p ~/scratch
+
+cd ..
+python3.9 ./BQ_Table_Building/build_gdc_file_clinical_tables.py ~/config/GDCFileClinicalTablesBuild.yaml
+deactivate
