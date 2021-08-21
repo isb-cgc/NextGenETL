509b9cf2051e569de99e34ca6c92bad627de4d77
Lauren Wolfe <60630673+laurenwolfe-isb@users.noreply.github.com>
2021-08-21 11:13:33 -0700

    raw tables building!

--- BQ_Table_Building/build_gdc_file_clinical_tables.py
+++ BQ_Table_Building/build_gdc_file_clinical_tables.py
@@ -564,7 +564,7 @@ def main(args):
                 if program == "TCGA":
                     file_type = "_".join(file_name.split('_')[2:-1])
                 elif program == "TARGET":
-                    file_type = "_".join(file_name.split("_")[3:-1])
+                    file_type = "_".join(file_name.split("_")[3:])
 
                 print(file_type)
 
