500466e9bb956379cd950bd155b574bcddedeae8
lhagen <lhagen@systemsbiology.org>
2022-07-15 15:03:22 -0700

    updated how the caller field is found

--- BQ_Table_Building/build_open_somatic_mut_bq_table.py
+++ BQ_Table_Building/build_open_somatic_mut_bq_table.py
@@ -414,6 +414,7 @@ def concat_all_files(all_files, one_big_tsv, program, callers, fields_to_fix):
                             #     outfile.write('\t')
                             #     outfile.write(callerName)
                             # else:
+                            print(caller_field_index)
                             caller_data = process_callers(line.rstrip('\n').split('\t')[caller_field_index], callers)
                             for caller in callers:
                                 outfile.write('\t')
