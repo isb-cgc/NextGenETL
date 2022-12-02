0dcc6fcfd791c88b30a9263d89974230c50f31c6
lhagen <lhagen@systemsbiology.org>
2022-12-02 10:54:58 -0800

    update file paths

--- BQ_Table_Building/build_open_somatic_mut_bq_table.py
+++ BQ_Table_Building/build_open_somatic_mut_bq_table.py
@@ -776,8 +776,8 @@ def main(args):
     hold_schema_list = f"{home}/{params['SCHEMA_REPO_LOCAL']}/{params['HOLD_SCHEMA_LIST']}"  # todo rename to appropriate file
     hold_schema_dict = f"{home}/{params['SCHEMA_REPO_LOCAL']}/{params['HOLD_SCHEMA_DICT']}"  # todo rename to appropriate file
     table_metadata = f"{home}/{params['SCHEMA_REPO_LOCAL']}/{params['SCHEMA_FILE_NAME']}"
-    metadata_mapping = f"{home}/{params['SCHEMA_REPO_LOCAL']}/{params['METADATA_MAPPINGS']}"
-    field_desc_fp = f"{home}/{params['SCHEMA_REPO_LOCAL']}/{params['FIELD_DESC_FILE']}"
+    metadata_mapping = f"params['SCHEMA_REPO_LOCAL']}/{params['METADATA_MAPPINGS']}"
+    field_desc_fp = f"{params['SCHEMA_REPO_LOCAL']}/{params['FIELD_DESC_FILE']}"
 
     # BigQuery Tables
     manifest_table = f"{params['DATA_TYPE']}_manifest_r{params['RELEASE']}"
