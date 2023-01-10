92f7a4c0a3e0711679cf34e14ffe2649d8969a4f
fseidl-gdit <57154819+fseidl-gdit@users.noreply.github.com>
2023-01-10 11:47:14 -0500

    Update publish_table.py

--- GDC-Metadata-Processing/scripts/publish_table.py
+++ GDC-Metadata-Processing/scripts/publish_table.py
@@ -35,11 +35,9 @@ def main(args):
     source_table = args[1]
     target_table = args[2]
     target_split = target_table.split('.')
-    try:
-        delete_table = delete_table_bq_job(target_split[1], target_split[2], target_split[0])
-    except:
-        print('table deletion failed')
-    publish_table(source_table, target_table)
+    try: overwrite = overwrite
+    except: overwrite = False
+    publish_table(source_table, target_table, overwrite)
 
 if __name__ == "__main__":
     main(sys.argv)
