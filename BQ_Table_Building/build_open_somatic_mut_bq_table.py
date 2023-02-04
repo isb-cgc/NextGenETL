12076111ca6d2e719c90d5e9bef768e6262392c4
lhagen <lhagen@systemsbiology.org>
2023-02-03 16:39:21 -0800

    updates to publish steps and function

--- BQ_Table_Building/build_open_somatic_mut_bq_table.py
+++ BQ_Table_Building/build_open_somatic_mut_bq_table.py
@@ -906,8 +906,7 @@ def main(args):
                                                        f"{params['PUBLICATION_PROJECT']}.{bq_dataset}_versioned.{standard_table}_{release}",
                                                        f"{params['PUBLICATION_PROJECT']}.{bq_dataset}.{standard_table}_current",
                                                        f"REL {str(params['RELEASE'])}",
-                                                       params['BQ_AS_BATCH'],
-                                                       {standard_table}")
+                                                       f"{standard_table}")
 
             if not success:
                 print("Publication step did not work")
