7975165b7a557c48737dde46292825169dda81a3
laurenwolfe-isb <lwolfe@systemsbiology.org>
2023-04-06 15:24:11 -0700

    CDA prototyping

--- common_etl/utils.py
+++ common_etl/utils.py
@@ -769,10 +769,10 @@ def load_create_table_job(bq_params, data_file, client, table_id, job_config):
     """
     gs_uri = f"gs://{bq_params['WORKING_BUCKET']}/{bq_params['WORKING_BUCKET_DIR']}/{data_file}"
 
-    print(f"Loading from {gs_uri}")
-
     try:
         load_job = client.load_table_from_uri(gs_uri, table_id, job_config=job_config)
+
+        print(load_job.errors())
 
         print(f' - Inserting into {table_id}... ', end="")
         await_insert_job(bq_params, client, table_id, load_job)
