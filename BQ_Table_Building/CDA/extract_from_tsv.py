acb51844f3f7a02f24b00aa07312ea6932ff7541
laurenwolfe-isb <lwolfe@systemsbiology.org>
2023-04-14 12:14:35 -0700

    Was accidentally deleting the tar file because I delete the extraction directory to prevent accidental duplication. Oops.

--- BQ_Table_Building/CDA/extract_from_tsv.py
+++ BQ_Table_Building/CDA/extract_from_tsv.py
@@ -179,7 +179,7 @@ def main(args):
     elif source_dc == "gdc":
         api_params = {
             "RELEASE": "2023_03",
-            "LOCAL_TAR_DIR": "scratch/cda_gdc",
+            "LOCAL_TAR_DIR": "scratch/cda_archive_files",
             "TAR_FILE": "2023_03_gdc_as_extracted.tgz"
         }
         bq_params = {
