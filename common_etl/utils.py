0d5b20fc64713883346e1ea8894094f675f46cf0
laurenwolfe-isb <lwolfe@systemsbiology.org>
2023-04-14 12:33:27 -0700

    Don't need timeout?

--- common_etl/utils.py
+++ common_etl/utils.py
@@ -857,6 +857,7 @@ def download_from_bucket(bq_params, filename, bucket_path=None, dir_path=None, t
 
     start_time = time.time()
 
+    """
     if timeout:
         while not os.path.isfile(file_path):
             if time.time() - start_time > timeout:
@@ -865,8 +866,10 @@ def download_from_bucket(bq_params, filename, bucket_path=None, dir_path=None, t
             else:
                 print(f"File {filename} not yet downloaded, waiting.")
                 time.sleep(2)
-        if os.path.isfile(file_path):
-            print(f"File successfully downloaded from bucket. Bucket location: {blob_name}, VM location: {file_path}")
+    """
+
+    if os.path.isfile(file_path):
+        print(f"File successfully downloaded from bucket to {file_path}")
 
 
 #   I/O - FILESYSTEM HELPERS
