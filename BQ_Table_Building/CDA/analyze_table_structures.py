d4ed3efe1183ea58a779f8ad40716ff8a0dd4211
laurenwolfe-isb <lwolfe@systemsbiology.org>
2023-04-18 11:57:43 -0700

    Analyze table structures

--- BQ_Table_Building/CDA/analyze_table_structures.py
+++ BQ_Table_Building/CDA/analyze_table_structures.py
@@ -1,18 +1,33 @@
+import csv
 import sys
 
 from common_etl.support import bq_harness_with_result
+from common_etl.utils import download_from_bucket, get_scratch_fp
 
 
 # tables, column names, data type
 # columns appearing in multiple tables
 
-def retrieve_dataset_columns(project_name, dataset_name):
+def retrieve_dataset_columns(project_name, dataset_name, version=None):
     table_column_query = f"""
         SELECT table_name, column_name, data_type
         FROM `{project_name}`.{dataset_name}.INFORMATION_SCHEMA.COLUMNS
     """
 
-    return bq_harness_with_result(sql=table_column_query, do_batch=False, verbose=False)
+    table_columns = bq_harness_with_result(sql=table_column_query, do_batch=False, verbose=False)
+
+    if not version:
+        return table_columns
+
+    filtered_table_columns = list()
+
+    for column_data in table_columns:
+        table_version = column_data[0][:7]
+
+        if table_version == version:
+            filtered_table_columns.append(column_data)
+
+    return filtered_table_columns
 
 
 def print_tables_columns_data_types(table_columns):
@@ -44,13 +59,31 @@ def print_columns_in_tables(table_columns, multiple_only=False):
             print(f"{column}\t{column_dict[column]}")
 
 
+def import_current_gdc_fields(bq_params):
+    filename = 'gdc_current_fields.tsv'
+    bucket_path = 'law/etl/analysis_files'
+    download_from_bucket(bq_params, filename, bucket_path)
+
+    with open(get_scratch_fp(bq_params, filename), mode="r") as fields_file:
+        tsv_reader = csv.reader(fields_file, delimiter="\t")
+
+        for row in tsv_reader:
+            print(f"{row[0]}\t{row[1]}\t{row[2]}")
+
+
 def main(args):
+    bq_params = {
+        "SCRATCH_DIR": "scratch"
+    }
     project_name = 'isb-project-zero'
     dataset_name = 'cda_gdc_test'
+    version = '2023_03'
 
-    table_columns = retrieve_dataset_columns(project_name, dataset_name)
+    # table_columns = retrieve_dataset_columns(project_name, dataset_name, version)
 
-    print_columns_in_tables(table_columns)
+    # print_columns_in_tables(table_columns)
+
+    import_current_gdc_fields(bq_params)
 
 
 if __name__ == "__main__":
