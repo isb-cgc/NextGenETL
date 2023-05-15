c2703a6108be7978b6230710acde412e4f2b74a0
laurenwolfe-isb <lwolfe@systemsbiology.org>
2023-05-15 13:16:16 -0700

    stitch together file metadata

--- BQ_Table_Building/CDA/transform_tables_file_metadata_gdc.py
+++ BQ_Table_Building/CDA/transform_tables_file_metadata_gdc.py
@@ -198,7 +198,10 @@ def create_file_metadata_table(bq_params, release):
     file_record_result = bq_harness_with_result(sql=make_base_file_metadata_sql(), do_batch=False, verbose=False)
 
     for row in file_record_result:
-        print(row.items())
+
+        file_gdc_id = row.get('file_gdc_id')
+
+        print(file_gdc_id)
         break
 
     """
