7363e85e50fecc68ab490681e2a9178ba50ba060
laurenwolfe-isb <lwolfe@systemsbiology.org>
2023-07-25 14:15:51 -0700

    test suite tweaks

--- BQ_Table_Building/CDA/tests/test_transform_tables_per_sample_file_gdc.py
+++ BQ_Table_Building/CDA/tests/test_transform_tables_per_sample_file_gdc.py
@@ -80,18 +80,18 @@ def main(args):
         print(f"For program: {program}")
         print("**********\n")
 
-        print("Comparing row counts!\n")
+        print("\n** Comparing row counts! **\n")
 
         compare_row_counts(old_table_id=gdc_table_id,
                            new_table_id=cda_table_id)
 
-        print("\nComparing primary table keys!\n")
+        print("\n** Comparing primary table keys! **\n")
 
         compare_id_keys(old_table_id=gdc_table_id,
                         new_table_id=cda_table_id,
                         primary_key=BQ_PARAMS['PRIMARY_KEY'])
 
-        print("\nComparing secondary table keys!\n")
+        print("\n** Comparing secondary table keys! **\n")
 
         compare_id_keys(old_table_id=gdc_table_id,
                         new_table_id=cda_table_id,
@@ -99,7 +99,7 @@ def main(args):
 
         columns_list = BQ_PARAMS["COLUMNS"]
 
-        print("Comparing table columns!\n")
+        print("\n** Comparing table columns! **\n")
 
         compare_table_columns(old_table_id=gdc_table_id,
                               new_table_id=cda_table_id,
@@ -107,11 +107,6 @@ def main(args):
                               secondary_key=BQ_PARAMS['SECONDARY_KEY'],
                               columns=columns_list)
 
-"""
-Testing notes:
-A given row will:
-- definitely have a file id and a case id that match, and either matching sample ids or null sample ids
-"""
 
 if __name__ == "__main__":
     main(sys.argv)
