a6e0a91be297410c80ecd4fb2247a07ac778a2f6
laurenwolfe-isb <lwolfe@systemsbiology.org>
2023-08-16 12:28:39 -0700

    pdc file metadata

--- BQ_Table_Building/CDA/PDC/create_tables_file_metadata_pdc.py
+++ BQ_Table_Building/CDA/PDC/create_tables_file_metadata_pdc.py
@@ -23,7 +23,7 @@ import sys
 import time
 
 from cda_bq_etl.utils import load_config, has_fatal_error, create_dev_table_id, format_seconds
-from cda_bq_etl.bq_helpers import load_table_from_query, publish_table
+from cda_bq_etl.bq_helpers import load_table_from_query, publish_table, update_table_schema_from_generic
 
 PARAMS = dict()
 YAML_HEADERS = ('params', 'steps')
@@ -35,10 +35,55 @@ def make_file_metadata_query() -> str:
     :return: sql query statement
     """
     return f"""
+        WITH file_instruments AS (
+            SELECT file_id, 
+                STRING_AGG(DISTINCT instrument, ';') AS instruments
+            FROM `{create_dev_table_id(PARAMS, 'file_instrument')}`
+            GROUP BY file_id
+        ), embargoes AS (
+            SELECT fs.file_id,
+                STRING_AGG(DISTINCT s.embargo_date, ';') AS embargo_dates
+            FROM `{create_dev_table_id(PARAMS, 'file_study_id')}` fs
+            JOIN `{create_dev_table_id(PARAMS, 'study')}` s
+                ON s.study_id = fs.study_id
+            GROUP BY fs.file_id
+        ), study_ids AS (
+            SELECT fs.file_id,
+                STRING_AGG(DISTINCT s.pdc_study_id, ';') as pdc_study_ids
+            FROM `{create_dev_table_id(PARAMS, 'file_study_id')}` fs
+            JOIN `{create_dev_table_id(PARAMS, 'study')}` s
+                ON s.study_id = fs.study_id
+            GROUP BY fs.file_id
+        )
+        
         SELECT f.file_id,
-        f.file_name,
-        FROM `isb-project-zero.cda_pdc_raw.2023_06_file` f
-        JOIN
+            f.file_name,
+            e.embargo_dates AS embargo_date,
+            si.pdc_study_ids,
+            srm.study_run_metadata_id,
+            srm.study_run_metadata_submitter_id,
+            f.file_format,
+            f.file_type,
+            f.data_category,
+            f.file_size,
+            f.fraction_number,
+            f.experiment_type,
+            f.plex_or_dataset_name,
+            f.analyte,
+            fi.instruments AS instrument,
+            f.md5sum,
+            "open" AS `access`
+        FROM `{create_dev_table_id(PARAMS, 'file')}` f
+        LEFT JOIN embargoes e
+            ON e.file_id = f.file_id
+        LEFT JOIN file_instruments fi
+            ON fi.file_id = f.file_id
+        LEFT JOIN study_ids si
+            ON si.file_id = f.file_id
+        LEFT JOIN `{create_dev_table_id(PARAMS, 'file_study_run_metadata_id')}` fsrm
+            ON fsrm.file_id = f.file_id
+        LEFT JOIN `{create_dev_table_id(PARAMS, 'study_run_metadata')}` srm
+            ON srm.study_run_metadata_id = fsrm.study_run_metadata_id
     """
 
 
@@ -51,7 +96,23 @@ def main(args):
 
     start_time = time.time()
 
-    # code here
+    dev_table_id = f"{PARAMS['DEV_PROJECT']}.{PARAMS['DEV_METADATA_DATASET']}.{PARAMS['TABLE_NAME']}_{PARAMS['RELEASE']}"
+
+    if 'create_table_from_query' in steps:
+        load_table_from_query(params=PARAMS, table_id=dev_table_id, query=make_file_metadata_query())
+
+        update_table_schema_from_generic(params=PARAMS, table_id=dev_table_id)
+
+    if 'publish_tables' in steps:
+        current_table_name = f"{PARAMS['TABLE_NAME']}_current"
+        current_table_id = f"{PARAMS['PROD_PROJECT']}.{PARAMS['PROD_DATASET']}.{current_table_name}"
+        versioned_table_name = f"{PARAMS['TABLE_NAME']}_{PARAMS['DC_RELEASE']}"
+        versioned_table_id = f"{PARAMS['PROD_PROJECT']}.{PARAMS['PROD_DATASET']}_versioned.{versioned_table_name}"
+
+        publish_table(params=PARAMS,
+                      source_table_id=dev_table_id,
+                      current_table_id=current_table_id,
+                      versioned_table_id=versioned_table_id)
 
     end_time = time.time()
 
