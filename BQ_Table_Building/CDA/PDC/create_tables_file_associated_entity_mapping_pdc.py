c84db9905a5e37dc618d2e9730c34610200cf811
laurenwolfe-isb <lwolfe@systemsbiology.org>
2023-08-15 17:05:10 -0700

    pdc file associated entity mapping

--- BQ_Table_Building/CDA/PDC/create_tables_file_associated_entity_mapping_pdc.py
+++ BQ_Table_Building/CDA/PDC/create_tables_file_associated_entity_mapping_pdc.py
@@ -23,7 +23,7 @@ import sys
 import time
 
 from cda_bq_etl.utils import load_config, has_fatal_error, create_dev_table_id, format_seconds
-from cda_bq_etl.bq_helpers import load_table_from_query, publish_table
+from cda_bq_etl.bq_helpers import load_table_from_query, publish_table, update_table_schema_from_generic
 
 PARAMS = dict()
 YAML_HEADERS = ('params', 'steps')
@@ -57,7 +57,23 @@ def main(args):
 
     start_time = time.time()
 
-    # code here
+    dev_table_id = f"{PARAMS['DEV_PROJECT']}.{PARAMS['DEV_METADATA_DATASET']}.{PARAMS['TABLE_NAME']}_{PARAMS['RELEASE']}"
+
+    if 'create_table_from_query' in steps:
+        load_table_from_query(params=PARAMS, table_id=dev_table_id, query=make_associated_entity_query())
+
+        update_table_schema_from_generic(params=PARAMS, table_id=dev_table_id)
+
+    if 'publish_tables' in steps:
+        current_table_name = f"{PARAMS['TABLE_NAME']}_current"
+        current_table_id = f"{PARAMS['PROD_PROJECT']}.{PARAMS['PROD_DATASET']}.{current_table_name}"
+        versioned_table_name = f"{PARAMS['TABLE_NAME']}_{PARAMS['DC_RELEASE']}"
+        versioned_table_id = f"{PARAMS['PROD_PROJECT']}.{PARAMS['PROD_DATASET']}_versioned.{versioned_table_name}"
+
+        publish_table(params=PARAMS,
+                      source_table_id=dev_table_id,
+                      current_table_id=current_table_id,
+                      versioned_table_id=versioned_table_id)
 
     end_time = time.time()
 
