1c28eafc6d24f02d8a11822dd1d69052d1983e8e
laurenwolfe-isb <lwolfe@systemsbiology.org>
2023-08-16 17:40:31 -0700

    per sample file

--- BQ_Table_Building/CDA/PDC/create_tables_per_sample_file_pdc.py
+++ BQ_Table_Building/CDA/PDC/create_tables_per_sample_file_pdc.py
@@ -115,6 +115,7 @@ def main(args):
 
     if 'create_project_tables' in steps:
         for project in projects_list:
+            print(project)
             project_table_name = f"{PARAMS['TABLE_NAME']}_{project['project_short_name']}_{PARAMS['RELEASE']}"
             project_table_id = f"{PARAMS['DEV_PROJECT']}.{PARAMS['DEV_SAMPLE_DATASET']}.{project_table_name}"
 
