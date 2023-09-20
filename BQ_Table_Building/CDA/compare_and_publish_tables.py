87ecac60f7f486303f13a9be411ffbe5ae363716
laurenwolfe-isb <lwolfe@systemsbiology.org>
2023-09-20 16:14:09 -0700

    Compare and publish tables: added compare_to_last output for added/removed/changed record counts

--- BQ_Table_Building/CDA/compare_and_publish_tables.py
+++ BQ_Table_Building/CDA/compare_and_publish_tables.py
@@ -23,6 +23,8 @@ import logging
 import sys
 import time
 
+from typing import Union
+
 from google.cloud.bigquery.table import _EmptyRowIterator
 
 from cda_bq_etl.bq_helpers import find_most_recent_published_table_id, exists_bq_table, exists_bq_dataset, \
@@ -32,6 +34,47 @@ from cda_bq_etl.utils import input_with_timeout, load_config, format_seconds
 
 PARAMS = dict()
 YAML_HEADERS = ('params', 'steps')
+
+
+def table_has_new_data(previous_table_id: str, current_table_id: str) -> bool:
+    """
+    Compare newly created table and existing published table. Only publish new table if there's a difference.
+    :param previous_table_id: table id for existing published table
+    :param current_table_id: table id for new table
+    :return:
+    """
+
+    def compare_two_tables_sql():
+        return f"""
+            (
+                SELECT * FROM `{previous_table_id}`
+                EXCEPT DISTINCT
+                SELECT * from `{current_table_id}`
+            )
+            UNION ALL
+            (
+                SELECT * FROM `{current_table_id}`
+                EXCEPT DISTINCT
+                SELECT * from `{previous_table_id}`
+            )
+        """
+
+    if not previous_table_id:
+        return True
+
+    compare_result = query_and_retrieve_result(sql=compare_two_tables_sql())
+
+    if isinstance(compare_result, _EmptyRowIterator):
+        # no distinct result rows, tables match
+        return False
+
+    if compare_result is None:
+        logger = logging.getLogger('base_script')
+        logger.info("No result returned for table comparison query. Often means that tables have differing schemas.")
+        return True
+
+    for row in compare_result:
+        return True if row else False
 
 
 def compare_tables(source_table_id: str, current_table_id: str, versioned_table_id: str):
@@ -46,50 +89,159 @@ def compare_tables(source_table_id: str, current_table_id: str, versioned_table_
     logger.info(f"Comparing tables {source_table_id} and {previous_versioned_table_id}.")
 
     # does source table exist?
-    if exists_bq_table(source_table_id):
-        logger.info("Source table id is valid.")
-    else:
+    if not exists_bq_table(source_table_id):
         logger.critical("Source table id doesn't exist, cannot publish.")
         sys.exit(-1)
 
     # does current dataset exist?
     current_dataset = ".".join(current_table_id.split('.')[:-1])
-    current_dataset_exists = exists_bq_dataset(current_dataset)
 
-    if current_dataset_exists:
-        logger.info(f"Dataset {current_dataset} is valid.")
-    else:
+    if not exists_bq_dataset(current_dataset):
         logger.critical(f"Dataset {current_dataset} doesn't exist, cannot publish.")
         sys.exit(-1)
 
     # does versioned dataset exist?
     versioned_dataset = ".".join(versioned_table_id.split('.')[:-1])
-    versioned_dataset_exists = exists_bq_dataset(versioned_dataset)
 
-    if versioned_dataset_exists:
-        logger.info(f"Dataset {versioned_dataset} is valid.")
-    else:
+    if not exists_bq_dataset(versioned_dataset):
         logger.critical(f"Dataset {versioned_dataset} doesn't exist, cannot publish.")
         sys.exit(-1)
 
+    logger.info("Previous published and source tables exist, great! Continuing.")
+
     # display published table_ids
-    logger.info("Published table_ids (to be created--not yet published):")
-    logger.info(f"current table_id: {current_table_id}")
-    logger.info(f"versioned table_id: {versioned_table_id}")
+    logger.info("To-be-published table_ids:")
+    logger.info(f"current: {current_table_id}")
+    logger.info(f"versioned: {versioned_table_id}")
 
     has_new_data = table_has_new_data(previous_versioned_table_id, source_table_id)
 
     # is there a previous version to compare with new table?
     # use previous_versioned_table_id
     if has_new_data:
-        logger.info(f"New data found compared to previous published table {previous_versioned_table_id}.")
-        logger.info("Table will be published.")
+        logger.info(f"New data found--table will be published.")
     elif not has_new_data:
-        logger.info(f"New table is identical to previous published table {previous_versioned_table_id}.")
-        logger.info("Table will not be published.")
+        logger.info(f"Tables are identical (no new data found)--table will not be published.")
 
 
-def find_changes_to_table(source_table_id: str, versioned_table_id: str):
+def find_record_difference_counts(table_type: str,
+                                  source_table_id: str,
+                                  versioned_table_id: str,
+                                  table_metadata: dict[str, Union[str, list[str]]]):
+    def make_record_count_query(table_id):
+        return f"""
+            SELECT COUNT(*) AS record_count
+            FROM `{table_id}`
+        """
+
+    def make_added_record_count_query():
+        return f"""
+            WITH new_rows AS (
+                SELECT * 
+                FROM `{source_table_id}`
+                EXCEPT DISTINCT
+                SELECT *
+                FROM `{previous_versioned_table_id}`
+            ), 
+            old_rows AS (
+                SELECT * 
+                FROM `{previous_versioned_table_id}`
+                EXCEPT DISTINCT
+                SELECT *
+                FROM `{source_table_id}`
+            )
+
+            # added aliquots
+            SELECT COUNT({primary_key}) AS changed_count, {output_key_string} 
+            FROM new_rows
+            WHERE {primary_key} NOT IN (
+                SELECT {primary_key} 
+                FROM old_rows
+            )
+            GROUP BY {output_key_string}
+            ORDER BY {output_key_string}
+        """
+
+    def make_removed_record_count_query():
+        return f"""
+            WITH new_rows AS (
+                SELECT * 
+                FROM `{source_table_id}`
+                EXCEPT DISTINCT
+                SELECT *
+                FROM `{previous_versioned_table_id}`
+            ), 
+            old_rows AS (
+                SELECT * 
+                FROM `{previous_versioned_table_id}`
+                EXCEPT DISTINCT
+                SELECT *
+                FROM `{source_table_id}`
+            )
+
+            # added aliquots
+            SELECT COUNT({primary_key}) AS changed_count, {output_key_string} 
+            FROM old_rows
+            WHERE {primary_key} NOT IN (
+                SELECT {primary_key} 
+                FROM new_rows
+            )
+            GROUP BY {output_key_string}
+            ORDER BY {output_key_string}
+        """
+
+    def make_changed_record_count_query():
+        return f"""
+            WITH new_rows AS (
+                SELECT * 
+                FROM `{source_table_id}`
+                EXCEPT DISTINCT
+                SELECT *
+                FROM `{previous_versioned_table_id}`
+            ), 
+            old_rows AS (
+                SELECT * 
+                FROM `{previous_versioned_table_id}`
+                EXCEPT DISTINCT
+                SELECT *
+                FROM `{source_table_id}`
+            ), intersects AS (
+                SELECT {primary_key}, {secondary_key} {output_key_string} 
+                FROM old_rows
+                INTERSECT DISTINCT
+                SELECT {primary_key}, {secondary_key} {output_key_string} 
+                FROM new_rows
+            )
+
+            SELECT COUNT({primary_key}) AS changed_count, {output_key_string}
+            FROM intersects
+            GROUP BY {output_key_string}
+            ORDER BY {output_key_string}
+        """
+
+    def compare_records(query: str) -> tuple[int, str]:
+        # find added/removed/changed records by project
+        result = query_and_retrieve_result(query)
+
+        total_results = 0
+        num_columns = len(table_metadata['output_keys']) + 1
+        output_string = ""
+
+        if result.total_rows > 0:
+            for row in result:
+                total_results += row[0]
+
+                # append the count, right justify
+                row_str = f"{row[0]:>10}"
+
+                # append the other values (e.g. project id, type) as specified in output keys
+                for i in range(1, num_columns):
+                    row_str += f"{row[i]:30}"
+
+                output_string += '\n' + row_str
+
+        return total_results, output_string
+
     logger = logging.getLogger('base_script')
 
     previous_versioned_table_id = find_most_recent_published_table_id(PARAMS, versioned_table_id)
@@ -98,140 +250,74 @@ def find_changes_to_table(source_table_id: str, versioned_table_id: str):
         logger.warning(f"No previous table found for {versioned_table_id}; therefore, no changes to report.")
         return
 
-    # gather two sets of data. all the values from these two queries.
-    # where primary key exists in previous but not current, the record was removed.
-    # where key exists in current but not previous, the record was added.
-    # where key exists in both current and previous, the record was changed.
-    # for project_id and any secondary display key, store count added, removed, changed
-    """
-    { 
-        'added':
-            (project_id, sample_type_name): count
-            ...
-        'deleted':
-            (project_id, sample_type_name): count
-            ...
-        'changed':
-            (project_id, sample_type_name): count
-            ...            
-    """
+    output_key_string = ",".join(table_metadata['output_keys'])
+    primary_key = table_metadata['primary_key']
 
-    # Get total row count--do 2x (once for each release)
-    """
-    SELECT COUNT(*) as record_count
-      FROM `isb-project-zero.GDC_metadata.rel35_fileData_current`
-    """
+    if table_metadata['secondary_key'] is not None:
+        secondary_key = table_metadata['secondary_key'] + ', '
+    else:
+        secondary_key = ''
 
-    #then display old rel count, new rel count, difference between versions
+    # get record count from previous versioned table
+    previous_version_count_result = query_and_retrieve_result(make_record_count_query(previous_versioned_table_id))
 
-    # For added aliquots (really portions)
-    # aliquot matches for r35, r37
-    # slide matches for r35, r37
-    # case matches for r35, r37
-    # file matches for r35, r37
-    # program_name, project_id, case_gdc_id, case_barcode, sample_gdc_id, sample_barcode, sample_type, sample_type_name
+    try:
+        previous_version_count = None
 
-    # dbName, file_gdc_id, access, acl, analysis_workflow_link, analysis_workflow_type,
+        for row in previous_version_count_result:
+            previous_version_count = row[0]
+            break
 
-    file_columns = """
-    dbName, `access`, analysis_workflow_link, analysis_workflow_type, archive_gdc_id, archive_revision, archive_state,
-    archive_submitter_id, associated_entities__entity_type, project_dbgap_accession_number, project_disease_type, 
-    project_name, program_dbgap_accession_number, program_name, project_short_name, created_datetime, data_category,
-    data_format, data_type, experimental_strategy, file_name, file_size, file_id, index_file_gdc_id, index_file_name,
-    index_file_size, md5sum, platform, file_state, file_submitter_id, file_type, updated_datetime
-    """
+        if previous_version_count is None:
+            raise TypeError
+    except TypeError:
+        logger.critical(f"No value returned for previous version row count in {previous_versioned_table_id}.")
+        logger.critical("Probably an error in the table id or SQL query.")
+        sys.exit(-1)
 
-    """
-    WITH new_rows AS (
-      SELECT * 
-      FROM `isb-project-zero.GDC_metadata.rel35_aliquot2caseIDmap`
-      EXCEPT DISTINCT
-      SELECT *
-      FROM `isb-project-zero.GDC_metadata.rel34_aliquot2caseIDmap`
-    ), 
-    old_rows AS (
-      SELECT * 
-      FROM `isb-project-zero.GDC_metadata.rel34_aliquot2caseIDmap`
-      EXCEPT DISTINCT
-      SELECT *
-      FROM `isb-project-zero.GDC_metadata.rel35_aliquot2caseIDmap`
-    )
-    
-    # added aliquots
-    SELECT COUNT(portion_gdc_id) AS changed_count, project_id, sample_type_name 
-    FROM new_rows
-    WHERE portion_gdc_id NOT IN (
-        SELECT portion_gdc_id 
-        FROM old_rows
-    )
-    GROUP BY project_id, sample_type_name
-    ORDER BY project_id, sample_type_name
-    """
+    new_version_count_result = query_and_retrieve_result(make_record_count_query(source_table_id))
 
-    # For removed aliquots (really portions)
-    # aliquot matches for r35, r37
-    # slide matches for r35, r37 (no removed for either)
-    # case matches for r35, r37 (no removed for either)
-    """
-    WITH new_rows AS (
-      SELECT * 
-      FROM `isb-project-zero.GDC_metadata.rel35_aliquot2caseIDmap`
-      EXCEPT DISTINCT
-      SELECT *
-      FROM `isb-project-zero.GDC_metadata.rel34_aliquot2caseIDmap`
-    ), 
-    old_rows AS (
-      SELECT * 
-      FROM `isb-project-zero.GDC_metadata.rel34_aliquot2caseIDmap`
-      EXCEPT DISTINCT
-      SELECT *
-      FROM `isb-project-zero.GDC_metadata.rel35_aliquot2caseIDmap`
-    )
-    
-    # removed aliquots
-    SELECT COUNT(portion_gdc_id) AS changed_count, project_id, sample_type_name 
-    FROM old_rows
-    WHERE portion_gdc_id NOT IN (
-        SELECT portion_gdc_id 
-        FROM new_rows
-    )
-    GROUP BY project_id, sample_type_name
-    ORDER BY project_id, sample_type_name
-    """
+    try:
+        new_version_count = None
 
-    # intersects / changed aliquots
-    # aliquot: this works for r35 but not r37, what am I missing?
-    # slide: matches for r35 and r37, but no changes to either
-    # case: similar for r35 and r37, but some project_ids have differing counts (my counts are higher)
-    # file: way higher counts in mine and also some data formats in mine that aren't found in compare_to_last
-    """
-    WITH new_rows AS (
-        SELECT * 
-        FROM `isb-project-zero.GDC_metadata.rel35_aliquot2caseIDmap`
-        EXCEPT DISTINCT
-        SELECT *
-        FROM `isb-project-zero.GDC_metadata.rel34_aliquot2caseIDmap`
-    ), 
-    old_rows AS (
-        SELECT * 
-        FROM `isb-project-zero.GDC_metadata.rel34_aliquot2caseIDmap`
-        EXCEPT DISTINCT
-        SELECT *
-        FROM `isb-project-zero.GDC_metadata.rel35_aliquot2caseIDmap`
-    ), intersects AS (
-        SELECT aliquot_gdc_id, portion_gdc_id, project_id, sample_type_name 
-        FROM old_rows
-        INTERSECT DISTINCT
-        SELECT aliquot_gdc_id, portion_gdc_id, project_id, sample_type_name 
-        FROM new_rows
-    )
-    
-    # changed aliquots
-    SELECT COUNT(aliquot_gdc_id) AS changed_count, project_id, sample_type_name
-    FROM intersects
-    GROUP BY project_id, sample_type_name
-    ORDER BY project_id, sample_type_name
-    """
+        for row in new_version_count_result:
+            new_version_count = row[0]
+            break
+
+        if new_version_count is None:
+            raise TypeError
+    except TypeError:
+        logger.critical(f"No value returned for new version row count in {source_table_id}.")
+        logger.critical("Probably an error in the table id or SQL query.")
+        sys.exit(-1)
+
+    count_difference = int(new_version_count) - int(previous_version_count)
+
+    logger.info(f"***** {table_type.upper()} *****")
+    logger.info(f"Current {table_type} count: {new_version_count}")
+    logger.info(f"Previous {table_type} count: {previous_version_count}")
+    logger.info(f"Difference: {count_difference}")
+
+    # find added records by project
+    added_count, added_str = compare_records(query=make_added_record_count_query())
+
+    logger.info(f"Added {table_type} count: {added_count}")
+    if added_str:
+        logger.info(added_str)
+
+    # find removed records by project
+    removed_count, removed_str = compare_records(query=make_removed_record_count_query())
+
+    logger.info(f"Removed {table_type} count: {removed_count}")
+    if removed_str:
+        logger.info(removed_str)
+
+    # find changed records by project
+    changed_count, changed_str = compare_records(query=make_changed_record_count_query())
+
+    logger.info(f"Changed {table_type} count: {changed_count}")
+    if changed_str:
+        logger.info(changed_str)
 
 
 def get_change_details():
@@ -288,46 +374,6 @@ def publish_table(source_table_id: str, current_table_id: str, versioned_table_i
             logger.info(f"{source_table_id} not published, no changes detected")
 
 
-def table_has_new_data(previous_table_id: str, current_table_id: str) -> bool:
-    """
-    Compare newly created table and existing published table. Only publish new table if there's a difference.
-    :param previous_table_id: table id for existing published table
-    :param current_table_id: table id for new table
-    :return:
-    """
-    def compare_two_tables_sql():
-        return f"""
-            (
-                SELECT * FROM `{previous_table_id}`
-                EXCEPT DISTINCT
-                SELECT * from `{current_table_id}`
-            )
-            UNION ALL
-            (
-                SELECT * FROM `{current_table_id}`
-                EXCEPT DISTINCT
-                SELECT * from `{previous_table_id}`
-            )
-        """
-
-    if not previous_table_id:
-        return True
-
-    compare_result = query_and_retrieve_result(sql=compare_two_tables_sql())
-
-    if isinstance(compare_result, _EmptyRowIterator):
-        # no distinct result rows, tables match
-        return False
-
-    if compare_result is None:
-        logger = logging.getLogger('base_script')
-        logger.info("No result returned for table comparison query. Often means that tables have differing schemas.")
-        return True
-
-    for row in compare_result:
-        return True if row else False
-
-
 def main(args):
     try:
         start_time = time.time()
@@ -340,6 +386,33 @@ def main(args):
     log_file_time = time.strftime('%Y.%m.%d-%H.%M.%S', time.localtime())
     log_filepath = f"{PARAMS['LOGFILE_PATH']}.{log_file_time}"
     logger = initialize_logging(log_filepath)
+
+    for table_type in PARAMS['TABLE_TYPES'].keys():
+        table_metadata = PARAMS['TABLE_TYPES'][table_type]
+
+        prod_project = PARAMS['PROD_PROJECT']
+        prod_dataset = table_metadata['prod_dataset']
+        dev_project = PARAMS['DEV_PROJECT']
+        dev_dataset = PARAMS['DEV_METADATA_DATASET']
+        table_base_name = table_metadata['table_base_name']
+
+        current_table_id = f"{prod_project}.{prod_dataset}.{table_base_name}_current"
+        versioned_table_id = f"{prod_project}.{prod_dataset}_versioned.{table_base_name}_{PARAMS['DC_RELEASE']}"
+        source_table_id = f"{dev_project}.{dev_dataset}.{table_base_name}_{PARAMS['RELEASE']}"
+
+        if 'compare_tables' in steps:
+            # confirm that datasets and table ids exist, and preview whether table will be published
+            compare_tables(source_table_id, current_table_id, versioned_table_id)
+
+            # display compare_to_last style output
+            find_record_difference_counts(table_type, source_table_id, versioned_table_id, table_metadata)
+
+            # todo
+            get_change_details()
+
+        if 'publish_tables' in steps:
+            # todo
+            publish_table(source_table_id, current_table_id, versioned_table_id)
 
     end_time = time.time()
     logger.info(f"Script completed in: {format_seconds(end_time - start_time)}")
