b7038dfd8b95b4f62afb9131cf9ce098d2630d0e
laurenwolfe-isb <lwolfe@systemsbiology.org>
2023-10-25 15:03:13 -0700

    test compare

--- BQ_Table_Building/CDA/compare_and_publish_tables.py
+++ BQ_Table_Building/CDA/compare_and_publish_tables.py
@@ -784,7 +784,7 @@ def main(args):
     dev_project = PARAMS['DEV_PROJECT']
 
     # COMPARE AND PUBLISH METADATA TABLES
-    # """
+    """
     for table_type, table_params in PARAMS['METADATA_TABLE_TYPES'].items():
         prod_dataset = table_params['prod_dataset']
         prod_table_name = table_params['table_base_name']
