f4672cea6b9d6316f2bcbc71725be1e33bdcb7ac
laurenwolfe-isb <lwolfe@systemsbiology.org>
2023-10-30 16:43:44 -0700

    aliquot query change

--- BQ_Table_Building/CDA/GDC/create_tables_aliquot_case_map_gdc.py
+++ BQ_Table_Building/CDA/GDC/create_tables_aliquot_case_map_gdc.py
@@ -36,58 +36,75 @@ def make_aliquot_case_table_sql() -> str:
     :return: sql query statement
     """
     return f"""
+        WITH aliquot_records AS (
+            (
+                SELECT cpp.program_name, 
+                    cpp.project_id,
+                    cpp.case_gdc_id, 
+                    cpp.case_barcode,
+                    s.sample_id AS sample_gdc_id,
+                    s.submitter_id AS sample_barcode,
+                    s.sample_type_id AS sample_type,
+                    s.sample_type AS sample_type_name,
+                    s.is_ffpe AS sample_is_ffpe, 
+                    s.preservation_method AS sample_preservation_method,
+                    p.portion_id AS portion_gdc_id,
+                    p.submitter_id AS portion_barcode,
+                    an.analyte_id AS analyte_gdc_id,
+                    an.submitter_id AS analyte_barcode,
+                    al.aliquot_id AS aliquot_gdc_id,
+                    al.submitter_id AS aliquot_barcode        
+                FROM `{create_dev_table_id(PARAMS, 'sample')}` s
+                JOIN `{create_dev_table_id(PARAMS, 'sample_from_case')}` sfc
+                    ON s.sample_id = sfc.sample_id
+                JOIN `{create_dev_table_id(PARAMS, 'case_project_program')}` cpp
+                    ON sfc.case_id = cpp.case_gdc_id
+                JOIN `{create_dev_table_id(PARAMS, 'portion_from_sample')}` pfs
+                    ON s.sample_id = pfs.sample_id
+                JOIN `{create_dev_table_id(PARAMS, 'portion')}` p 
+                    ON pfs.portion_id = p.portion_id
+                LEFT JOIN `{create_dev_table_id(PARAMS, 'analyte_from_portion')}` afp
+                    ON p.portion_id = afp.portion_id
+                LEFT JOIN `{create_dev_table_id(PARAMS, 'analyte')}` an
+                    ON afp.analyte_id = an.analyte_id
+                LEFT JOIN `{create_dev_table_id(PARAMS, 'aliquot_of_analyte')}` aoa
+                    ON an.analyte_id = aoa.analyte_id
+                LEFT JOIN `{create_dev_table_id(PARAMS, 'aliquot')}` al
+                    ON aoa.aliquot_id = al.aliquot_id
+            ) UNION DISTINCT (
+                SELECT program_name, 
+                    project_id,
+                    case_gdc_id, 
+                    case_barcode,
+                    sample_gdc_id,
+                    sample_barcode,
+                    sample_type,
+                    sample_type_name,
+                    CAST(sample_is_ffpe AS BOOL), 
+                    sample_preservation_method,
+                    portion_gdc_id,
+                    portion_barcode,
+                    analyte_gdc_id,
+                    analyte_barcode,
+                    aliquot_gdc_id,
+                    aliquot_barcode  
+                FROM `{PARAMS['LEGACY_TABLE_ID']}` 
+            )
+        )
+        
         (
-            SELECT cpp.program_name, 
-                cpp.project_id,
-                cpp.case_gdc_id, 
-                cpp.case_barcode,
-                s.sample_id AS sample_gdc_id,
-                s.submitter_id AS sample_barcode,
-                s.sample_type_id AS sample_type,
-                s.sample_type AS sample_type_name,
-                s.is_ffpe AS sample_is_ffpe, 
-                s.preservation_method AS sample_preservation_method,
-                p.portion_id AS portion_gdc_id,
-                p.submitter_id AS portion_barcode,
-                an.analyte_id AS analyte_gdc_id,
-                an.submitter_id AS analyte_barcode,
-                al.aliquot_id AS aliquot_gdc_id,
-                al.submitter_id AS aliquot_barcode        
-            FROM `{create_dev_table_id(PARAMS, 'sample')}` s
-            JOIN `{create_dev_table_id(PARAMS, 'sample_from_case')}` sfc
-                ON s.sample_id = sfc.sample_id
-            JOIN `{create_dev_table_id(PARAMS, 'case_project_program')}` cpp
-                ON sfc.case_id = cpp.case_gdc_id
-            JOIN `{create_dev_table_id(PARAMS, 'portion_from_sample')}` pfs
-                ON s.sample_id = pfs.sample_id
-            JOIN `{create_dev_table_id(PARAMS, 'portion')}` p 
-                ON pfs.portion_id = p.portion_id
-            LEFT JOIN `{create_dev_table_id(PARAMS, 'analyte_from_portion')}` afp
-                ON p.portion_id = afp.portion_id
-            LEFT JOIN `{create_dev_table_id(PARAMS, 'analyte')}` an
-                ON afp.analyte_id = an.analyte_id
-            LEFT JOIN `{create_dev_table_id(PARAMS, 'aliquot_of_analyte')}` aoa
-                ON an.analyte_id = aoa.analyte_id
-            LEFT JOIN `{create_dev_table_id(PARAMS, 'aliquot')}` al
-                ON aoa.aliquot_id = al.aliquot_id
-        ) UNION DISTINCT (
-            SELECT program_name, 
-                project_id,
-                case_gdc_id, 
-                case_barcode,
-                sample_gdc_id,
-                sample_barcode,
-                sample_type,
-                sample_type_name,
-                CAST(sample_is_ffpe AS BOOL), 
-                sample_preservation_method,
-                portion_gdc_id,
-                portion_barcode,
-                analyte_gdc_id,
-                analyte_barcode,
-                aliquot_gdc_id,
-                aliquot_barcode  
-            FROM `{PARAMS['LEGACY_TABLE_ID']}` 
+            SELECT * 
+            FROM aliquot_records
+        )
+        EXCEPT DISTINCT 
+        (
+            SELECT *
+            FROM aliquot_records
+            WHERE aliquot_barcode is null
+                and aliquot_gdc_id is null
+                and analyte_barcode is null
+                and analyte_gdc_id is null
+                and portion_barcode is null 
         )
     """
 
