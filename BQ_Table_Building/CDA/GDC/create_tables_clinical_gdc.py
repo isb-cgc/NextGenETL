bd604682a508fe1ffcb9f89f842fff814101a103
laurenwolfe-isb <lwolfe@systemsbiology.org>
2023-11-06 12:19:56 -0800

    gdc file clinical

--- BQ_Table_Building/CDA/GDC/create_tables_clinical_gdc.py
+++ BQ_Table_Building/CDA/GDC/create_tables_clinical_gdc.py
@@ -761,9 +761,10 @@ def main(args):
     logger = initialize_logging(log_filepath)
 
     if 'find_missing_fields' in steps:
-        # logger.debug("Passing find_missing_fields")
+        logger.debug("Passing find_missing_fields")
         # Find discrepancies in field lists in yaml config and CDA data
         # find_missing_fields()
+
     if 'create_tables' in steps:
         # create dict of programs : base/supplemental tables to be created
         tables_per_program_dict = find_program_tables()
