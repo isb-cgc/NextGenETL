b66eeb456b90ebfa60a0f8a3fe96ebf3e01971b5
Lauren Hagen <hagen@systemsbiology.org>
2024-02-06 14:49:06 -0800

    update iteration

--- BQ_Table_Building/GDC/build_gdc_data_tables.py
+++ BQ_Table_Building/GDC/build_gdc_data_tables.py
@@ -301,7 +301,7 @@ def main(args):
     # Derived Data Steps
     for data_type in data_to_gather:
         if data_to_gather[data_type]:
-            for program in data_type:
+            for program in data_to_gather[data_type]:
                 build_bq_tables_steps(params, home, local_dir, workflow_run_ver, steps, data_type, program)
         else:
             logger.info(f"{data_type} will not run")
