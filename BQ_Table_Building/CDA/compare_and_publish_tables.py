80d5adde3c2996e9377be9751fb77f0712a0b1f0
laurenwolfe-isb <lwolfe@systemsbiology.org>
2024-03-01 15:21:42 -0800

    Adjusting string output formatting

--- BQ_Table_Building/CDA/compare_and_publish_tables.py
+++ BQ_Table_Building/CDA/compare_and_publish_tables.py
@@ -26,6 +26,7 @@ import sys
 import time
 
 from typing import Union
+from tabulate import tabluate
 
 from google.cloud.bigquery.table import _EmptyRowIterator
 
@@ -121,28 +122,28 @@ def list_added_or_removed_rows(select_table_id: str, join_table_id: str, table_m
         logger.info("")
         return
 
-    header_str = f"{table_metadata['primary_key']:50} "
+    header_str = f"{table_metadata['primary_key']:45}"
 
     if table_metadata['secondary_key'] is not None:
-        header_str += f"{table_metadata['secondary_key']:50} "
+        header_str += f"{table_metadata['secondary_key']:45}"
 
     if table_metadata['output_keys']:
         for output_key in table_metadata['output_keys']:
-            header_str += f"{output_key:50}"
+            header_str += f"{output_key:45} "
 
     logger.info(header_str)
 
     i = 0
 
     for row in row_result:
-        row_str = f"{row[table_metadata['primary_key']]:50} "
+        row_str = f"{row[table_metadata['primary_key']]:45}"
 
         if table_metadata['secondary_key']:
-            row_str += f"{row[table_metadata['secondary_key']]:50}"
+            row_str += f"{row[table_metadata['secondary_key']]:45}"
 
         if table_metadata['output_keys']:
             for output_key in table_metadata['output_keys']:
-                row_str += f"{row[output_key]:50}"
+                row_str += f"{row[output_key]:45}"
 
         logger.info(row_str)
 
@@ -790,9 +791,9 @@ def compare_table_columns(table_ids: dict[str, str], table_params: TableParams, 
 
             # output header row
             if secondary_key is None:
-                logger.info(f"{primary_key:50} {column} ")
+                logger.info(f"{primary_key:45} {column} ")
             else:
-                logger.info(f"{primary_key:50} {secondary_key:50} {column:50}")
+                logger.info(f"{primary_key:45} {secondary_key:45} {column:45}")
 
             i = 0
 
@@ -821,9 +822,9 @@ def compare_table_columns(table_ids: dict[str, str], table_params: TableParams, 
                     else:
                         secondary_key_val = str(old_second_key_val)
 
-                    logger.info(f"{primary_key_val:50} {secondary_key_val:50} {column_val}")
+                    logger.info(f"{primary_key_val:45} {secondary_key_val:45} {column_val}")
                 else:
-                    logger.info(f"{primary_key_val:50} {column_val}")
+                    logger.info(f"{primary_key_val:45} {column_val}")
 
                 i += 1
                 if i == max_display_rows:
@@ -986,10 +987,10 @@ def compare_concat_columns(table_ids: dict[str, str], table_params: TableParams,
                 old_column_header = f"old {column}"
 
                 if table_params['secondary_key'] is None:
-                    logger.info(f"{table_params['primary_key']:50} {old_column_header:50} {new_column_header}")
+                    logger.info(f"{table_params['primary_key']:45} {old_column_header:45} {new_column_header}")
                 else:
-                    logger.info(f"{table_params['primary_key']:50} {table_params['secondary_key']:50}"
-                                f" {old_column_header:50} {new_column_header}")
+                    logger.info(f"{table_params['primary_key']:45} {table_params['secondary_key']:45}"
+                                f" {old_column_header:45} {new_column_header}")
 
                 for mismatched_record in mismatched_records:
                     if ';' in mismatched_record['record_id']:
@@ -1001,11 +1002,11 @@ def compare_concat_columns(table_ids: dict[str, str], table_params: TableParams,
                         secondary_key_val = None
 
                     if table_params['secondary_key'] is None:
-                        logger.info(f"{primary_key_val:50} {mismatched_record['old_table_value']:50} "
+                        logger.info(f"{primary_key_val:45} {mismatched_record['old_table_value']:45} "
                                     f"{mismatched_record['new_table_value']}")
                     else:
-                        logger.info(f"{primary_key_val:50} {secondary_key_val:50} "
-                                    f"{mismatched_record['old_table_value']:50} {mismatched_record['new_table_value']}")
+                        logger.info(f"{primary_key_val:45} {secondary_key_val:45} "
+                                    f"{mismatched_record['old_table_value']:45} {mismatched_record['new_table_value']}")
 
                     i += 1
 
