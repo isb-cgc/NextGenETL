0567c580a44559b1fca99dc181646d332ef3f613
laurenwolfe-isb <lwolfe@systemsbiology.org>
2024-03-01 14:54:00 -0800

    Output for added or removed rows

--- BQ_Table_Building/CDA/compare_and_publish_tables.py
+++ BQ_Table_Building/CDA/compare_and_publish_tables.py
@@ -275,12 +275,10 @@ def find_record_difference_counts(table_type: str,
         query_logger.info(query)
         result = query_and_retrieve_result(query)
 
-        output_string = f"{'Count':10}"
+        output_string = f"\n{'Count':10}"
 
         for header in table_metadata['output_keys']:
             output_string += f"{header:30}"
-
-        output_string += "\n"
 
         total_results = 0
         num_columns = len(table_metadata['output_keys']) + 1
