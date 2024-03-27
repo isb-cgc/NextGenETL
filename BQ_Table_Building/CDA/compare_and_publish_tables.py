c1bbfa7b87ed0400b6d1be90fb82ac49f4a81919
laurenwolfe-isb <lwolfe@systemsbiology.org>
2024-03-27 16:42:14 -0700

    per sample file comparison fixes

--- BQ_Table_Building/CDA/compare_and_publish_tables.py
+++ BQ_Table_Building/CDA/compare_and_publish_tables.py
@@ -567,10 +567,7 @@ def find_missing_tables(dataset: str, table_type: str):
     for new_table_name in new_table_names:
         new_table_name = new_table_name.replace(f"{PARAMS['RELEASE']}_", "")
         new_table_name = new_table_name.replace(f"_{PARAMS['NODE']}", "")
-
-        # filter out legacy tables
-        if 'hg19' not in new_table_name:
-            new_table_names_no_rel.append(new_table_name)
+        new_table_names_no_rel.append(new_table_name)
 
     for current_table_name in published_table_names:
         if 'hg19' in current_table_name:
