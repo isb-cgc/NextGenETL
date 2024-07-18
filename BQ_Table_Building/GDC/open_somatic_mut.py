706be4df40fcb8945beaf39addb54acff9cac7ce
Lauren Hagen <hagen@systemsbiology.org>
2024-07-18 08:56:41 -0700

    updated cluster_fields

--- BQ_Table_Building/GDC/open_somatic_mut.py
+++ BQ_Table_Building/GDC/open_somatic_mut.py
@@ -488,7 +488,7 @@ def create_somatic_mut_table(raw_somatic_mut, draft_somatic_mut, file_table, ali
         sys.exit()
 
     # todo describe
-    cluster_fields = ["project_short_name", "case_barcode", "sample_barcode", "aliquot_barcode"]
+    cluster_fields = ["project_short_name", "case_barcode", "sample_barcode_tumor", "aliquot_barcode_tumor"]
     cluster_table_result = cluster_table(f"{project_id}.{dataset}.{step_3_table}",
                                          f"{project_id}.{dataset}.{draft_somatic_mut}", cluster_fields)
     if cluster_table_result == 'DONE':
