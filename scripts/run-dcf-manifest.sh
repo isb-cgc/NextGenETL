6ffb26a4627d7a952996cbfbf30783503e72f2cc
laurenwolfe-isb <lwolfe@systemsbiology.org>
2024-08-09 14:35:28 -0700

    publish new fileData legacy table

---
+++ scripts/run-dcf-manifest.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# Copyright 2024, Institute for Systems Biology
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+source ~/setEnvVars.sh
+
+export MY_VENV=~/virtualEnvETL3_9
+export PYTHONPATH=.:${MY_VENV}/lib:~/extlib
+
+mkdir -p ~/config
+pushd ~/config > /dev/null
+gsutil cp gs://${CONFIG_BUCKET}/${CURRENT_CONFIG_PATH}/${CONFIG_FILE} .
+popd > /dev/null
+
+pushd ${MY_VENV} > /dev/null
+source bin/activate
+popd > /dev/null
+
+mkdir -p ~/scratch
+
+cd ..
+python3.9 ./BQ_Table_Building/DCF/build_dcf_manifest_bq_tables.py ~/config/${CONFIG_FILE}
+deactivate
