3f7f541c03a2babc2a61f02187c942a6d91dbbab
laurenwolfe-isb <lwolfe@systemsbiology.org>
2024-09-30 15:29:55 -0700

    update clinical friendly names for gdc r38

--- BQ_Table_Building/CDA/run_maint_tasks.py
+++ BQ_Table_Building/CDA/run_maint_tasks.py
@@ -25,7 +25,7 @@ import time
 
 from cda_bq_etl.data_helpers import initialize_logging
 from cda_bq_etl.utils import load_config, input_with_timeout
-from cda_bq_etl.bq_helpers import query_and_retrieve_result, delete_bq_table, copy_bq_table
+from cda_bq_etl.bq_helpers import query_and_retrieve_result, delete_bq_table, copy_bq_table, update_friendly_name
 
 PARAMS = dict()
 YAML_HEADERS = ('params', 'steps')
@@ -71,6 +71,11 @@ def restore_deleted_table(deleted_table_id, new_table_id, snapshot_epoch):
     copy_bq_table(PARAMS, src_table=snapshot_table_id, dest_table=new_table_id)
 
 
+def update_friendly_names(friendly_name_dict):
+    for table_id, friendly_name in friendly_name_dict:
+        update_friendly_name(PARAMS, table_id, friendly_name)
+
+
 def main(args):
     try:
         global PARAMS
@@ -93,6 +98,10 @@ def main(args):
         new_table_id = 'isb-project-zero.cda_gdc_imported.r37_case_metadata_legacy'
         snapshot_epoch = 1697655753782
         restore_deleted_table(deleted_table_id, new_table_id, snapshot_epoch)
+
+    if 'update_friendly_names' in steps:
+        friendly_name_dict = PARAMS['FRIENDLY_NAME_DICT']
+        update_friendly_names(friendly_name_dict)
 
 
 if __name__ == "__main__":
