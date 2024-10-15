3acaef990aaaed5ffda818033ff77ab2c99d17bf
Lauren Hagen <hagen@systemsbiology.org>
2024-10-15 16:55:11 -0700

    Updates to documentation

--- BQ_Table_Building/GDC/mirna_isoform_expr.py
+++ BQ_Table_Building/GDC/mirna_isoform_expr.py
@@ -17,12 +17,18 @@ from gdc_file_utils import query_bq, cluster_table
 
 mirna_isoform_expr_logger = logging.getLogger(name='base_script.mirna_isoform_expr')
 
-'''First BQ Processing: Add Aliquot IDs
-The GDC file UUID for the isoform file was pulled from the bucket path.
-We need aliquots, samples, and associated barcodes. We get this by first attaching
-the aliquot IDs using the file table that provides aliquot UUIDs for files.'''
 
 def attach_aliquot_ids(input_table, file_table, output_table):
+    """
+    SQL query for adding GDC file metadata to the table. GDC file UUID for the file
+    is pulled from the bucket path. We need aliquots, samples, and associated barcodes.
+    We get this by first attaching the aliquot IDs using the file table that provides
+    aliquot UUIDs for files.
+    :param input_table: Table id with miRNA isoform seq data
+    :param file_table: GDC metadata table id with file metadata
+    :param output_table: Table id to create
+    :return: If the function worked
+    """
     sql = f'''
         WITH
         a1 AS (SELECT DISTINCT LEFT(file_name, 36) as file_gdc_id FROM `{input_table}`),        
@@ -40,12 +46,17 @@ def attach_aliquot_ids(input_table, file_table, output_table):
         '''
     return query_bq(sql, output_table)
 
-'''
-----------------------------------------------------------------------------------------------
-Add Barcodes
-# With the aliquot UUIDs known, we can now use the aliquot table to glue in sample info
-'''
+
 def attach_barcodes(input_table, aliquot_table, case_table, output_table):
+    """
+    Create a table with sample information. DISTINCT was added to the query to avoid duplicates in aliquots when
+    tables were joined together.
+    :param input_table: Table id with miRNA isoform seq data
+    :param aliquot_table: Metadata table id with aliquot data
+    :param case_table: Metadata table id with case data
+    :param output_table: Table id to create
+    :return: If the function worked
+    """
     sql = f'''
         WITH a1 AS (
             SELECT DISTINCT 
@@ -82,8 +93,15 @@ def attach_barcodes(input_table, aliquot_table, case_table, output_table):
 Glue the New Info to the Original Table
 All the new info we have pulled together goes in the first columns of the final table
 '''
-def final_merge(input_table, barcode_table, output_table):
-
+def join_barcode_and_raw_tables(input_table, barcode_table, output_table):
+    """
+    Glue the New Info to the Original Table
+    All the new info we have pulled together goes in the first columns of this table.
+    :param input_table: Table id with miRNA isoform seq data
+    :param barcode_table: Metadata table id with barcode data
+    :param output_table: Table id to create
+    :return: If the function worked
+    """
     sql = f'''
         SELECT
             a.project_short_name,
@@ -113,6 +131,12 @@ def final_merge(input_table, barcode_table, output_table):
 
 
 def merge_samples_by_aliquot(input_table, output_table):
+    """
+    Aggregate sample barcodes and ids based on aliquots
+    :param input_table: Combined table id with metadata and miRNA seq data
+    :param output_table: Table id to create
+    :return: If the function worked
+    """
     sql = f'''
     SELECT 
         project_short_name,
@@ -191,10 +215,10 @@ def create_mirna_isoform_expr_table(raw_isoform_mirna_expr, draft_mirna_isoform_
         mirna_isoform_expr_logger.error("Creating miRNA Seq aliquot barcode table failed")
         sys.exit()
 
-    final_merge_results = final_merge(f"{project_id}.{dataset}.{raw_isoform_mirna_expr}",
+    join_barcode_and_raw_tables_results = join_barcode_and_raw_tables(f"{project_id}.{dataset}.{raw_isoform_mirna_expr}",
                                       f"{project_id}.{dataset}.{step_2_table}",
                                       f"{project_id}.{dataset}.{step_3_table}")
-    if final_merge_results == 'DONE':
+    if join_barcode_and_raw_tables_results == 'DONE':
         created_tables.append(step_3_table)
     else:
         mirna_isoform_expr_logger.error("Creating miRNA Seq merged barcode table failed")
