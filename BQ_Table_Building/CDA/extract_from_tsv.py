74deabdbc257baf0c31c5f38f34bcd400371d219
laurenwolfe-isb <lwolfe@systemsbiology.org>
2024-11-07 15:05:13 -0800

    Changes for archive naming mismatch

--- BQ_Table_Building/CDA/extract_from_tsv.py
+++ BQ_Table_Building/CDA/extract_from_tsv.py
@@ -68,7 +68,7 @@ def extract_tarfile(src_path: str, dest_path: str, print_contents: bool = False,
                 logger.info(f"{tar_info.name}, {tar_info.size} bytes")
                 src_dir_set.add(tar_info.name.split("/")[0])
 
-    if src_dir_set > 1:
+    if len(src_dir_set) > 1:
         logger.critical(f"More than one base directory found: {src_dir_set}")
         sys.exit(-1)
 
