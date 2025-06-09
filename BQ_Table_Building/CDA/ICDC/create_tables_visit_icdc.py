ca0d677078897b0532a6e58968c36df1ce539aa9
laurenwolfe-isb <lwolfe@systemsbiology.org>
2025-06-09 15:38:15 -0700

    ICDC visit pipeline

--- BQ_Table_Building/CDA/ICDC/create_tables_visit_icdc.py
+++ BQ_Table_Building/CDA/ICDC/create_tables_visit_icdc.py
@@ -12,11 +12,13 @@ YAML_HEADERS = ('params', 'steps')
 # todo should this be a single or program-level table?
 def make_visit_sql() -> str:
     return f"""
-        SELECT * 
-        FROM `{create_dev_table_id(PARAMS, 'visit')}`
-        LEFT JOIN `{create_dev_table_id(PARAMS, 'case_visit_id')}`
+        SELECT v.visit_id, v.visit_date, v.visit_number, 
+            cv.case_id, 
+            ccv.case_id AS cycle_case_id
+        FROM `{create_dev_table_id(PARAMS, 'visit')}` v 
+        LEFT JOIN `{create_dev_table_id(PARAMS, 'case_visit_id')}` cv
           USING(visit_id)
-        LEFT JOIN `{create_dev_table_id(PARAMS, 'cycle_case_id_and_visit_id')}`
+        LEFT JOIN `{create_dev_table_id(PARAMS, 'cycle_case_id_and_visit_id')}` ccv
           USING(visit_id)
     """
 
@@ -53,11 +55,40 @@ def main(args):
     logger = initialize_logging(log_filepath)
 
     if 'retrieve_visit_data_and_build_jsonl' in steps:
+        visit_dict = dict()
+
         visit_result = query_and_retrieve_result(make_visit_sql())
 
-        visit_result_dict = dict(visit_result)
+        for row in visit_result:
+            visit_id = row['visit_id']
+            visit_date = row['visit_date']
+            visit_number = row['visit_number']
+            case_id = row['case_id']
+            cycle_case_id = row['cycle_case_id']
 
-        print(visit_result_dict)
+            # for whatever reason, the case_visit_id file only contains 30 associations.
+            # The cycle_case_and_visit_id table contains most of the needed case-visit associations.
+            # However, they seem to be mutually exclusive--most (all?) entries are only in one of the two files.
+            # Therefore, we're running a comparison here to flag any issues and to put the case_id in a single field.
+            if case_id and cycle_case_id and case_id != cycle_case_id:
+                # todo change to critical
+                logger.warning(f"Mismatched case_id, visit_case_id ({case_id}, {cycle_case_id} for visit_id: {visit_id}")
+            elif not case_id and not cycle_case_id:
+                logger.warning(f"No case_id match for visit_id: {visit_id}")
+            elif not case_id:
+                case_id = cycle_case_id
+
+            if visit_id in visit_dict:
+                logger.critical(f"visit id {visit_id} already found in raw data. This shouldn't happen--exiting.")
+
+            visit_dict[visit_id] = {
+                'visit_id': visit_id,
+                'visit_date': visit_date,
+                'visit_number': visit_number,
+                'case_id': case_id
+            }
+
+            print(visit_dict[visit_id])
 
     end_time = time.time()
     logger.info(f"Script completed in: {format_seconds(end_time - start_time)}")
