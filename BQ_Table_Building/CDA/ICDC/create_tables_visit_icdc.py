d767a48fbae3f2ea418e3306b92e7077dd875cd8
laurenwolfe-isb <lwolfe@systemsbiology.org>
2025-06-09 16:56:32 -0700

    ICDC visit pipeline nested fields

--- BQ_Table_Building/CDA/ICDC/create_tables_visit_icdc.py
+++ BQ_Table_Building/CDA/ICDC/create_tables_visit_icdc.py
@@ -12,7 +12,7 @@ YAML_HEADERS = ('params', 'steps')
 # todo should this be a single or program-level table?
 def make_visit_sql() -> str:
     return f"""
-        SELECT v.visit_id, v.visit_date, v.visit_number, 
+        SELECT v.visit_id, v.visit_date, 
             cv.case_id, 
             ccv.case_id AS cycle_case_id
         FROM `{create_dev_table_id(PARAMS, 'visit')}` v 
@@ -24,20 +24,27 @@ def make_visit_sql() -> str:
 
 
 def make_disease_extent_sql() -> str:
+    # todo handle the null field exclusions differently
     return f"""
-        SELECT * FROM `{create_dev_table_id(PARAMS, 'disease_extent')}`
+        SELECT * EXCEPT(longest_measurement_unit, longest_measurement_original, longest_measurement_original_unit, 
+        previously_treated, previously_irradiated)
+        FROM `{create_dev_table_id(PARAMS, 'disease_extent')}`
     """
 
 
 def make_physical_exam_sql() -> str:
+    # todo handle the null field exclusions differently
     return f"""
-        SELECT * FROM `{create_dev_table_id(PARAMS, 'physical_exam')}`
+        SELECT * EXCEPT(enrollment_id, day_in_cycle, phase_pe, assessment_timepoint)
+        FROM `{create_dev_table_id(PARAMS, 'physical_exam')}`
     """
 
 
 def make_vital_signs_sql() -> str:
+    # todo handle the null field exclusions differently
     return f"""
-        SELECT * FROM `{create_dev_table_id(PARAMS, 'vital_signs')}`
+        SELECT * 
+        FROM `{create_dev_table_id(PARAMS, 'vital_signs')}`
     """
 
 
@@ -62,19 +69,18 @@ def main(args):
         for row in visit_result:
             visit_id = row['visit_id']
             visit_date = row['visit_date']
-            visit_number = row['visit_number']
             case_id = row['case_id']
             cycle_case_id = row['cycle_case_id']
 
-            # for whatever reason, the case_visit_id file only contains 30 associations.
-            # The cycle_case_and_visit_id table contains most of the needed case-visit associations.
-            # However, they seem to be mutually exclusive--most (all?) entries are only in one of the two files.
-            # Therefore, we're running a comparison here to flag any issues and to put the case_id in a single field.
+            # The case_visit_id raw mapping file only contains 30 associations--cycle_case_and_visit_id contains most
+            # of the needed case-visit associations. However, they seem to be mutually exclusive--currently, the
+            # entries only exist in one of the two files. Therefore, we're running a comparison here to flag any future
+            # issues and to put the case_id in a single field.
             if case_id and cycle_case_id and case_id != cycle_case_id:
-                # todo change to critical
-                logger.warning(f"Mismatched case_id, visit_case_id ({case_id}, {cycle_case_id} for visit_id: {visit_id}")
+                logger.critical(f"Mismatched case_id, visit_case_id ({case_id}, {cycle_case_id} "
+                                f"for visit_id: {visit_id}. Exiting.")
             elif not case_id and not cycle_case_id:
-                logger.warning(f"No case_id match for visit_id: {visit_id}")
+                logger.warning(f"No case_id match for visit_id: {visit_id}. This warrants further investigation.")
             elif not case_id:
                 case_id = cycle_case_id
 
@@ -84,11 +90,105 @@ def main(args):
             visit_dict[visit_id] = {
                 'visit_id': visit_id,
                 'visit_date': visit_date,
-                'visit_number': visit_number,
-                'case_id': case_id
+                'case_id': case_id,
+                'disease_extent': list(),
+                'physical_exam': list(),
+                'vital_signs': list()
             }
 
-            print(visit_dict[visit_id])
+        disease_extent_result = query_and_retrieve_result(make_disease_extent_sql())
+
+        for row in disease_extent_result:
+            visit_id = row['visit_id']
+
+            if not visit_id:
+                logger.warning(f"No visit id {visit_id} found in disease_extent. Should be investigated. "
+                               f"Skipping this row.")
+            elif visit_id not in visit_dict:
+                logger.warning(f"visit id {visit_id} found in disease_extent raw data but not in visit. "
+                               "Should be investigated. Skipping this row.")
+
+            visit_dict[visit_id]['disease_extent'].append({
+                'lesion_number': row['lesion_number'],
+                'lesion_site': row['lesion_site'],
+                'lesion_description': row['lesion_description'],
+                'measurable_lesion': row['measurable_lesion'],
+                'target_lesion': row['target_lesion'],
+                'date_of_evaluation': row['date_of_evaluation'],
+                'measured_how': row['measured_how'],
+                'longest_measurement': row['longest_measurement'],
+                'evaluation_number': row['evaluation_number'],
+                'evaluation_code': row['evaluation_code']
+            })
+
+        physical_exam_result = query_and_retrieve_result(make_physical_exam_sql())
+
+        for row in physical_exam_result:
+            visit_id = row['visit_id']
+
+            if not visit_id:
+                logger.warning(f"No visit id {visit_id} found in physical_exam. Should be investigated. "
+                               f"Skipping this row.")
+            elif visit_id not in visit_dict:
+                logger.warning(f"visit id {visit_id} found in physical_exam raw data but not in visit. "
+                               "Should be investigated. Skipping this row.")
+
+            visit_dict[visit_id]['physical_exam'].append({
+                'date_of_examination': row['date_of_examination'],
+                'body_system': row['body_system'],
+                'pe_finding': row['pe_finding'],
+                'pe_comment': row['pe_comment']
+            })
+
+        vital_signs_result = query_and_retrieve_result(make_vital_signs_sql())
+
+        for row in vital_signs_result:
+            visit_id = row['visit_id']
+
+            if not visit_id:
+                logger.warning(f"No visit id {visit_id} found in vital_signs. Should be investigated. "
+                               f"Skipping this row.")
+            elif visit_id not in visit_dict:
+                logger.warning(f"visit id {visit_id} found in vital_signs raw data but not in visit. "
+                               "Should be investigated. Skipping this row.")
+
+            visit_dict[visit_id]['vital_signs'].append({
+                'date_of_vital_signs': row['date_of_vital_signs'],
+                'body_temperature': row['body_temperature'],
+                'body_temperature_unit': row['body_temperature_unit'],
+                'body_temperature_original': row['body_temperature_original'],
+                'body_temperature_original_unit': row['body_temperature_original_unit'],
+                'pulse': row['pulse'],
+                'pulse_unit': row['pulse_unit'],
+                'pulse_original': row['pulse_original'],
+                'pulse_original_unit': row['pulse_original_unit'],
+                'respiration_rate': row['respiration_rate'],
+                'respiration_rate_unit': row['respiration_rate_unit'],
+                'respiration_rate_original': row['respiration_rate_original'],
+                'respiration_rate_original_unit': row['respiration_rate_original_unit'],
+                'respiration_pattern': row['respiration_pattern'],
+                'systolic_bp': row['systolic_bp'],
+                'systolic_bp_unit': row['systolic_bp_unit'],
+                'systolic_bp_original': row['systolic_bp_original'],
+                'systolic_bp_original_unit': row['systolic_bp_original_unit'],
+                'pulse_ox': row['pulse_ox'],
+                'pulse_ox_unit': row['pulse_ox_unit'],
+                'pulse_ox_original': row['pulse_ox_original'],
+                'pulse_ox_original_unit': row['pulse_ox_original_unit'],
+                'patient_weight': row['patient_weight'],
+                'patient_weight_unit': row['patient_weight_unit'],
+                'patient_weight_original': row['patient_weight_original'],
+                'patient_weight_original_unit': row['patient_weight_original_unit'],
+                'body_surface_area': row['body_surface_area'],
+                'body_surface_area_unit': row['body_surface_area_unit'],
+                'body_surface_area_original': row['body_surface_area_original'],
+                'body_surface_area_original_unit': row['body_surface_area_original_unit'],
+                'modified_ecog': row['modified_ecog']
+            })
+
+    for visit in visit_dict.values():
+        print(visit)
+        print()
 
     end_time = time.time()
     logger.info(f"Script completed in: {format_seconds(end_time - start_time)}")
