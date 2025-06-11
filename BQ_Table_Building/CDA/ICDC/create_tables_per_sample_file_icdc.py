d7a610b7a0b0ac04d92f35ffb247ba4a5d1d9c77
laurenwolfe-isb <lwolfe@systemsbiology.org>
2025-06-10 17:08:30 -0700

    Make ICDC per_sample_file program level

--- BQ_Table_Building/CDA/ICDC/create_tables_per_sample_file_icdc.py
+++ BQ_Table_Building/CDA/ICDC/create_tables_per_sample_file_icdc.py
@@ -76,6 +76,9 @@ def main(args):
 
             count_result = query_and_retrieve_result(make_row_count_sql(program))
 
+            print(make_table_sql(program))
+
+
             for row in count_result:
                 count = row['count']
                 break
