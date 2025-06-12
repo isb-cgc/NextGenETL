5fa0bf8575f1f4c0d2d736f3d128df841b06855d
laurenwolfe-isb <lwolfe@systemsbiology.org>
2025-06-11 17:53:26 -0700

    Nest ICDC visit tables by case as well as visit

--- BQ_Table_Building/CDA/ICDC/create_tables_visit_icdc.py
+++ BQ_Table_Building/CDA/ICDC/create_tables_visit_icdc.py
@@ -147,9 +147,9 @@ def main(args):
 
             logger.info("Appending vital signs")
             for vital_sign_row in vital_signs_result:
-                print(vital_sign_row)
                 # confirm visit_id field exists in the query results
                 if 'visit_id' not in vital_sign_row:
+                    print(vital_sign_row)
                     logger.warning("No visit_id found in vital_signs result. Should be investigated. Skipping row.")
                     # todo remove
                     exit(-1)
