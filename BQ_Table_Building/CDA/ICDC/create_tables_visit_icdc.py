c2cfbbb960aabb8a82c8f923f3d3e2c065e0d6b5
laurenwolfe-isb <lwolfe@systemsbiology.org>
2025-06-12 14:40:00 -0700

    Rebuilt visit table generation to make it more reusable

--- BQ_Table_Building/CDA/ICDC/create_tables_visit_icdc.py
+++ BQ_Table_Building/CDA/ICDC/create_tables_visit_icdc.py
@@ -1,9 +1,10 @@
 import sys
 import time
+import logging
+from typing import Any
 
 from cda_bq_etl.data_helpers import initialize_logging, write_list_to_jsonl_and_upload
-from cda_bq_etl.utils import load_config, create_dev_table_id, format_seconds, create_metadata_table_id, \
-    create_clinical_table_id
+from cda_bq_etl.utils import (load_config, create_dev_table_id, format_seconds, create_clinical_table_id)
 from cda_bq_etl.bq_helpers import (update_table_schema_from_generic, query_and_retrieve_result,
                                    create_and_upload_schema_for_json, retrieve_bq_schema_object,
                                    create_and_load_table_from_jsonl, get_program_schema_tags_icdc)
@@ -19,6 +20,8 @@ def make_program_acronym_sql() -> str:
     """
 
 
+# I also recycle this query to filter vital_signs, disease_extent and physical_exam by program.
+# BQ caches results for 24 hours to reduce costs, so reusing it isn't computationally expensive.
 def make_visit_sql(program) -> str:
     return f"""
         WITH merged_case_ids AS (
@@ -41,6 +44,71 @@ def make_visit_sql(program) -> str:
     """
 
 
+def make_child_table_sql(program, table_type) -> str:
+    logger = logging.getLogger('base_script')
+
+    if table_type not in PARAMS['TABLE_COLUMNS']:
+        logger.critical(f"Table type {table_type} not defined in yaml config. Please add it and re-run the script.")
+        exit(-1)
+
+    column_list = PARAMS['TABLE_COLUMNS'][table_type]
+
+    select_statement = "SELECT "
+
+    # either select all columns from child table, or select based on list from TABLE_COLUMNS in yaml config.
+    if not column_list:
+        select_statement += "child_table.* "
+    else:
+        for column in column_list:
+            select_statement += f"child_table.{column}, "
+        select_statement = select_statement[:-2] + " "
+
+    return f"""
+        WITH visit_program_mapping AS ({make_visit_sql(program)})
+            {select_statement}
+            FROM `{create_dev_table_id(PARAMS, table_type)}` child_table
+            LEFT JOIN visit_program_mapping
+                USING (visit_id)
+            WHERE program_acronym = '{program}'
+    """
+
+
+def create_child_field_list(visit_case_mapping: dict[str, str], program: str, table_type: str) -> list[dict] | None:
+    logger = logging.getLogger('base_script')
+
+    child_table_result = query_and_retrieve_result(make_child_table_sql(program, table_type))
+
+    if child_table_result.total_rows == 0:
+        logger.info(f"No rows found for {table_type} in {program}, skipping.")
+        return None
+    else:
+        logger.info(f"Appending {table_type} to {program} visit table.")
+
+    child_row_list = list()
+
+    for row in child_table_result:
+        visit_id = row['visit_id']
+
+        # confirm visit_id is non-null and that it can be mapped to a case_id
+        if not visit_id:
+            logger.warning(f"No visit id {visit_id} found in {table_type}. Skipping row; investigate.")
+            continue
+        elif visit_id not in visit_case_mapping:
+            logger.warning(f"visit id {visit_id} found in {table_type} but not in visit_case_mapping. "
+                           f"Skipping row; investigate.")
+            continue
+
+        child_row_dict = dict()
+
+        for column in PARAMS['TABLE_COLUMNS'][table_type]:
+            child_row_dict[column] = row[column]
+
+        child_row_list.append(child_row_dict)
+
+    return child_row_list
+
+
+'''
 def make_vital_signs_sql(program) -> str:
     return f"""
         WITH visit_program_mapping AS ({make_visit_sql(program)})
@@ -54,6 +122,10 @@ def make_vital_signs_sql(program) -> str:
 
 def make_disease_extent_sql(program) -> str:
     # todo handle the null field exclusions differently
+    except_clause = ''
+    
+    for excluded_column in PARAMS['']
+    
     return f"""
         WITH visit_program_mapping AS ({make_visit_sql(program)})
         SELECT de.* EXCEPT(longest_measurement_unit, longest_measurement_original, longest_measurement_original_unit, 
@@ -75,6 +147,7 @@ def make_physical_exam_sql(program) -> str:
             USING (visit_id)
         WHERE program_acronym = '{program}'
     """
+'''
 
 
 def main(args):
@@ -90,12 +163,13 @@ def main(args):
     log_filepath = f"{PARAMS['LOGFILE_PATH']}.{log_file_time}"
     logger = initialize_logging(log_filepath)
 
-    if 'retrieve_visit_data_and_build_jsonl' in steps:
-        logger.info("Entering retrieve_visit_data_and_build_jsonl")
-        program_result = query_and_retrieve_result(make_program_acronym_sql())
+    program_result = query_and_retrieve_result(make_program_acronym_sql())
 
-        for row in program_result:
-            program = row['program_acronym']
+    for row in program_result:
+        program = row['program_acronym']
+
+        if 'retrieve_visit_data_and_build_jsonl' in steps:
+            logger.info("Entering retrieve_visit_data_and_build_jsonl")
 
             visit_result = query_and_retrieve_result(make_visit_sql(program))
 
@@ -143,117 +217,21 @@ def main(args):
 
                 visit_case_mapping[visit_id] = case_id
 
-            vital_signs_result = query_and_retrieve_result(make_vital_signs_sql(program))
+            # retrieve child data for each visit id and append to visits_dict
+            for visit_id in visits_dict.keys():
+                for table_type in ['vital_signs', 'disease_extent', 'physical_exam']:
+                    logger.info(f"Appending {table_type}")
+                    visits_dict[visit_id][table_type] = create_child_field_list(visit_case_mapping=visit_case_mapping,
+                                                                                program=program,
+                                                                                table_type='vital_signs')
 
-            logger.info("Appending vital signs")
-            for vital_sign_row in vital_signs_result:
-                visit_id = vital_sign_row['visit_id']
-
-                # confirm visit_id is non-null and that it can be mapped to a case_id
-                if not visit_id:
-                    logger.warning(f"No visit id {visit_id} found in vital_signs. Should be investigated. "
-                                   f"Skipping row.")
-                    continue
-                elif visit_id not in visit_case_mapping:
-                    logger.warning(f"visit id {visit_id} found in vital_signs but not in visit_case_mapping. "
-                                   "Should be investigated. Skipping this row.")
-                    continue
-
-                visits_dict[visit_id]['vital_signs'].append({
-                    'date_of_vital_signs': vital_sign_row['date_of_vital_signs'],
-                    'body_temperature': vital_sign_row['body_temperature'],
-                    'body_temperature_unit': vital_sign_row['body_temperature_unit'],
-                    'body_temperature_original': vital_sign_row['body_temperature_original'],
-                    'body_temperature_original_unit': vital_sign_row['body_temperature_original_unit'],
-                    'pulse': vital_sign_row['pulse'],
-                    'pulse_unit': vital_sign_row['pulse_unit'],
-                    'pulse_original': vital_sign_row['pulse_original'],
-                    'pulse_original_unit': vital_sign_row['pulse_original_unit'],
-                    'respiration_rate': vital_sign_row['respiration_rate'],
-                    'respiration_rate_unit': vital_sign_row['respiration_rate_unit'],
-                    'respiration_rate_original': vital_sign_row['respiration_rate_original'],
-                    'respiration_rate_original_unit': vital_sign_row['respiration_rate_original_unit'],
-                    'respiration_pattern': vital_sign_row['respiration_pattern'],
-                    'systolic_bp': vital_sign_row['systolic_bp'],
-                    'systolic_bp_unit': vital_sign_row['systolic_bp_unit'],
-                    'systolic_bp_original': vital_sign_row['systolic_bp_original'],
-                    'systolic_bp_original_unit': vital_sign_row['systolic_bp_original_unit'],
-                    'pulse_ox': vital_sign_row['pulse_ox'],
-                    'pulse_ox_unit': vital_sign_row['pulse_ox_unit'],
-                    'pulse_ox_original': vital_sign_row['pulse_ox_original'],
-                    'pulse_ox_original_unit': vital_sign_row['pulse_ox_original_unit'],
-                    'patient_weight': vital_sign_row['patient_weight'],
-                    'patient_weight_unit': vital_sign_row['patient_weight_unit'],
-                    'patient_weight_original': vital_sign_row['patient_weight_original'],
-                    'patient_weight_original_unit': vital_sign_row['patient_weight_original_unit'],
-                    'body_surface_area': vital_sign_row['body_surface_area'],
-                    'body_surface_area_unit': vital_sign_row['body_surface_area_unit'],
-                    'body_surface_area_original': vital_sign_row['body_surface_area_original'],
-                    'body_surface_area_original_unit': vital_sign_row['body_surface_area_original_unit'],
-                    'modified_ecog': vital_sign_row['modified_ecog']
-                })
-
-            disease_extent_result = query_and_retrieve_result(make_disease_extent_sql(program))
-
-            logger.info("Appending disease_extent")
-
-            for disease_extent_row in disease_extent_result:
-                visit_id = disease_extent_row['visit_id']
-
-                # confirm visit_id is non-null and that it can be mapped to a case_id
-                if not visit_id:
-                    logger.warning(f"No visit id {visit_id} found in disease_extent. Should be investigated. "
-                                   f"Skipping this row.")
-                    continue
-                elif visit_id not in visit_case_mapping:
-                    logger.warning(f"visit id {visit_id} found in disease_extent but not in visit_case_mapping. "
-                                   "Should be investigated. Skipping this row.")
-                    continue
-
-                visits_dict[visit_id]['disease_extent'].append({
-                    'lesion_number': disease_extent_row['lesion_number'],
-                    'lesion_site': disease_extent_row['lesion_site'],
-                    'lesion_description': disease_extent_row['lesion_description'],
-                    'measurable_lesion': disease_extent_row['measurable_lesion'],
-                    'target_lesion': disease_extent_row['target_lesion'],
-                    'date_of_evaluation': disease_extent_row['date_of_evaluation'],
-                    'measured_how': disease_extent_row['measured_how'],
-                    'longest_measurement': disease_extent_row['longest_measurement'],
-                    'evaluation_number': disease_extent_row['evaluation_number'],
-                    'evaluation_code': disease_extent_row['evaluation_code']
-                })
-
-            physical_exam_result = query_and_retrieve_result(make_physical_exam_sql(program))
-
-            logger.info("Appending physical exam")
-            for physical_exam_row in physical_exam_result:
-                visit_id = physical_exam_row['visit_id']
-
-                # confirm visit_id is non-null and that it can be mapped to a case_id
-                if not visit_id:
-                    logger.warning(f"No visit id {visit_id} found in physical_exam. Should be investigated. "
-                                   f"Skipping row.")
-                    continue
-                elif visit_id not in visit_case_mapping:
-                    logger.warning(f"visit id {visit_id} found in physical_exam but not in visit_case_mapping. "
-                                   "Should be investigated. Skipping this row.")
-                    continue
-
-                visits_dict[visit_id]['physical_exam'].append({
-                    'date_of_examination': physical_exam_row['date_of_examination'],
-                    'body_system': physical_exam_row['body_system'],
-                    'pe_finding': physical_exam_row['pe_finding'],
-                    'pe_comment': physical_exam_row['pe_comment']
-                })
-
-            for visit_id, visit_dict in visits_dict.items():
-                # find the case_id associated with the case,
-                # append the visit dict to the list of visits in the cases_visits_dict
+                # append nested visit records to cases_visits_dict
                 case_id = visit_case_mapping[visit_id]
-                cases_visits_dict[case_id]['visits'].append(visit_dict)
+                cases_visits_dict[case_id]['visits'].append(visits_dict[visit_id])
 
             case_visit_record_list = list()
 
+            # make case record list, convert to jsonl and create the schema
             for case_visit_record in cases_visits_dict.values():
                 case_visit_record_list.append(case_visit_record)
 
@@ -266,11 +244,7 @@ def main(args):
                                               table_name=file_prefix,
                                               include_release=True)
 
-    if 'create_table' in steps:
-        program_result = query_and_retrieve_result(make_program_acronym_sql())
-
-        for row in program_result:
-            program = row['program_acronym']
+        if 'create_table' in steps:
             file_prefix = f"{program}_{PARAMS['TABLE_NAME']}"
 
             # Download schema file from Google Cloud bucket
